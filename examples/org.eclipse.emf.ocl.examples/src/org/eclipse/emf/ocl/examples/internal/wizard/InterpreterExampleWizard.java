cbffd81006247345c5f3ae367899931f5f9ccee8
cmcgee <cmcgee>
2006-04-26 17:03:24 +0000

    [132018] emft_head cmcgee 060425 Create a example zipper plugin to be included with the SDK for EMFT components

--- examples/org.eclipse.emf.ocl.examples/src/org/eclipse/emf/ocl/examples/internal/wizard/InterpreterExampleWizard.java
+++ examples/org.eclipse.emf.ocl.examples/src/org/eclipse/emf/ocl/examples/internal/wizard/InterpreterExampleWizard.java
@@ -1,15 +1,22 @@
 package org.eclipse.emf.ocl.examples.internal.wizard;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
 
 
 public class InterpreterExampleWizard
 	extends AbstractExampleWizard {
-
+	
 	protected Collection getProjectDescriptors() {
-		// We simply need the interpreter zip to be unzipped as a project
-		//  into the workspace.
-		return Collections.singletonList(new ProjectDescriptor("org.eclipse.emf.ocl.examples", "zips/interpreter.zip", "org.eclipse.emf.ocl.examples.interpreter"));  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+		// We need the interpreter example to be unzipped along with the
+		// EMF library example model, edit and editor examples
+		List projects = new ArrayList(4);
+		projects.add(new ProjectDescriptor("org.eclipse.emf.ocl.examples", "zips/interpreter.zip", "org.eclipse.emf.ocl.examples.interpreter")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		projects.add(new ProjectDescriptor("org.eclipse.emf.ocl.examples", "zips/library.zip", "org.eclipse.emf.examples.library"));  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+		projects.add(new ProjectDescriptor("org.eclipse.emf.ocl.examples", "zips/libraryEdit.zip", "org.eclipse.emf.examples.library.edit")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+		projects.add(new ProjectDescriptor("org.eclipse.emf.ocl.examples", "zips/libraryEditor.zip", "org.eclipse.emf.examples.library.editor"));  //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
+		
+		return projects;
 	}
 }
