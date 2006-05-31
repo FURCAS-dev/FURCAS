d291fcde9f828f15fb2b341d709603e16f7d4435
cdamus <cdamus>
2006-05-31 15:05:46 +0000

    [144669] emft_head cdamus 060531 OCL, Query, Transaction, Validation Javadocs include internal packages

--- doc/org.eclipse.emf.ocl.doc/build/antJavadoc.sh
+++ doc/org.eclipse.emf.ocl.doc/build/antJavadoc.sh
@@ -57,8 +57,10 @@ for pluginDir in $pluginDirs; do
 	pluginDir=`echo $pluginDir | sed -e 's/\/runtime$//g'`
 	srcDir=$pluginDir/src
 	if [ -d "$srcDir" ]; then
-		packagesets=$packagesets"<packageset dir=\"$srcDir\"><exclude name=\"$srcDir/**/doc-files/**\"/></packageset>"
+		packagesets=$packagesets"<packageset dir=\"$srcDir\">"
+		packagesets=$packagesets"<exclude name=\"$srcDir/**/doc-files/**\"/>"
 		packagesets=$packagesets""$javadocExclusions
+		packagesets=$packagesets"</packageset>"
 		copydocfiles=$copydocfiles"<copyDocFiles pluginDir=\"$pluginDir\"/>"
 	fi
 done
