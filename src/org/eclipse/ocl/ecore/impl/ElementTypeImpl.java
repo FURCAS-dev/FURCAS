8cd7cd994997ba84d064e9a0970be885796328a2
cdamus <cdamus>
2007-02-14 13:42:01 +0000

    [174117] cdamus - Code clean-up Add copyrights where they were absent

--- src/org/eclipse/ocl/ecore/impl/ElementTypeImpl.java
+++ src/org/eclipse/ocl/ecore/impl/ElementTypeImpl.java
@@ -1,8 +1,18 @@
 /**
  * <copyright>
+ * 
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   IBM - Initial API and implementation
+ * 
  * </copyright>
  *
- * $Id: ElementTypeImpl.java,v 1.1 2007/01/25 18:29:09 cdamus Exp $
+ * $Id: ElementTypeImpl.java,v 1.2 2007/02/14 13:42:01 cdamus Exp $
  */
 package org.eclipse.ocl.ecore.impl;
 
