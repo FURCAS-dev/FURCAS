cd6ea57c0caea367d8f29a33e73dbe600ce68ff8
cdamus <cdamus>
2007-02-14 14:45:53 +0000

    [174117] cdamus - Code clean-up Update copyrights

--- plugins/org.eclipse.ocl.uml/src/org/eclipse/ocl/uml/internal/OCLUMLPlugin.java
+++ plugins/org.eclipse.ocl.uml/src/org/eclipse/ocl/uml/internal/OCLUMLPlugin.java
@@ -1,3 +1,19 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: OCLUMLPlugin.java,v 1.2 2007/02/14 14:45:53 cdamus Exp $
+ */
 package org.eclipse.ocl.uml.internal;
 
 import org.eclipse.core.runtime.IStatus;
