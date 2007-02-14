abda7c6efdc02ae59c0718b267c31b99a84170e1
cdamus <cdamus>
2007-02-14 13:42:12 +0000

    [174117] cdamus - Code clean-up Add copyrights where they were absent

--- plugins/org.eclipse.ocl/src/org/eclipse/ocl/types/impl/MessageTypeImpl.java
+++ plugins/org.eclipse.ocl/src/org/eclipse/ocl/types/impl/MessageTypeImpl.java
@@ -1,8 +1,18 @@
 /**
  * <copyright>
+ * 
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   IBM - Initial API and implementation
+ * 
  * </copyright>
  *
- * $Id: MessageTypeImpl.java,v 1.1 2007/01/25 18:24:35 cdamus Exp $
+ * $Id: MessageTypeImpl.java,v 1.2 2007/02/14 13:42:15 cdamus Exp $
  */
 package org.eclipse.ocl.types.impl;
 
