1c24948b048921ad01d7f2f3dcb0b1834e0bc197
cdamus <cdamus>
2007-05-17 16:03:32 +0000

    [159795] cdamus - Unresolved external Javadoc links Fix javadoc links for compatibility API

--- doc/org.eclipse.emf.ocl.doc/build/antJavadoc.sh
+++ doc/org.eclipse.emf.ocl.doc/build/antJavadoc.sh
@@ -98,7 +98,11 @@ if [ $debug -gt 0 ]; then
 fi
 
 # Finds the proper org.eclipse.platform.doc.isv jar
-docjar=`find $eclipseDir/plugins/ -name "org.eclipse.platform.doc.isv*.jar" -printf "%f"`; if [ $debug -gt 1 ]; then echo "[antJd] docjar: "$docjar; fi
+platformDocJar=`find $eclipseDir/plugins/ -name "org.eclipse.platform.doc.isv*.jar" -printf "%f"`; if [ $debug -gt 1 ]; then echo "[antJd] platformDocJar: "$platformDocJar; fi
+
+# Finds the proper org.eclipse.emf.doc jar
+# *** NOTE: EMF docs are still a directory plug-in ***
+emfDocJar=`find $eclipseDir/plugins/ -type d -name "org.eclipse.emf.doc*" -printf "%f/doc.zip"`; if [ $debug -gt 1 ]; then echo "[antJd] emfDocJar: "$emfDocJar; fi
 
 if [ -f $antScript.template ]; then
 	true;
@@ -120,7 +124,8 @@ ant -f $antScript \
 	-DdestDir="$destDir" \
 	-Dclasspath="$classpath" \
 	-DeclipseDir="$eclipseDir" \
-	-Ddocjar="$docjar" \
+	-DplatformDocJar="$eclipseDir/plugins/$platformDocJar" \
+	-DemfDocJar="$eclipseDir/plugins/$emfDocJar" \
 	-DwindowTitle="$windowTitle" \
 	-DgroupTitle="$groupTitle" \
 	-Doverview="$currentPath/overview.html";
