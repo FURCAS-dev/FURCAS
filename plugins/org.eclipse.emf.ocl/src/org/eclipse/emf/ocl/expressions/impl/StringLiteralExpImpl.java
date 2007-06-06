3fefebcfe2f469aa7d1402dffffab83404317117
cdamus <cdamus>
2007-06-06 18:56:34 +0000

    [191313] cdamus - [Legal] Update Copyright dates in source code for Europa Release

--- plugins/org.eclipse.emf.ocl/src/org/eclipse/emf/ocl/expressions/impl/StringLiteralExpImpl.java
+++ plugins/org.eclipse.emf.ocl/src/org/eclipse/emf/ocl/expressions/impl/StringLiteralExpImpl.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2005, 2006 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: StringLiteralExpImpl.java,v 1.2 2007/02/14 14:45:39 cdamus Exp $
+ * $Id: StringLiteralExpImpl.java,v 1.3 2007/06/06 18:56:36 cdamus Exp $
  */
 
 package org.eclipse.emf.ocl.expressions.impl;
