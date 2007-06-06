91dea171a0995304c3d48a186b1b12cf0ac8f24d
cdamus <cdamus>
2007-06-06 18:56:55 +0000

    [191313] cdamus - [Legal] Update Copyright dates in source code for Europa Release

--- examples/org.eclipse.emf.ocl.examples.interpreter/src/org/eclipse/emf/ocl/examples/interpreter/OCLExamplePlugin.java
+++ examples/org.eclipse.emf.ocl.examples.interpreter/src/org/eclipse/emf/ocl/examples/interpreter/OCLExamplePlugin.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2005 IBM Corporation and others.
+ * Copyright (c) 2005, 2007 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: OCLExamplePlugin.java,v 1.3 2006/04/25 17:26:19 cmcgee Exp $
+ * $Id: OCLExamplePlugin.java,v 1.4 2007/06/06 18:56:55 cdamus Exp $
  */
 
 package org.eclipse.emf.ocl.examples.interpreter;
