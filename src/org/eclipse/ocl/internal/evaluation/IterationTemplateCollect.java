70f65c6d3eb71519f8fa4b3141dd61ac4fa844e9
cdamus <cdamus>
2008-03-26 21:17:25 +0000

    [217461] cdamus - Recursive flattening of collections

--- src/org/eclipse/ocl/internal/evaluation/IterationTemplateCollect.java
+++ src/org/eclipse/ocl/internal/evaluation/IterationTemplateCollect.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2005, 2008 IBM Corporation and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: IterationTemplateCollect.java,v 1.2 2007/10/11 23:05:05 cdamus Exp $
+ * $Id: IterationTemplateCollect.java,v 1.3 2008/03/26 21:17:25 cdamus Exp $
  */
 
 package org.eclipse.ocl.internal.evaluation;
@@ -23,6 +23,7 @@ import java.util.List;
 import org.eclipse.ocl.EvaluationEnvironment;
 import org.eclipse.ocl.EvaluationVisitor;
 import org.eclipse.ocl.expressions.Variable;
+import org.eclipse.ocl.util.CollectionUtil;
 
 /**
  *
@@ -57,7 +58,7 @@ public class IterationTemplateCollect<PK, C, O, P, EL, PM, S, COA, SSA, CT, CLS,
 		
 		if (bodyVal instanceof Collection) {
 			Collection<?> bodyColl = (Collection<?>) bodyVal;
-			currVal.addAll(bodyColl);
+			currVal.addAll(CollectionUtil.flatten(bodyColl));
 		}
 		else
 			currVal.add(bodyVal);
