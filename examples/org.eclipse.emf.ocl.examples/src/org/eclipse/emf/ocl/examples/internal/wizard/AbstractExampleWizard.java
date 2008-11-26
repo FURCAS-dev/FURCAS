91b5c7fb604e1954fc0d3d643ac84e0de5d3bb7f
cdamus <cdamus>
2008-11-26 18:53:27 +0000

    [233468] cdamus - Copyrights

--- examples/org.eclipse.emf.ocl.examples/src/org/eclipse/emf/ocl/examples/internal/wizard/AbstractExampleWizard.java
+++ examples/org.eclipse.emf.ocl.examples/src/org/eclipse/emf/ocl/examples/internal/wizard/AbstractExampleWizard.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2006, 2007 IBM Corporation and others.
+ * Copyright (c) 2006, 2008 IBM Corporation, Zeligsoft Inc., and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,6 +9,7 @@
  *
  * Contributors:
  *   IBM - Initial API and implementation
+ *   Zeligsoft - Bug 233486
  *
  * </copyright>
  *
