f574895853dbfd84e470b3caa49a2b16b7d4500e
bkolb <bkolb>
2009-08-28 08:53:17 +0000

    initial

---
+++ prototypes/query2syntax/org.eclipse.emf.query2/src/org/eclipse/emf/query2/validation/QueryJavaValidator.java
@@ -0,0 +1,13 @@
+package org.eclipse.emf.query2.validation;
+ 
+
+public class QueryJavaValidator extends AbstractQueryJavaValidator {
+
+//	@Check
+//	public void checkTypeNameStartsWithCapital(Type type) {
+//		if (!Character.isUpperCase(type.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.TYPE__NAME);
+//		}
+//	}
+
+}
