2cd8f9c25721c212f1085764fa19641c7b6c76c4
ewillink <ewillink>
2009-10-23 20:55:59 +0000

    [283509] Fix lexer error message substitutions

--- tests/org.eclipse.ocl.ecore.tests/src/org/eclipse/ocl/ecore/tests/KeywordsTest.java
+++ tests/org.eclipse.ocl.ecore.tests/src/org/eclipse/ocl/ecore/tests/KeywordsTest.java
@@ -9,10 +9,11 @@
  *
  * Contributors:
  *   IBM - Initial API and implementation
+ *   E.D.Willink Bug 283509
  *
  * </copyright>
  *
- * $Id: KeywordsTest.java,v 1.2 2009/10/07 20:39:27 ewillink Exp $
+ * $Id: KeywordsTest.java,v 1.3 2009/10/23 20:55:59 ewillink Exp $
  */
 
 package org.eclipse.ocl.ecore.tests;
@@ -45,6 +46,7 @@ import org.eclipse.ocl.expressions.Variable;
  * 
  * @author Christian W. Damus (cdamus)
  */
+@SuppressWarnings("nls")
 public class KeywordsTest
     extends AbstractTestSuite {
 
@@ -61,10 +63,10 @@ public class KeywordsTest
         helper.setContext(property);
 
         try {
-            helper.createInvariant("self.isUnique implies self.upper > 1"); //$NON-NLS-1$
-            helper.createInvariant("isUnique implies self.upper > 1"); //$NON-NLS-1$
+            helper.createInvariant("self.isUnique implies self.upper > 1");
+            helper.createInvariant("isUnique implies self.upper > 1");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -73,11 +75,11 @@ public class KeywordsTest
 
         try {
             helper
-                .createInvariant("self._package.oclIsUndefined() implies Set{VisibilityKind::protected, VisibilityKind::_package}->excludes(self.visibility)"); //$NON-NLS-1$
+                .createInvariant("self._package.oclIsUndefined() implies Set{VisibilityKind::protected, VisibilityKind::_package}->excludes(self.visibility)");
             helper
-                .createInvariant("_package.oclIsUndefined() implies Set{VisibilityKind::protected, VisibilityKind::_package}->excludes(self.visibility)"); //$NON-NLS-1$
+                .createInvariant("_package.oclIsUndefined() implies Set{VisibilityKind::protected, VisibilityKind::_package}->excludes(self.visibility)");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -86,11 +88,11 @@ public class KeywordsTest
 
         try {
             helper
-                .createInvariant("self._context <> null implies _context.oclIsKindOf(Class)"); //$NON-NLS-1$
+                .createInvariant("self._context <> null implies _context.oclIsKindOf(Class)");
             helper
-                .createInvariant("_context <> null implies self._context.oclIsKindOf(Class)"); //$NON-NLS-1$
+                .createInvariant("_context <> null implies self._context.oclIsKindOf(Class)");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -99,16 +101,16 @@ public class KeywordsTest
 
         Variable<EClassifier, EParameter> var = ocl.getEnvironment()
             .getOCLFactory().createVariable();
-        var.setName("aBlock"); //$NON-NLS-1$
+        var.setName("aBlock");
         var.setType(block);
 
         ocl.getEnvironment().addElement(var.getName(), var, true);
 
         try {
-            helper.createInvariant("self.isUnique(aBlock)"); //$NON-NLS-1$
-            helper.createInvariant("isUnique(aBlock)"); //$NON-NLS-1$
+            helper.createInvariant("self.isUnique(aBlock)");
+            helper.createInvariant("isUnique(aBlock)");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -117,16 +119,16 @@ public class KeywordsTest
 
         Variable<EClassifier, EParameter> var = ocl.getEnvironment()
             .getOCLFactory().createVariable();
-        var.setName("aBlock"); //$NON-NLS-1$
+        var.setName("aBlock");
         var.setType(block);
 
         ocl.getEnvironment().addElement(var.getName(), var, true);
 
         try {
-            helper.createInvariant("self.select(aBlock).isEmpty()"); //$NON-NLS-1$
-            helper.createInvariant("select(aBlock).isEmpty()"); //$NON-NLS-1$
+            helper.createInvariant("self.select(aBlock).isEmpty()");
+            helper.createInvariant("select(aBlock).isEmpty()");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -134,10 +136,10 @@ public class KeywordsTest
         helper.setContext(block);
 
         try {
-            helper.createInvariant("self._context(self).name = 'Block'"); //$NON-NLS-1$
-            helper.createInvariant("_context(self).name = 'Block'"); //$NON-NLS-1$
+            helper.createInvariant("self._context(self).name = 'Block'");
+            helper.createInvariant("_context(self).name = 'Block'");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -146,9 +148,9 @@ public class KeywordsTest
 
         try {
             helper
-                .createInvariant("Smalltalk::runtime::_context::language.allInstances()->notEmpty()"); //$NON-NLS-1$
+                .createInvariant("Smalltalk::runtime::_context::language.allInstances()->notEmpty()");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -156,9 +158,9 @@ public class KeywordsTest
         try {
             ocl
                 .parse(new OCLInput(
-                    "package Smalltalk::runtime::_context context language inv: true endpackage")); //$NON-NLS-1$
+                    "package Smalltalk::runtime::_context context language inv: true endpackage"));
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -167,15 +169,15 @@ public class KeywordsTest
 
         try {
             helper
-                .createInvariant("let e : _else = self in _else.allInstances()->forAll(oclIsKindOf(_else))"); //$NON-NLS-1$
+                .createInvariant("let e : _else = self in _else.allInstances()->forAll(oclIsKindOf(_else))");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
 
         try {
-            helper.createInvariant("self.oclIsKindOf(_context::_else)"); //$NON-NLS-1$
+            helper.createInvariant("self.oclIsKindOf(_context::_else)");
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
         }
     }
 
@@ -183,12 +185,23 @@ public class KeywordsTest
         try {
             ocl
                 .parse(new OCLInput(
-                    "package Smalltalk context runtime::_context::_else::doIt() : pre: true endpackage")); //$NON-NLS-1$
+                    "package Smalltalk context runtime::_context::_else::doIt() : pre: true endpackage"));
             ocl
                 .parse(new OCLInput(
-                    "package Smalltalk::runtime::_context context _else::doIt() : pre: true endpackage")); //$NON-NLS-1$
+                    "package Smalltalk::runtime::_context context _else::doIt() : pre: true endpackage"));
         } catch (Exception e) {
-            fail("Failed to parse: " + e.getLocalizedMessage()); //$NON-NLS-1$
+            fail("Failed to parse: " + e.getLocalizedMessage());
+        }
+    }
+
+	public void test_unexpectedCharacter_283509() {
+        helper.setContext(property);
+
+        try {
+            helper.createInvariant("self = #self");
+            fail("Missing exception");
+        } catch (Exception e) {
+            assertEquals("2:6 \"#\" unexpected character ignored", e.getLocalizedMessage());
         }
     }
 
@@ -203,38 +216,38 @@ public class KeywordsTest
         // create a little test model for a Smalltalk-like collection class that
         // defines operations corresponding to OCL iterators
 
-        res = new ResourceImpl(URI.createURI("foo://smalltalk")); //$NON-NLS-1$
+        res = new ResourceImpl(URI.createURI("foo://smalltalk"));
 
         smalltalk = EcoreFactory.eINSTANCE.createEPackage();
-        smalltalk.setName("Smalltalk"); //$NON-NLS-1$
-        smalltalk.setNsPrefix("st"); //$NON-NLS-1$
-        smalltalk.setNsURI("foo://smalltalk"); //$NON-NLS-1$
+        smalltalk.setName("Smalltalk");
+        smalltalk.setNsPrefix("st");
+        smalltalk.setNsURI("foo://smalltalk");
 
         res.getContents().add(smalltalk);
         resourceSet.getPackageRegistry().put(smalltalk.getNsURI(), smalltalk);
 
         EClass object = EcoreFactory.eINSTANCE.createEClass();
-        object.setName("Object"); //$NON-NLS-1$
+        object.setName("Object");
         smalltalk.getEClassifiers().add(object);
 
         collection = EcoreFactory.eINSTANCE.createEClass();
-        collection.setName("Collection"); //$NON-NLS-1$
+        collection.setName("Collection");
         smalltalk.getEClassifiers().add(collection);
 
         block = EcoreFactory.eINSTANCE.createEClass();
-        block.setName("Block"); //$NON-NLS-1$
+        block.setName("Block");
         smalltalk.getEClassifiers().add(block);
 
         clazz = EcoreFactory.eINSTANCE.createEClass();
-        clazz.setName("Class"); //$NON-NLS-1$
+        clazz.setName("Class");
         smalltalk.getEClassifiers().add(clazz);
 
         property = EcoreFactory.eINSTANCE.createEClass();
-        property.setName("Property"); //$NON-NLS-1$
+        property.setName("Property");
         smalltalk.getEClassifiers().add(property);
 
         constraint = EcoreFactory.eINSTANCE.createEClass();
-        constraint.setName("Constraint"); //$NON-NLS-1$
+        constraint.setName("Constraint");
         smalltalk.getEClassifiers().add(constraint);
 
         constraint.getESuperTypes().add(object);
@@ -244,57 +257,57 @@ public class KeywordsTest
         collection.getESuperTypes().add(object);
 
         EEnum visibilityKind = EcoreFactory.eINSTANCE.createEEnum();
-        visibilityKind.setName("VisibilityKind"); //$NON-NLS-1$
+        visibilityKind.setName("VisibilityKind");
         smalltalk.getEClassifiers().add(visibilityKind);
-        createLiteral(visibilityKind, "public"); //$NON-NLS-1$
-        createLiteral(visibilityKind, "protected"); //$NON-NLS-1$
-        createLiteral(visibilityKind, "package"); //$NON-NLS-1$
-        createLiteral(visibilityKind, "private"); //$NON-NLS-1$
+        createLiteral(visibilityKind, "public");
+        createLiteral(visibilityKind, "protected");
+        createLiteral(visibilityKind, "package");
+        createLiteral(visibilityKind, "private");
         
-        createAttribute(clazz, "name", EcorePackage.Literals.ESTRING); //$NON-NLS-1$
-        createAttribute(clazz, "visibility", visibilityKind); //$NON-NLS-1$
-        createReference(clazz, "package", object); //$NON-NLS-1$
-        createAttribute(property, "isUnique", EcorePackage.Literals.EBOOLEAN); //$NON-NLS-1$
-        createAttribute(property, "upper", EcorePackage.Literals.EINT); //$NON-NLS-1$
-        createReference(constraint, "context", clazz); //$NON-NLS-1$
+        createAttribute(clazz, "name", EcorePackage.Literals.ESTRING);
+        createAttribute(clazz, "visibility", visibilityKind);
+        createReference(clazz, "package", object);
+        createAttribute(property, "isUnique", EcorePackage.Literals.EBOOLEAN);
+        createAttribute(property, "upper", EcorePackage.Literals.EINT);
+        createReference(constraint, "context", clazz);
         
         EList<String> names = new BasicEList<String>(1);
         EList<EClassifier> types = new BasicEList<EClassifier>(1);
 
-        names.add("object"); //$NON-NLS-1$
+        names.add("object");
         types.add(object);
-        createOwnedOperation(block, "context", names, types, clazz); //$NON-NLS-1$
+        createOwnedOperation(block, "context", names, types, clazz);
 
-        names.set(0, "block"); //$NON-NLS-1$
+        names.set(0, "block");
         types.set(0, block);
         createOwnedOperation(collection,
-            "isUnique", names, types, EcorePackage.Literals.EBOOLEAN); //$NON-NLS-1$
-        createOwnedOperation(collection, "select", names, types, collection); //$NON-NLS-1$
+            "isUnique", names, types, EcorePackage.Literals.EBOOLEAN);
+        createOwnedOperation(collection, "select", names, types, collection);
 
         names.clear();
         types.clear();
         createOwnedOperation(collection,
-            "isEmpty", names, types, EcorePackage.Literals.EBOOLEAN); //$NON-NLS-1$
+            "isEmpty", names, types, EcorePackage.Literals.EBOOLEAN);
 
         // create some qualified classifier and package names that need
         // escaping of one or more segments. Likewise state names
         EPackage nested = EcoreFactory.eINSTANCE.createEPackage();
-        nested.setName("runtime"); //$NON-NLS-1$
+        nested.setName("runtime");
         smalltalk.getESubpackages().add(nested);
 
         EPackage contextPackage = EcoreFactory.eINSTANCE.createEPackage();
-        contextPackage.setName("context"); //$NON-NLS-1$
+        contextPackage.setName("context");
         nested.getESubpackages().add(contextPackage);
 
         EClass tmp = EcoreFactory.eINSTANCE.createEClass();
-        tmp.setName("language"); //$NON-NLS-1$
+        tmp.setName("language");
         contextPackage.getEClassifiers().add(tmp);
 
         elseClass = EcoreFactory.eINSTANCE.createEClass();
-        elseClass.setName("else"); //$NON-NLS-1$
+        elseClass.setName("else");
         contextPackage.getEClassifiers().add(elseClass);
 
-        createOwnedOperation(elseClass, "doIt", null, null, null); //$NON-NLS-1$
+        createOwnedOperation(elseClass, "doIt", null, null, null);
     }
 
     private static EOperation createOwnedOperation(EClass eclass, String name,
