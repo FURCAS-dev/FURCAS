ad88b01d79341ceb3934fd7a507ec123c64cef98
theaschroeter <theaschroeter@3ca8e212-6386-4b1e-b09d-39f68028012c>
2009-11-10 20:16:16 +0000

    emf query hinzugefügt
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@67 3ca8e212-6386-4b1e-b09d-39f68028012c

--- test/src/test2/handlers/SampleHandler2.java
+++ test/src/test2/handlers/SampleHandler2.java
@@ -2,15 +2,23 @@ package test2.handlers;
 
 import java.io.IOException;
 import java.util.Collections;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-//import org.eclipse.emf.common.util.EList;
+
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.query.conditions.eobjects.EObjectCondition;
+import org.eclipse.emf.query.conditions.eobjects.EObjectInstanceCondition;
+import org.eclipse.emf.query.conditions.eobjects.EObjectTypeRelationCondition;
+import org.eclipse.emf.query.conditions.eobjects.structuralfeatures.EObjectReferenceValueCondition;
+import org.eclipse.emf.query.conditions.eobjects.structuralfeatures.EObjectReferencerCondition;
+import org.eclipse.emf.query.statements.FROM;
+import org.eclipse.emf.query.statements.SELECT;
+import org.eclipse.emf.query.statements.WHERE;
 import org.eclipse.ocl.ParserException;
 import org.eclipse.ocl.ecore.OCL;
 import org.eclipse.ocl.ecore.OCLExpression;
@@ -36,24 +44,28 @@ public class SampleHandler2 extends AbstractHandler{
 			e.printStackTrace();
 		}
 		UsecaseDiagram usecasediagram=(UsecaseDiagram) resource.getContents().get(0);//save content of first element in list UsecaseDiagram  
-		//Element element=usecasediagram.getElements().get(0);//save content of first element in list Element
-		OCL ocl=OCL.newInstance();//starte OCL
-		Helper oclhelper=ocl.createOCLHelper();//starte OCL helper um Ausdrücke auswerten zu können
-		oclhelper.setContext(UsecasePackage.Literals.ACTOR);//um self zuordnen zu können
+		OCL ocl=OCL.newInstance();//start OCL
+		Helper oclhelper=ocl.createOCLHelper();//start OCL helper to interpret constraints
+		oclhelper.setContext(UsecasePackage.Literals.ACTOR);//to assign "self"
 		
 		for(Element ele:usecasediagram.getElements()){
 			if(ele instanceof Actor){
 				Actor actor=(Actor)ele;	
 				String string ="self.outgoing->notEmpty()";
 				try {
-					OCLExpression oclexpression=oclhelper.createQuery(string);//erstellt aus dem string ein ocl constraint
-					System.out.println(ocl.evaluate(actor, oclexpression));//evaluate wertet constraint aus, ausgabe
+					OCLExpression oclexpression=oclhelper.createQuery(string);//create ocl constraint out of string 
+					System.out.println(ocl.evaluate(actor, oclexpression));//evaluate constraint, then  
 				} catch (ParserException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
-		}		
+		}
+		//IEStructuralFeatureValueGetter getter = EStructuralFeatureValueGetter.getInstance();
+		
+		EObjectCondition condition = new EObjectReferenceValueCondition( new EObjectTypeRelationCondition(UsecasePackage.Literals.USECASE), UsecasePackage.Literals.ELEMENT__DIAGRAM ,  new EObjectTypeRelationCondition(UsecasePackage.Literals.USECASE_DIAGRAM));
+		SELECT select= new SELECT(new FROM(usecasediagram) ,new WHERE(condition) );//emf query ähnlich sql anfrage, condition im wurzelelement prüfen
+		System.out.println(select.execute().size());//ausgabe auf der console
 		return null;
 		
 	}
