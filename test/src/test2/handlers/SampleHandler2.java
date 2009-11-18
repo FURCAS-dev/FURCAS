1aa8729eb64fe327a3ae53c12277012f7709e552
theaschroeter <theaschroeter@3ca8e212-6386-4b1e-b09d-39f68028012c>
2009-11-18 14:30:14 +0000

    ocl constraint als emf query erledigt
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@77 3ca8e212-6386-4b1e-b09d-39f68028012c

--- test/src/test2/handlers/SampleHandler2.java
+++ test/src/test2/handlers/SampleHandler2.java
@@ -1,6 +1,7 @@
 package test2.handlers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,6 +30,8 @@ import org.eclipse.ocl.ecore.OCL;
 import org.eclipse.ocl.ecore.OCLExpression;
 import org.eclipse.ocl.ecore.OCL.Helper;
 
+import de.hpi.sam.qcl.OclUtil;
+
 import usecase.Actor;
 import usecase.Element;
 import usecase.UsecaseDiagram;
@@ -38,6 +41,7 @@ public class SampleHandler2 extends AbstractHandler{
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
+		String completeQuery= "context Usecase inv: self.ID<10"; 
 		// create Resource My.usecase in Resourceset 
 		ResourceSet resourceset=new ResourceSetImpl();
 		Resource resource = resourceset.createResource(URI.createPlatformResourceURI("/test/My.usecase", true));
@@ -46,68 +50,19 @@ public class SampleHandler2 extends AbstractHandler{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		/*UsecaseDiagram usecasediagram=(UsecaseDiagram) resource.getContents().get(0);//save content of first element in list UsecaseDiagram  
-		OCL ocl=OCL.newInstance();//start OCL
-		Helper oclhelper=ocl.createOCLHelper();//start OCL helper to interpret constraints
-		oclhelper.setContext(UsecasePackage.Literals.ACTOR);
-		oclhelper.setContext(UsecasePackage.Literals.ACTOR);
-		
-		for(Element ele:usecasediagram.getElements()){
-			if(ele instanceof Actor){
-				Actor actor=(Actor)ele;	
-				String string ="self.outgoing->notEmpty()";
-				try {
-					OCLExpression oclexpression=oclhelper.createQuery(string);//create ocl constraint out of string 
-					System.out.println("OCL : "+ocl.evaluate(actor, oclexpression));//evaluate constraint, then  
-				} catch (ParserException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}*/
 		
 		try{
-		String completeQuery=		"context UsecaseDiagram inv: self.elements->size()=2"; //leerzeichen vor UsecaseDiagram fixen, element nicht korrekt?
-		String regex="context([\\w\\W]*)? inv";//try for match: use the beginning-of-input and end-of-input anchors: ^ and $. 
-		Pattern pattern=Pattern.compile(regex);
-		Matcher match=pattern.matcher(completeQuery);
-		String queryContext="";
-		if(match.find()){
-			queryContext=match.group(1);
-		}
-		queryContext=queryContext.trim();
-		String queryInvariant=completeQuery.substring(completeQuery.indexOf("inv:")+4);
-		queryInvariant=queryInvariant.trim();
-		IQueryResult results=null;
-		EClassifier context=UsecasePackage.eINSTANCE.getEClassifier(queryContext);
-		try {
-			results = validateOclQuery(context.eClass(), queryInvariant, resource);
-		} catch (ParserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("oclEMFResult : "+results.size());
+		IQueryResult results = OclUtil.executeQueryOn(completeQuery, resource);
+		if(results==null)
+			System.out.println("Results null");
+		else
+			System.out.println("oclEMFResult : "+results.size());
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-			
-		/*EObjectCondition condition1 = new EObjectReferenceValueCondition( new EObjectTypeRelationCondition(UsecasePackage.Literals.USECASE), UsecasePackage.Literals.ELEMENT__DIAGRAM ,  new EObjectTypeRelationCondition(UsecasePackage.Literals.USECASE_DIAGRAM));
-		SELECT select= new SELECT(new FROM(usecasediagram) ,new WHERE(condition1) );//emf query ähnlich sql anfrage, condition im wurzelelement prüfen
-		System.out.println("PUREEMF : "+select.execute().size());//ausgabe auf der console*/
 		return null;
 		
 	}
-	public IQueryResult validateOclQuery(EClass context, String query, Resource resource) throws ParserException{
-		OCL ocl=OCL.newInstance();
-		Condition condition=null;
-			condition = new org.eclipse.emf.query.ocl.conditions.BooleanOCLCondition<EClassifier, EClass, EObject>(
-				    ocl.getEnvironment(),
-				    query,
-				    context);
-		SELECT statement = new SELECT(SELECT.UNBOUNDED, false,
-				new FROM(resource.getContents()), new WHERE((EObjectCondition) condition));
 
-		return statement.execute();
-	}
 
 }
