baadf01ccf456759eb7c33f29ffcea709780a3d1
phberger <phberger@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-02-26 09:05:43 +0000

    Here we go
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@323 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ EcoreModelDataTypes/src/com/sap/mi/gfw/mm/datatypes/impl/ViewPortImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.sap.mi.gfw.mm.datatypes.impl;
+
+import com.sap.mi.gfw.mm.datatypes.DatatypesPackage;
+import com.sap.mi.gfw.mm.datatypes.ViewPort;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>View Port</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class ViewPortImpl extends EObjectImpl implements ViewPort {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected ViewPortImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return DatatypesPackage.Literals.VIEW_PORT;
+	}
+
+} //ViewPortImpl
