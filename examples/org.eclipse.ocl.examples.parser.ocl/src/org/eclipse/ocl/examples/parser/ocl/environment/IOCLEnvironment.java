2945c8962bc019507c6efc1785f6f3f82c1f0ed8
ewillink <ewillink>
2010-03-11 14:43:53 +0000

    [289761] Migrate M2M/QVTd OCL editor to MDT/OCL

---
+++ examples/org.eclipse.ocl.examples.parser.ocl/src/org/eclipse/ocl/examples/parser/ocl/environment/IOCLEnvironment.java
@@ -0,0 +1,23 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) 2008,2010 Eclipse Modeling Project and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ * E.D.Willink - initial API and implementation
+ * 
+ * </copyright>
+ *
+ * $Id: IOCLEnvironment.java,v 1.1 2010/03/11 14:43:54 ewillink Exp $
+ */
+package org.eclipse.ocl.examples.parser.ocl.environment;
+
+import org.eclipse.ocl.examples.parser.environment.ecore.ICSTNodeEcoreEnvironment;
+
+public interface IOCLEnvironment extends ICSTNodeEcoreEnvironment
+{
+}
