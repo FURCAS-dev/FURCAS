1cacd5ff1d574dc18be3f405adbd3381c1709f0f
ewillink <ewillink>
2010-03-22 01:21:23 +0000

    Use ResourceSet as OCLInEcore AST Root

--- examples/org.eclipse.ocl.examples.editor.ocl.ui/src/org/eclipse/ocl/examples/editor/ocl/ui/imp/OCLTreeModelBuilder.java
+++ examples/org.eclipse.ocl.examples.editor.ocl.ui/src/org/eclipse/ocl/examples/editor/ocl/ui/imp/OCLTreeModelBuilder.java
@@ -12,11 +12,12 @@
  * 
  * </copyright>
  *
- * $Id: OCLTreeModelBuilder.java,v 1.1 2010/03/11 14:52:53 ewillink Exp $
+ * $Id: OCLTreeModelBuilder.java,v 1.2 2010/03/22 01:21:23 ewillink Exp $
  */
 package org.eclipse.ocl.examples.editor.ocl.ui.imp;
 
 import org.eclipse.ocl.examples.editor.ocl.ui.OCLCreationFactory;
+import org.eclipse.ocl.examples.editor.ui.ICreationFactory;
 import org.eclipse.ocl.examples.editor.ui.imp.CommonTreeModelBuilder;
 
 
@@ -28,5 +29,9 @@ public class OCLTreeModelBuilder extends CommonTreeModelBuilder
 
 	public OCLTreeModelBuilder(boolean showAST) {
 		super(OCLCreationFactory.INSTANCE, showAST);
+	}
+
+	public OCLTreeModelBuilder(ICreationFactory creationFactory, boolean showAST) {
+		super(creationFactory, showAST);
 	}
 }
