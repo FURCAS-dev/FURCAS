cf7744d4cee218280c1326d19c8f1dcbaf18bc07
unknown <C5106462@.(none)>
2010-03-31 15:36:57 +0200

    updated moin_tests

---
+++ moin_tests/com.sap.tc.moin.repository.test.MoinTestEclipsePlugin/src/com/sap/tc/moin/repository/test/ocl/TestSuiteOCLAttributeInitializer.java
@@ -0,0 +1,11 @@
+package com.sap.tc.moin.repository.test.ocl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.sap.junit.MoinSuite;
+
+@RunWith( MoinSuite.class )
+@Suite.SuiteClasses( { TestAttributeInitialization.class } )
+public class TestSuiteOCLAttributeInitializer {
+}
