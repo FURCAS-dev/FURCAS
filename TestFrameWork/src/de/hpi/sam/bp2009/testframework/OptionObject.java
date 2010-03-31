700436578737f7e9ce09cd0b0669937f1e4eaeff
phberger <phberger@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-03-31 13:46:47 +0000

    Removed deprecated Projects from trunk
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@403 3ca8e212-6386-4b1e-b09d-39f68028012c

--- TestFrameWork/src/de/hpi/sam/bp2009/testframework/OptionObject.java
+++
@@ -1,7 +0,0 @@
-package de.hpi.sam.bp2009.testframework;
-
-public class OptionObject {
-	static public OptionObject defaultValue(){
-		throw new NoSuchMethodError();
-	};
-}
--- TestFrameWork/src/de/hpi/sam/bp2009/testframework/generator/dynamicemf/Generator.java
+++
@@ -1,7 +0,0 @@
-package de.hpi.sam.bp2009.testframework.generator.dynamicemf;
-import de.hpi.sam.bp2009.testframework.OptionObject;
-
-public interface Generator {
-	public org.eclipse.emf.ecore.resource.Resource generate(Object options);
-	public Class<? extends OptionObject> getOptionObjectClass();
-}
