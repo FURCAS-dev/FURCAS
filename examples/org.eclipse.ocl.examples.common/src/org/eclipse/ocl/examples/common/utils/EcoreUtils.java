653e92d5fc057b6926c1aef556f21d9d65a1ba9b
ewillink <ewillink>
2010-04-08 06:27:46 +0000

    Fix copyrights: <copyright> and $Id$, not Eclipse Modeling Project

--- examples/org.eclipse.ocl.examples.common/src/org/eclipse/ocl/examples/common/utils/EcoreUtils.java
+++ examples/org.eclipse.ocl.examples.common/src/org/eclipse/ocl/examples/common/utils/EcoreUtils.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) 2008,2010 Eclipse Modeling Project and others.
+ * Copyright (c) 2008,2010 E.D.Willink and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  * 
  * </copyright>
  *
- * $Id: EcoreUtils.java,v 1.2 2010/03/22 01:09:32 ewillink Exp $
+ * $Id: EcoreUtils.java,v 1.3 2010/04/08 06:27:47 ewillink Exp $
  */
 package org.eclipse.ocl.examples.common.utils;
 
