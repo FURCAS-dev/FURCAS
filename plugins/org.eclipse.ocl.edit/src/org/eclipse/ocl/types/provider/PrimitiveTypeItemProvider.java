c72b3f597f6d58bb10553d06e5cff76d6097c9fc
ewillink <ewillink>
2010-04-08 06:27:38 +0000

    Fix copyrights: <copyright> and $Id$, not Eclipse Modeling Project

--- plugins/org.eclipse.ocl.edit/src/org/eclipse/ocl/types/provider/PrimitiveTypeItemProvider.java
+++ plugins/org.eclipse.ocl.edit/src/org/eclipse/ocl/types/provider/PrimitiveTypeItemProvider.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2009 Eclipse Modeling Project and others.
+ * Copyright (c) 2009 E.D.Willink and others.
  * All rights reserved.   This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: PrimitiveTypeItemProvider.java,v 1.2 2010/01/14 12:44:25 ewillink Exp $
+ * $Id: PrimitiveTypeItemProvider.java,v 1.3 2010/04/08 06:27:40 ewillink Exp $
  */
 package org.eclipse.ocl.types.provider;
 
