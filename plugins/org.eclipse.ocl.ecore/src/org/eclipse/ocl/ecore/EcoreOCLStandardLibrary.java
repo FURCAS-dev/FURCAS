7a7550de7d90d433826c9f520785d53d38f4ee84
ewillink <ewillink>
2010-04-08 06:27:20 +0000

    Fix copyrights: <copyright> and $Id$, not Eclipse Modeling Project

--- plugins/org.eclipse.ocl.ecore/src/org/eclipse/ocl/ecore/EcoreOCLStandardLibrary.java
+++ plugins/org.eclipse.ocl.ecore/src/org/eclipse/ocl/ecore/EcoreOCLStandardLibrary.java
@@ -1,4 +1,6 @@
-/*******************************************************************************
+/**
+ * <copyright>
+ *
  * Copyright (c) 2009 Open Canarias S.L. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +9,11 @@
  *
  * Contributors:
  *     A. Sanchez-Barbudo Herrera - initial API and implementation
- *******************************************************************************/
+ *
+ * </copyright>
+ *
+ * $Id: EcoreOCLStandardLibrary.java,v 1.3 2010/04/08 06:27:21 ewillink Exp $
+ */
 package org.eclipse.ocl.ecore;
 
 import org.eclipse.emf.ecore.EClassifier;
--- plugins/org.eclipse.ocl.ecore/src/org/eclipse/ocl/ecore/internal/OCLEcorePlugin.java
+++ plugins/org.eclipse.ocl.ecore/src/org/eclipse/ocl/ecore/internal/OCLEcorePlugin.java
@@ -1,4 +1,6 @@
-/*******************************************************************************
+/**
+ * <copyright>
+ *
  * Copyright (c) 2005, 2008 IBM Corporation, Zeligsoft Inc., and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -9,9 +11,13 @@
  *     IBM Corporation - initial API and implementation
  *     Zeligsoft - Bug 256040
  * 
- * $Id: OCLEcorePlugin.java,v 1.5 2008/11/24 00:40:47 cdamus Exp $
+ * $Id: OCLEcorePlugin.java,v 1.6 2010/04/08 06:27:21 ewillink Exp $
  * 
- *******************************************************************************/
+ *
+ * </copyright>
+ *
+ * $Id: OCLEcorePlugin.java,v 1.6 2010/04/08 06:27:21 ewillink Exp $
+ */
 package org.eclipse.ocl.ecore.internal;
 
 import org.eclipse.core.runtime.IStatus;
