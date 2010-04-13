d6d1c3e0d64083dc62cd2873b4b57c6fdd39e122
ewillink <ewillink>
2010-04-13 16:04:04 +0000

    Add missing copyrights

--- examples/org.eclipse.ocl.examples.editor.ui/src/org/eclipse/ocl/examples/editor/ui/text/ITextEditorWithUndoContext.java
+++ examples/org.eclipse.ocl.examples.editor.ui/src/org/eclipse/ocl/examples/editor/ui/text/ITextEditorWithUndoContext.java
@@ -1,3 +1,19 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2010 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: ITextEditorWithUndoContext.java,v 1.2 2010/04/13 16:04:04 ewillink Exp $
+ */
 package org.eclipse.ocl.examples.editor.ui.text;
 
 import org.eclipse.core.commands.operations.IUndoContext;
