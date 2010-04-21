cb4aede337d46d7c328209ea0e8d7e4dc336b2bb
phberger <phberger@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-04-21 10:29:29 +0000

    removed model nature from ImpactAnalyzer changed API for IA
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@622 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/ImpactAnalyzer.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/ImpactAnalyzer.java
@@ -6,37 +6,34 @@
  */
 package de.hpi.sam.bp2009.solution.impactAnalyzer;
 
+import java.util.Collection;
+
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.ocl.ecore.Constraint;
+import org.eclipse.ocl.ecore.OCLExpression;
 
 import de.hpi.sam.bp2009.solution.eventManager.EventFilter;
 import de.hpi.sam.bp2009.solution.eventManager.ModelChangeEvent;
 
-/**
- * <!-- begin-user-doc -->
- * A representation of the model object '<em><b>Impact Analyzer</b></em>'.
- * <!-- end-user-doc -->
- *
- *
- * @see de.hpi.sam.bp2009.solution.impactAnalyzer.ImpactAnalyzerPackage#getImpactAnalyzer()
- * @model
- * @generated
- */
-public interface ImpactAnalyzer extends EObject {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @model
-	 * @generated
-	 */
-	EventFilter createFilterForQuery(Constraint query);
+
+public interface ImpactAnalyzer {
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @model
-	 * @generated
+	 * Creates a filter for the given OCL expression, which matches all relevant events for the given expression
+	 * @param query the expression to create the filter for
+	 * @param cls the context of the expression
+	 * @return the filter matching all relevant events
 	 */
-	Constraint getContextObjects(ModelChangeEvent event, Constraint query);
+	EventFilter createFilterForQuery(OCLExpression expression, EClass context);
+
+
+	/**
+	 * Calculates all context objects for an event, which should be starting point of the evaluation of the given query
+	 * @param event the event to calculate for
+	 * @param query the query which should be reevaluated
+	 * @param cls the context of the query
+	 * @return all relevant context objects
+	 */
+	Collection<EObject> getContextObjects(ModelChangeEvent event, OCLExpression expression, EClass context);
 
 } // ImpactAnalyzer
--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/impl/ImpactAnalyzerImpl.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/impl/ImpactAnalyzerImpl.java
@@ -1,31 +1,25 @@
-/**
- * <copyright>
- * </copyright>
- *
- * $Id$
- */
+
 package de.hpi.sam.bp2009.solution.impactAnalyzer.impl;
 
+import java.util.Collection;
+
 import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.impl.EObjectImpl;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.ocl.ecore.Constraint;
+import org.eclipse.ocl.ecore.OCLExpression;
 
 import de.hpi.sam.bp2009.solution.eventManager.EventFilter;
 import de.hpi.sam.bp2009.solution.eventManager.ModelChangeEvent;
 import de.hpi.sam.bp2009.solution.eventManager.impl.InstanceFilterImpl;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.ImpactAnalyzer;
-import de.hpi.sam.bp2009.solution.impactAnalyzer.ImpactAnalyzerPackage;
+
 
 /**
- * <!-- begin-user-doc -->
- * An implementation of the model object '<em><b>Impact Analyzer</b></em>'.
- * <!-- end-user-doc -->
- * <p>
- * </p>
+ * Implementation of the {@link ImpactAnalyzer} 
+ * @author Philipp
  *
- * @generated
  */
-public class ImpactAnalyzerImpl extends EObjectImpl implements ImpactAnalyzer {
+public class ImpactAnalyzerImpl implements ImpactAnalyzer {
 	private class TautologyFilter extends InstanceFilterImpl{
 		@Override
 		public boolean matchesFor(ModelChangeEvent event) {
@@ -33,45 +27,32 @@ public class ImpactAnalyzerImpl extends EObjectImpl implements ImpactAnalyzer {
 		}
 	}
 
-
 	/**
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	protected ImpactAnalyzerImpl() {
-		super();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated NOT
-	 */
-	public EventFilter createFilterForQuery(Constraint query) {
-
-		return new TautologyFilter();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected EClass eStaticClass() {
-		return ImpactAnalyzerPackage.Literals.IMPACT_ANALYZER;
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public Constraint getContextObjects(ModelChangeEvent event, Constraint query) {
+	public Collection<EObject> getContextObjects(ModelChangeEvent event, Constraint query) {
 		// TODO: implement this method
 		// Ensure that you remove @generated or mark it @generated NOT
 		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public EventFilter createFilterForQuery(OCLExpression query, EClass cls) {
+		// TODO Auto-generated methodcreateFilterForQuery stub
+		System.out.println("createFilterForQuery");
+		return new TautologyFilter();
+		
+	}
+
+	@Override
+	public Collection<EObject> getContextObjects(ModelChangeEvent event,
+			OCLExpression query, EClass cls) {
+		// TODO Auto-generated methodgetContextObjects stub
+		System.out.println("getContextObjects");
+		return null;
+		
 	}
 
 } //ImpactAnalyzerImpl
