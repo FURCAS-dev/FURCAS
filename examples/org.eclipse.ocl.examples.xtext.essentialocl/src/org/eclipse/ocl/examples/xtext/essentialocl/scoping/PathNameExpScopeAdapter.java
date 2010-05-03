783310bdf5b718671386bdf02bc82b423481cb43
ewillink <ewillink>
2010-05-03 11:53:57 +0000

    Fix Enum literals

--- examples/org.eclipse.ocl.examples.xtext.essentialocl/src/org/eclipse/ocl/examples/xtext/essentialocl/scoping/PathNameExpScopeAdapter.java
+++ examples/org.eclipse.ocl.examples.xtext.essentialocl/src/org/eclipse/ocl/examples/xtext/essentialocl/scoping/PathNameExpScopeAdapter.java
@@ -12,15 +12,19 @@
  *
  * </copyright>
  *
- * $Id: PathNameExpScopeAdapter.java,v 1.1 2010/05/03 05:38:00 ewillink Exp $
+ * $Id: PathNameExpScopeAdapter.java,v 1.2 2010/05/03 11:53:57 ewillink Exp $
  */
 package org.eclipse.ocl.examples.xtext.essentialocl.scoping;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.ocl.examples.xtext.base.baseCST.BaseCSTPackage;
+import org.eclipse.ocl.examples.xtext.base.baseCST.QualifiedTypeRefCS;
 import org.eclipse.ocl.examples.xtext.base.baseCST.SimplePackageRefCS;
 import org.eclipse.ocl.examples.xtext.base.baseCST.TypeCS;
 import org.eclipse.ocl.examples.xtext.base.scope.AbstractScopeAdapter;
+import org.eclipse.ocl.examples.xtext.base.scope.FilteredAccesses;
+import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.EssentialOCLCSTPackage;
 import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.PathNameExpCS;
 import org.eclipse.ocl.examples.xtext.essentialocl.essentialOCLCST.SimpleNameExpCS;
 import org.eclipse.xtext.resource.IEObjectDescription;
@@ -30,6 +34,21 @@ public class PathNameExpScopeAdapter extends AbstractScopeAdapter<PathNameExpCS>
 {
 	public PathNameExpScopeAdapter(PathNameExpCS csElement) {
 		super(csElement);
+	}
+
+	@Override
+	public void createContents(FilteredAccesses filteredAccesses, EStructuralFeature containmentFeature) {
+		if (containmentFeature == null) {			
+		}
+		else if (filteredAccesses == EssentialOCLCSTPackage.Literals.PATH_NAME_EXP_CS__NAMESPACE) {
+		}
+		else {
+			PathNameExpCS target = getTarget();
+			AbstractScopeAdapter<?> scopeAdapter = AbstractScopeAdapter.getScopeAdapter(target.getNamespace());
+			if (scopeAdapter != null) {
+				scopeAdapter.getInclusiveInheritedContents(filteredAccesses);
+			}
+		}
 	}
 
 	@Override
