e8cbb6646aa2d644a392695da64205504079c44a
ewillink <ewillink>
2010-05-23 10:27:33 +0000

    Sharpen formatting

--- examples/org.eclipse.ocl.examples.xtext.essentialocl/src/org/eclipse/ocl/examples/xtext/essentialocl/formatting/EssentialOCLFormatter.java
+++ examples/org.eclipse.ocl.examples.xtext.essentialocl/src/org/eclipse/ocl/examples/xtext/essentialocl/formatting/EssentialOCLFormatter.java
@@ -12,11 +12,11 @@
  *
  * </copyright>
  *
- * $Id: EssentialOCLFormatter.java,v 1.2 2010/05/03 05:38:40 ewillink Exp $
+ * $Id: EssentialOCLFormatter.java,v 1.3 2010/05/23 10:27:33 ewillink Exp $
  */
 package org.eclipse.ocl.examples.xtext.essentialocl.formatting;
 
-import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
+import org.eclipse.ocl.examples.xtext.essentialocl.services.EssentialOCLGrammarAccess;
 import org.eclipse.xtext.formatting.impl.FormattingConfig;
 
 /**
@@ -27,12 +27,26 @@ import org.eclipse.xtext.formatting.impl.FormattingConfig;
  * 
  * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
  */
-public class EssentialOCLFormatter extends AbstractDeclarativeFormatter {
-	
+public class EssentialOCLFormatter extends AbstractEssentialOCLFormatter
+{
 	@Override
 	protected void configureFormatting(FormattingConfig c) {
-//		EssentialOCLGrammarAccess f = (EssentialOCLGrammarAccess) getGrammarAccess();
 
-		//...
+	    c.setAutoLinewrap(120);
+
+		EssentialOCLGrammarAccess f = (EssentialOCLGrammarAccess)getGrammarAccess();	
+		configureCollectionLiteralExpCS(c, f.getCollectionLiteralExpCSAccess());
+		configureCollectionTypeCS(c, f.getCollectionTypeCSAccess());
+		configureIfExpCS(c, f.getIfExpCSAccess());
+		configureLetExpCS(c, f.getLetExpCSAccess());
+	    configureNavigationExpCS(c, f.getNavigationExpCSAccess());
+	    configureNavigatingExpCS(c, f.getNavigatingExpCSAccess());
+		configurePathNameExpCS(c, f.getPathNameExpCSAccess());
+		configurePreExpCS(c, f.getPreExpCSAccess());
+	    configureRoundBracketExpCS(c, f.getRoundBracketExpCSAccess());
+	    configureSquareBracketExpCS(c, f.getSquareBracketExpCSAccess());
+		configureSubNavigationExpCS(c, f.getSubNavigationExpCSAccess());
+	    configureTupleLiteralExpCS(c, f.getTupleLiteralExpCSAccess());
+	    configureTupleTypeCS(c, f.getTupleTypeCSAccess());
 	}
 }
