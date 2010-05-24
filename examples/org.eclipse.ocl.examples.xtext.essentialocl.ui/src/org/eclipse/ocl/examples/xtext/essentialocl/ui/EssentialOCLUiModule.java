1f92b142cf966cd712b813c6ce165b13a19cdbbf
ewillink <ewillink>
2010-05-24 19:36:33 +0000

    Add cross-reference syntax highlighting

--- examples/org.eclipse.ocl.examples.xtext.essentialocl.ui/src/org/eclipse/ocl/examples/xtext/essentialocl/ui/EssentialOCLUiModule.java
+++ examples/org.eclipse.ocl.examples.xtext.essentialocl.ui/src/org/eclipse/ocl/examples/xtext/essentialocl/ui/EssentialOCLUiModule.java
@@ -12,11 +12,15 @@
  *
  * </copyright>
  *
- * $Id: EssentialOCLUiModule.java,v 1.1 2010/04/13 06:33:11 ewillink Exp $
+ * $Id: EssentialOCLUiModule.java,v 1.2 2010/05/24 19:36:33 ewillink Exp $
  */
 package org.eclipse.ocl.examples.xtext.essentialocl.ui;
 
+import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.EssentialOCLHighlightingConfiguration;
+import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.EssentialOCLSemanticHighlightingCalculator;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -24,5 +28,13 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class EssentialOCLUiModule extends org.eclipse.ocl.examples.xtext.essentialocl.ui.AbstractEssentialOCLUiModule {
 	public EssentialOCLUiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+
+	public Class<? extends ISemanticHighlightingCalculator> bindISemanticHighlightingCalculator() {
+		return EssentialOCLSemanticHighlightingCalculator.class;
+	}
+
+	public Class<? extends IHighlightingConfiguration> bindIHighlightingConfiguration() {
+		return EssentialOCLHighlightingConfiguration.class;
 	}
 }
