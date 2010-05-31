c424e964fea8ea4865ea9a8031061e961147a517
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-05-31 15:57:21 +0000

    Added a placeholder for quickly parsing, debugging and evaluating OCL expressions
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@783 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/QuickOclParseAndEvalTest.java
@@ -0,0 +1,26 @@
+package de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.tests;
+
+import junit.framework.TestCase;
+
+import org.eclipse.ocl.ParserException;
+import org.eclipse.ocl.ecore.OCL;
+import org.eclipse.ocl.ecore.OCL.Helper;
+import org.eclipse.ocl.ecore.OCLExpression;
+import org.junit.Test;
+
+import data.classes.ClassesFactory;
+import data.classes.ClassesPackage;
+import data.classes.SapClass;
+
+public class QuickOclParseAndEvalTest extends TestCase {
+	@Test
+	public void testParseAndEvaluateOclExpression() throws ParserException {
+		OCL ocl = OCL.newInstance();
+		Helper oclHelper = ocl.createOCLHelper();
+		oclHelper.setContext(ClassesPackage.eINSTANCE.getSapClass());
+		OCLExpression expression = oclHelper.createQuery("self.ownedSignatures.input");
+		SapClass context = ClassesFactory.eINSTANCE.createSapClass();
+		Object result = ocl.evaluate(context, expression);
+		assertNotNull(result);
+	}
+}
