d7bbe4eb30f29344b64d349b6fe2f54a26bd5b00
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-01 07:55:23 +0000

    a different example
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@789 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/QuickOclParseAndEvalTest.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/QuickOclParseAndEvalTest.java
@@ -18,7 +18,7 @@ public class QuickOclParseAndEvalTest extends TestCase {
 		OCL ocl = OCL.newInstance();
 		Helper oclHelper = ocl.createOCLHelper();
 		oclHelper.setContext(ClassesPackage.eINSTANCE.getMethodSignature());
-		OCLExpression expression = oclHelper.createQuery("self.owner.oclAsType(data::classes::TypeAdapter).to.oclIsInvalid()");
+		OCLExpression expression = oclHelper.createQuery("self.owner.oclAsType(data::classes::TypeAdapter).oclIsInvalid()");
 		MethodSignature context = ClassesFactory.eINSTANCE.createMethodSignature();
 		Object result = ocl.evaluate(context, expression);
 		assertNotNull(result);
