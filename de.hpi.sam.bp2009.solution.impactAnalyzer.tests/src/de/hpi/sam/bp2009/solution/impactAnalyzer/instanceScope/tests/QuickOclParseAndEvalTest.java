31eda98d20d19a4f3c300c420fef080412660caf
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-07 11:30:44 +0000

    more sophisticated test around ->collect behavior for null values
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@879 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/QuickOclParseAndEvalTest.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/QuickOclParseAndEvalTest.java
@@ -8,19 +8,34 @@ import org.eclipse.ocl.ecore.OCL.Helper;
 import org.eclipse.ocl.ecore.OCLExpression;
 import org.junit.Test;
 
+import data.classes.ClassTypeDefinition;
 import data.classes.ClassesFactory;
 import data.classes.ClassesPackage;
 import data.classes.MethodSignature;
+import data.classes.Parameter;
+import data.classes.SapClass;
 
 public class QuickOclParseAndEvalTest extends TestCase {
 	@Test
 	public void testParseAndEvaluateOclExpression() throws ParserException {
-		OCL ocl = OCL.newInstance();
-		Helper oclHelper = ocl.createOCLHelper();
-		oclHelper.setContext(ClassesPackage.eINSTANCE.getMethodSignature());
-		OCLExpression expression = oclHelper.createQuery("self.name->isEmpty()");
+                SapClass class1 = ClassesFactory.eINSTANCE.createSapClass();
+                class1.setName("class1");
+                SapClass class2 = ClassesFactory.eINSTANCE.createSapClass();
+                class2.setName("class2");
 		MethodSignature context = ClassesFactory.eINSTANCE.createMethodSignature();
-		Object result = ocl.evaluate(context, expression);
+		context.setName("context");
+		Parameter param = ClassesFactory.eINSTANCE.createParameter();
+		param.setName("p");
+		ClassTypeDefinition ctd = ClassesFactory.eINSTANCE.createClassTypeDefinition();
+		ctd.setClazz(class1);
+		param.setOwnedTypeDefinition(ctd);
+		context.setOwner(class2);
+		
+                OCL ocl = OCL.newInstance();
+                Helper oclHelper = ocl.createOCLHelper();
+                oclHelper.setContext(ClassesPackage.eINSTANCE.getParameter());
+                OCLExpression expression = oclHelper.createQuery("self.ownedTypeDefinition.clazz");
+		Object result = ocl.evaluate(param, expression);
 		assertNotNull(result);
 	}
 }
