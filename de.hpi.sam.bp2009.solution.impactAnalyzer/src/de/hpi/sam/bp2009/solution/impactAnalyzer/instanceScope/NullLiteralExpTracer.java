df08556f17897213caa8b50e6855b93d4945048a
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-09 22:06:56 +0000

    added tracer for null and invalid literals
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@903 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/NullLiteralExpTracer.java
@@ -0,0 +1,9 @@
+package de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope;
+
+import org.eclipse.ocl.ecore.NullLiteralExp;
+
+public class NullLiteralExpTracer extends AbstractTracer<NullLiteralExp> {
+	public NullLiteralExpTracer(NullLiteralExp expression) {
+		super(expression);
+	}
+}
