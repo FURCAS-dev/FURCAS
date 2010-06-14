81332be8204d82f15591e603bf78c1d7e13ee748
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-14 13:26:23 +0000

    allow clients to pass an @pre notification that indicates that the partial evaluation has to take place on the pre-state of the model; not fully implemented yet
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@926 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/deltaPropagation/PartialEvaluationVisitorImpl.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/deltaPropagation/PartialEvaluationVisitorImpl.java
@@ -1,8 +1,12 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.deltaPropagation;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EClassifier;
@@ -23,6 +27,7 @@ import org.eclipse.ocl.ecore.Constraint;
 import org.eclipse.ocl.ecore.SendSignalAction;
 import org.eclipse.ocl.expressions.AssociationClassCallExp;
 import org.eclipse.ocl.expressions.BooleanLiteralExp;
+import org.eclipse.ocl.expressions.CollectionKind;
 import org.eclipse.ocl.expressions.CollectionLiteralExp;
 import org.eclipse.ocl.expressions.EnumLiteralExp;
 import org.eclipse.ocl.expressions.IfExp;
@@ -45,20 +50,29 @@ import org.eclipse.ocl.expressions.TypeExp;
 import org.eclipse.ocl.expressions.UnlimitedNaturalLiteralExp;
 import org.eclipse.ocl.expressions.UnspecifiedValueExp;
 import org.eclipse.ocl.expressions.VariableExp;
+import org.eclipse.ocl.types.CollectionType;
+import org.eclipse.ocl.util.CollectionUtil;
 
 import de.hpi.sam.bp2009.solution.impactAnalyzer.impl.ImpactAnalyzerPlugin;
 
 /**
- * When a {@link ValueNotFoundException} occurs during evaluating an expression, it is not caught, logged ans
- * swallowed but forwarded to the caller.<p>
+ * When a {@link ValueNotFoundException} occurs during evaluating an expression, it is not caught, logged ans swallowed but
+ * forwarded to the caller.
+ * <p>
  * 
- * All <tt>visit...</tt> operations check if the expression to evaluate is the {@link #sourceExpression} passed
- * to the constructor. If so, instead of actually evaluating the expression, the {@link #valueOfSourceExpression}
- * object is returned which was also passed to the constructor. This allows for partial evaluation of any
- * {@link CallExp} with a given value for the source expression.
+ * All <tt>visit...</tt> operations check if the expression to evaluate is the {@link #sourceExpression} passed to the
+ * constructor. If so, instead of actually evaluating the expression, the {@link #valueOfSourceExpression} object is returned
+ * which was also passed to the constructor. This allows for partial evaluation of any {@link CallExp} with a given value for the
+ * source expression.
+ * <p>
+ * 
+ * FIXME Issue with recursive operations may exist: if the sourceExpression belongs to the body of a recursive operation and
+ * evaluating it performs a recursive call and the source expression may depend on operation parameters or the self object then
+ * the sourceExpressionValue must not be used during recursive evaluation. First produce this error in a failing test case, then
+ * fix.
  * 
  * @author Axel Uhl
- *
+ * 
  */
 public class PartialEvaluationVisitorImpl
         extends
@@ -66,14 +80,32 @@ public class PartialEvaluationVisitorImpl
     private org.eclipse.ocl.ecore.OCLExpression sourceExpression;
     private Object valueOfSourceExpression;
 
+    /**
+     * a {@link Notification} object such that an evaluation will be based on the state *before* the notification. For example, if
+     * the notification indicates the removal of a reference from an element <tt>e1</tt> to an element <tt>e2</tt> across
+     * reference <tt>r</tt> then when during partial evaluation <tt>r</tt> is traversed starting from <tt>e1</tt> then <tt>e2</tt>
+     * will show in the results although in the current version of the model it would not. If <tt>null</tt>, the evaluator will
+     * evaluate expressions on the model as is.
+     */
+    private Notification atPre;
+
+    /**
+     * @param atPre
+     *            a {@link Notification} object such that an evaluation will be based on the state *before* the notification. For
+     *            example, if the notification indicates the removal of a reference from an element <tt>e1</tt> to an element
+     *            <tt>e2</tt> across reference <tt>r</tt> then when during partial evaluation <tt>r</tt> is traversed starting
+     *            from <tt>e1</tt> then <tt>e2</tt> will show in the results although in the current version of the model it would
+     *            not. If <tt>null</tt>, the evaluator will evaluate expressions on the model as is.
+     */
     public PartialEvaluationVisitorImpl(
             Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
             EvaluationEnvironment<EClassifier, EOperation, EStructuralFeature, EClass, EObject> evalEnv,
-            Map<? extends EClass, ? extends Set<? extends EObject>> extentMap, org.eclipse.ocl.ecore.OCLExpression sourceExpression,
-            Object valueOfSourceExpression) {
+            Map<? extends EClass, ? extends Set<? extends EObject>> extentMap,
+            org.eclipse.ocl.ecore.OCLExpression sourceExpression, Object valueOfSourceExpression, Notification atPre) {
         super(env, evalEnv, extentMap);
         this.sourceExpression = sourceExpression;
         this.valueOfSourceExpression = valueOfSourceExpression;
+        this.atPre = atPre;
     }
 
     @Override
@@ -121,7 +153,77 @@ public class PartialEvaluationVisitorImpl
         if (pc == sourceExpression) {
             return valueOfSourceExpression;
         }
-        return super.visitPropertyCallExp(pc);
+
+        /*
+         * These are the sources of the super implementation copied here which is ugly. We only want to get access to the value of
+         * the source expression because it may be needed for the comparison with the atPre event later.
+         * 
+         * TODO evaluate source expression here and cache the result before delegating to super.visitPropertyCallExp; when then
+         * super.visitPropertyCallExp asks the visitor to evaluate the source expression, the result is taken from the cache
+         */
+        EStructuralFeature property = pc.getReferredProperty();
+        OCLExpression<EClassifier> source = pc.getSource();
+        // evaluate source
+        Object context = source.accept(getVisitor());
+        Object localResult;
+        // if source is undefined, result is OclInvalid
+        if (isUndefined(context)) {
+            localResult = getInvalid();
+        } else {
+            OCLExpression<EClassifier> derivation = getPropertyBody(property);
+            if (derivation != null) {
+                // this is an additional property
+                localResult = navigate(property, derivation, context);
+            } else {
+                List<Object> qualifiers;
+                if (pc.getQualifier().isEmpty()) {
+                    qualifiers = Collections.emptyList();
+                } else {
+                    // handle qualified association navigation
+                    qualifiers = new java.util.ArrayList<Object>();
+                    for (OCLExpression<EClassifier> q : pc.getQualifier()) {
+                        qualifiers.add(q.accept(getVisitor()));
+                    }
+                }
+                localResult = getEvaluationEnvironment().navigateProperty(property, qualifiers, context);
+                if ((pc.getType() instanceof CollectionType<?, ?>) && !(result instanceof Collection<?>)) {
+                    // this was an XSD "unspecified multiplicity". Now that we know what
+                    // the multiplicity is, we can coerce it to a collection value
+                    @SuppressWarnings("unchecked")
+                    CollectionKind kind = ((CollectionType<EClassifier, EOperation>) pc.getType()).getKind();
+                    Collection<Object> collection = CollectionUtil.createNewCollection(kind);
+                    collection.add(localResult);
+                    localResult = collection;
+                }
+            }
+        }
+        
+        if (atPre != null && atPre.getNotifier() != null && atPre.getNotifier() == context && pc == atPre.getFeature()) {
+            // evaluate property call based on the model state that existed before the change
+            // described by the atPre notification
+            switch (atPre.getEventType()) {
+            case Notification.ADD:
+                // if the addition operated on the result of the source expression, remove the element from the local results
+                // again
+            case Notification.ADD_MANY:
+                // if the addition operated on the result of the source expression, remove the elements from the local results
+                // again
+            case Notification.MOVE:
+                // if the move operated on the result of the source expression, move the element back to the old index
+                // indicated by getOldIntValue()
+            case Notification.REMOVE:
+                // if the removal operated on the result of the source expression, add the element back at the index provided
+            case Notification.REMOVE_MANY:
+                // if the removal operated on the result of the source expression, add the elements back in the order provided by
+                // the getOldValue() collection
+            case Notification.SET:
+            case Notification.UNSET:
+                // if the setting/unsetting operated on the result of the source expression, return the old value
+            default:
+                throw new RuntimeException("Don't understand @pre notification " + atPre);
+            }
+        }
+        return localResult;
     }
 
     @Override
