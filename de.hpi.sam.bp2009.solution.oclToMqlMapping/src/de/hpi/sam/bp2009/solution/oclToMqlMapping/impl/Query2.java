f3ad747862115caa0ab8d07b60f517bd185af8c3
theaschroeter <theaschroeter@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-17 12:59:35 +0000

    fixed tearDown in testcase, fixed result of collect(attribute)
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@956 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.oclToMqlMapping/src/de/hpi/sam/bp2009/solution/oclToMqlMapping/impl/Query2.java
+++ de.hpi.sam.bp2009.solution.oclToMqlMapping/src/de/hpi/sam/bp2009/solution/oclToMqlMapping/impl/Query2.java
@@ -1,12 +1,16 @@
 package de.hpi.sam.bp2009.solution.oclToMqlMapping.impl;
 
+import java.lang.reflect.Field;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
@@ -37,10 +41,18 @@ import de.hpi.sam.bp2009.solution.scopeProvider.impl.ProjectBasedScopeProviderIm
 @SuppressWarnings("restriction")
 public class Query2  {
 
-
+/**
+ * buildMqlQuery prepares for each Predefined type of the Iterator Expression a matching Query 2
+ * after mapping all parts of the Expression, the query 2 will be executed
+ * @param allO a Set<EObject> for Setting the Query Context
+ * @param ocType an Object for building the scope for Query 2
+ * @param body is the OCLExpression, this must be the body of the IteratorExp for mapping all parts of the Expression to Query 2
+ * @param ie is the IteratorExp, needed to defined the different mapping for each Predefined Type 
+ * @param mapev is the Mapping Evaluation Visitor, needed to instantiate a second Visitor to map the expression
+ * @return result
+ */
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public static Object buildMqlQuery(Set<EObject> allO,  Object ocType, OCLExpression body, IteratorExp ie, MappingEvaluationVisitor<?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?> mapev){
-        //TODO other query for each predefinded type
         Object result = null;
         MqlMapperToString<?, EClassifier, EOperation, EStructuralFeature, ?, ?, ?, ?, ?, Constraint, EClass, EObject> mmts ;
         mmts = new MqlMapperToString(mapev.getEnvironment(), mapev.getEvaluationEnvironment(), mapev.getExtentMap());
@@ -89,39 +101,39 @@ public class Query2  {
             boolean test5 =match5.find();
             boolean test6 =match6.find();
             if(test && test2 && test3){
-                
-                    String nav = stringBody.substring(match.start(1), stringBody.indexOf("***"));
-                    String cond = stringBody.substring(stringBody.indexOf("***")+3);
-                    String ali = stringBody.substring(match3.start(1), stringBody.indexOf("."));
-                    if (body instanceof OperationCallExp<?, ?>){
 
-                        Object prop = ((PropertyCallExp) ((PropertyCallExp) ((OperationCallExp<?, ?>) body).getSource()).getSource()).getReferredProperty();
-                        if(prop instanceof EReference){
-                            Object propType = ((EReference) prop).getEType();
-                            EClassifier eclass2 = (EClassifier) propType;
-                            URI uri2=EcoreUtil.getURI(eclass2);
+                String nav = stringBody.substring(match.start(1), stringBody.indexOf("***"));
+                String cond = stringBody.substring(stringBody.indexOf("***")+3);
+                String ali = stringBody.substring(match3.start(1), stringBody.indexOf("."));
+                if (body instanceof OperationCallExp<?, ?>){
 
-                            try{
-                                //if the body is a navigation
-                                resultSet = queryProcessor.execute("select "+ali+" from  [" + uri1 + //$NON-NLS-1$
-                                        "] as "+ali+" where "+nav+" in ( select p2 from [" + uri2+"] as p2 where p2." + cond+")", queryContext); //$NON-NLS-1$
-                            }
-                            catch (EvaluationHaltedException e) {
-                                // evaluation stopped on demand, propagate further
-                                throw e;
-                            } catch (RuntimeException e) {
-                                result=null;
-                            }
-                            finally{
-                                result = buildResult(resultSet, queryContext, ali);
-                                }
+                    Object prop = ((PropertyCallExp) ((PropertyCallExp) ((OperationCallExp<?, ?>) body).getSource()).getSource()).getReferredProperty();
+                    if(prop instanceof EReference){
+                        Object propType = ((EReference) prop).getEType();
+                        EClassifier eclass2 = (EClassifier) propType;
+                        URI uri2=EcoreUtil.getURI(eclass2);
 
-                        }}}
+                        try{
+                            //select over body with navigation
+                            resultSet = queryProcessor.execute("select "+ali+" from  [" + uri1 + //$NON-NLS-1$
+                                    "] as "+ali+" where "+nav+" in ( select p2 from [" + uri2+"] as p2 where p2." + cond+")", queryContext); //$NON-NLS-1$
+                        }
+                        catch (EvaluationHaltedException e) {
+                            // evaluation stopped on demand, propagate further
+                            throw e;
+                        } catch (RuntimeException e) {
+                            result=null;
+                        }
+                        finally{
+                            result = buildResult(resultSet, queryContext, ali);
+                        }
+
+                    }}}
             else if(test3 &&!test2 &&!test6){
-                
+
                 String ali = stringBody.substring(match3.start(1), stringBody.indexOf("."));
                 try{
-                    //if the body is a comparison between an attribute and a value
+                    //select over body with condition
                     resultSet = queryProcessor.execute("select "+ ali+" from  [" + uri1 + //$NON-NLS-1$
                             "] as "+ali+" where " + stringBody, queryContext); //$NON-NLS-1$
                 }
@@ -133,15 +145,15 @@ public class Query2  {
                 }
                 finally{
                     result = buildResult(resultSet, queryContext, ali);
-                    }
+                }
             }
             else if(test4 && test5 ){
-                
+
                 String ali = stringBody.substring(match4.start(1), stringBody.indexOf("."));
                 String nav = stringBody.substring(match5.end(), stringBody.indexOf("***"));
                 String cond = stringBody.substring(stringBody.indexOf("."),match5.end());
                 String cond2=stringBody.substring(stringBody.indexOf("***")+3);
-                
+
                 if (body instanceof OperationCallExp<?, ?>){
                     List<OCLExpression> args = ((OperationCallExp) body).getArgument();
                     OCLExpression arg = args.get(0);
@@ -152,31 +164,31 @@ public class Query2  {
                         EClassifier eclass2 = (EClassifier) propType;
                         URI uri2=EcoreUtil.getURI(eclass2);
 
-                try{
-                    //if the body is a comparison with navigation, wrong result
-                    resultSet = queryProcessor.execute("select "+ ali+" from  [" + uri1 + //$NON-NLS-1$
-                            "] as "+ali+" ,["+uri2+"] as p3 where " +nav+"=p3 where "+ali+ cond+ "p3."+ cond2, queryContext); //$NON-NLS-1$
-                }
-                catch (EvaluationHaltedException e) {
-                    // evaluation stopped on demand, propagate further
-                    throw e;
-                } catch (RuntimeException e) {
-                    result=null;
-                }
-                finally{
-                    result = buildResult(resultSet, queryContext, ali);
-                    }
-            }}
-               
-                }
+                        try{
+                            //select over body with a comparison with navigation
+                            resultSet = queryProcessor.execute("select "+ ali+" from  [" + uri1 + //$NON-NLS-1$
+                                    "] as "+ali+" ,["+uri2+"] as p3 where " +nav+"=p3 where "+ali+ cond+ "p3."+ cond2, queryContext); //$NON-NLS-1$
+                        }
+                        catch (EvaluationHaltedException e) {
+                            // evaluation stopped on demand, propagate further
+                            throw e;
+                        } catch (RuntimeException e) {
+                            result=null;
+                        }
+                        finally{
+                            result = buildResult(resultSet, queryContext, ali);
+                        }
+                    }}
+
+            }
             else if(test6 && test3){
                 String ali = stringBody.substring(match6.start(1), stringBody.indexOf("."));
                 String cond= stringBody.replaceFirst(alias,"p.");
-//                cond= cond.replaceFirst("e2", ali);
-                
+                //                cond= cond.replaceFirst("e2", ali);
+
                 try{
-                   
-                    //if the body is a comparison without navigation, wrong query
+
+                    //select over body with a comparison without navigation 
                     resultSet = queryProcessor.execute("select "+ ali+" from [" + uri1 + //$NON-NLS-1$
                             "] as p , ["+uri1+"] as "+ali+" where "+ali+"=p where "+cond, queryContext); //$NON-NLS-1$
                 }
@@ -188,13 +200,9 @@ public class Query2  {
                 }
                 finally{
                     result = buildResult(resultSet, queryContext, ali);
-                    }}
+                }}
             return result;
         }
-
-
-
-
 
         case 205:{
             //any
@@ -207,29 +215,28 @@ public class Query2  {
             QueryContext queryContext = setQueryContext(allO);
             ResultSet resultSet=null;
             QueryProcessor queryProcessor = setQueryProcessor();
-//            String regex="([0-9A-Za-z]+)\\.([0-9A-Za-z]+)";//regex collect (Attribute) body
-            
+
             String alias = "([0-9A-Za-z]{1}\\.)";
-//            Pattern pattern=Pattern.compile(regex);
-            
+
+
             Pattern pattern3 = Pattern.compile(alias);
-//            Matcher match=pattern.matcher(stringBody);
+
             Matcher match3=pattern3.matcher(stringBody);
-//            boolean test1 = match.find();
-//            boolean test2 = match2.find();
             match3.find();
             if (body instanceof PropertyCallExp<?, ?>){
 
                 Object prop = ((PropertyCallExp) body).getReferredProperty();
                 if(prop instanceof EReference){
+                    //collect over an association
                     Object propType = ((EReference) prop).getEType();
                     EClassifier eclass2 = (EClassifier) propType;
                     URI uri2=EcoreUtil.getURI(eclass2);
                     String ali = stringBody.substring(match3.start(1), stringBody.indexOf("."));
-                    //TODO delete "with" and "in assoc", compare with ebnf for right mql
+                    String alia= "a2";
+                    
                     try{
-                        resultSet = queryProcessor.execute("select "+ali+" from  [" +uri1+ //$NON-NLS-1$
-                                "] as "+ali+" , [" +uri2+ "] as a2 where " +stringBody+"=a2" , queryContext); //$NON-NLS-1$
+                        resultSet = queryProcessor.execute("select "+alia+" from  [" + uri1 + //$NON-NLS-1$
+                                "] as "+ali+" , [" +uri2+ "] as a2 where "+stringBody+" in ( select p2 from [" + uri2+"] as p2)", queryContext);
                     }
                     catch (EvaluationHaltedException e) {
                         // evaluation stopped on demand, propagate further
@@ -238,13 +245,42 @@ public class Query2  {
                         result=null;
                     }
                     finally{
-                        result = buildResult(resultSet, queryContext, ali);
+                      //FIXME missing the number of associations, in ocl the result is sth like 
+                        //{null=5, company\\.impl\\.DivisionImpl@([0-9A-Za-z]+) \\(name: Div1, budget: 1234567\\)=1}
+//                        result = buildResult(resultSet, queryContext, ali);
+                        if(!resultSet.isEmpty()){
+                            Collection<EObject> col= new HashSet<EObject>();
+                            for(int i=0; i<resultSet.getSize();i++){
+                                col.add(queryContext.getResourceSet().getEObject(resultSet.getUri(i, alia), /* loadOnDemand */true)); //$NON-NLS-1$
+
+                            }
+                            Object obj = col;
+                            int count = 1;
+                            Map<Object, Integer> map= new HashMap<Object,Integer>();
+                            map.put(obj,count);
+                            for(int i = 0;i<resultSet.getSize();i++){
+                               URI[] uris = resultSet.getUris(alia);
+                               for(int j=0;j<uris.length;j++){
+                                   URI uri = uris[j];
+                                   System.out.println(uri);
+                               }
+                               
+                                }
+                            
+                            result=col;
+                            System.out.println("successfull");
                         }
+                        else {
+                            Map<Object, Integer> col= new HashMap<Object,Integer>();
+
+                            result = col;
+                        }
+                    }
 
                 }else if(prop instanceof EAttribute){
                     String ali = stringBody.substring(match3.start(1), stringBody.indexOf("."));
                     try{
-                        //TODO change result
+                        //collect over  an attribute
                         resultSet = queryProcessor.execute("select "+ stringBody+" from  [" + uri1 + //$NON-NLS-1$
                                 "] as "+ali, queryContext); //$NON-NLS-1$
                     }
@@ -255,18 +291,30 @@ public class Query2  {
                         result=null;
                     }
                     finally{
-                        if(!resultSet.isEmpty()){
-                            Collection<EObject> col= new HashSet<EObject>();
-                            for(int i=0; i<resultSet.getSize();i++){
-                                col.add((EObject) resultSet); //$NON-NLS-1$
 
+                        if(!resultSet.isEmpty()){
+                            int count = 1;
+                            Map<Object, Integer> col= new HashMap<Object,Integer>();
+                            for(int i = 0;i<resultSet.getSize();i++){
+                                Object value=null;
+                                Object[] attribute = resultSet.getAttributes(i, ali);
+                                for (int j=0; j<attribute.length;j++){
+                                    value =attribute[j];
+                                }
+                                if(col.containsKey(value)){
+                                    count++;  
+                                    col.put(value,count);}
+                                else{
+                                    count=1;
+                                    col.put(value,count);
+                                }
                             }
-                            result = col;
+                            result=col;
                             System.out.println("successfull");
                         }
                         else {
-                            Collection<EObject> col= new HashSet<EObject>();
-                            
+                            Map<Object, Integer> col= new HashMap<Object,Integer>();
+
                             result = col;
                         }
                     } 
@@ -298,35 +346,35 @@ public class Query2  {
 
 
     }
-    
+
     private static Object buildResult(ResultSet resultSet, QueryContext queryContext, String ali){
         Object result;
-    if(!resultSet.isEmpty()){
-        Collection<EObject> col= new HashSet<EObject>();
-        for(int i=0; i<resultSet.getSize();i++){
-            col.add(queryContext.getResourceSet().getEObject(resultSet.getUri(i, ali), /* loadOnDemand */true)); //$NON-NLS-1$
+        if(!resultSet.isEmpty()){
+            Collection<EObject> col= new HashSet<EObject>();
+            for(int i=0; i<resultSet.getSize();i++){
+                col.add(queryContext.getResourceSet().getEObject(resultSet.getUri(i, ali), /* loadOnDemand */true)); //$NON-NLS-1$
 
+            }
+            result = col;
+            System.out.println("successfull");
         }
-        result = col;
-        System.out.println("successfull");
+        else {
+            Collection<EObject> col= new HashSet<EObject>();
+
+            result = col;
+        }
+        return result;
     }
-    else {
-        Collection<EObject> col= new HashSet<EObject>();
-        
-        result = col;
-    }
-    return result;
-    }
-    
+
     private static QueryContext setQueryContext(Set<EObject> allO){
         ScopeProvider pro = new ProjectBasedScopeProviderImpl(allO.toArray(new EObject[allO.size()]));
         QueryContext queryContext = pro.getForwardScopeAsQueryContext();
-    return queryContext;
+        return queryContext;
     }
     private static QueryProcessor setQueryProcessor(){
         Index DEFAULT_INDEX = null;
         DEFAULT_INDEX = new PageableIndexImpl(Options.PAGING_AND_DUMPING_DISABLED);
         QueryProcessor queryProcessor = new QueryProcessorImpl(DEFAULT_INDEX);
-    return queryProcessor;
+        return queryProcessor;
     }
 }
