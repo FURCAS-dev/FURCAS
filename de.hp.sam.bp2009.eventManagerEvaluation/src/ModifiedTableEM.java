319804e45c1480764a4dfed6c61f888814106395
phberger <phberger@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-22 07:29:00 +0000

    Initial event manager runtimetest impl Creates a csv file
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@987 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ de.hp.sam.bp2009.eventManagerEvaluation/src/ModifiedTableEM.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.io.Writer;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+
+import de.hpi.sam.bp2009.solution.eventManager.filters.EventFilter;
+import de.hpi.sam.bp2009.solution.eventManager.framework.EventManagerTableBased;
+
+/**
+ * 
+ */
+
+/**
+ * @author Philipp
+ *
+ */
+public class ModifiedTableEM extends EventManagerTableBased {
+
+    private long startHandleEvent =0L;
+    private Writer writer;
+
+    /**
+     * @param set
+     * @param writer 
+     */
+    public ModifiedTableEM(ResourceSet set, Writer writer) {
+        super(set);
+        this.writer= writer;
+        // TODO Auto-generated constructor stub
+    }
+
+    /* (non-Javadoc)
+     * @see de.hpi.sam.bp2009.solution.eventManager.framework.EventManagerTableBased#notifyApplication(org.eclipse.emf.common.notify.Adapter, org.eclipse.emf.common.notify.Notification, de.hpi.sam.bp2009.solution.eventManager.filters.EventFilter)
+     */
+    @Override
+    public void notifyApplication(Adapter application, Notification msg, EventFilter matchingFilter) {
+//        System.out.println("Event to Notification, " + (System.nanoTime()-startHandleEvent));
+        super.notifyApplication(application, msg, matchingFilter);
+        
+    }
+
+    /* (non-Javadoc)
+     * @see de.hpi.sam.bp2009.solution.eventManager.framework.EventManagerTableBased#handleEMFEvent(org.eclipse.emf.common.notify.Notification)
+     */
+    @Override
+    public void handleEMFEvent(Notification notification) {
+        if(notifierByListener.isEmpty()){
+            return;
+        }
+        this.startHandleEvent=System.nanoTime();
+        super.handleEMFEvent(notification);        
+        try {
+            writer.write((System.nanoTime()-startHandleEvent)+",");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        
+    }
+
+    /* (non-Javadoc)
+     * @see de.hpi.sam.bp2009.solution.eventManager.framework.EventManagerTableBased#unsubscribe(org.eclipse.emf.common.notify.Adapter)
+     */
+    @Override
+    public boolean unsubscribe(Adapter caller) {
+        // TODO Auto-generated methodunsubscribe stub
+        return super.unsubscribe(caller);
+        
+    }
+    
+}
