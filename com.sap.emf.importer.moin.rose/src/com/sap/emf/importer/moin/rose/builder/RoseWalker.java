b6db9dcb917279d9d02aad6733977f70c033d031
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-24 22:49:04 +0000

    An adjusted EMF Ecore Rose importer that handles the specificities of the Unisys/SAP MOIN profile
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1023 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ com.sap.emf.importer.moin.rose/src/com/sap/emf/importer/moin/rose/builder/RoseWalker.java
@@ -0,0 +1,71 @@
+/**
+ * <copyright> 
+ *
+ * Copyright (c) 2002-2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: RoseWalker.java,v 1.3 2006/12/28 06:56:06 marcelop Exp $
+ */
+package com.sap.emf.importer.moin.rose.builder;
+
+import java.util.List;
+
+import com.sap.emf.importer.moin.rose.parser.RoseNode;
+
+
+/**
+ * Traverses the Rose file a second time to create mapping object.
+ */
+public class RoseWalker
+{
+  private RoseNode roseNode;
+  private RoseVisitor visitor;
+
+  public RoseWalker(RoseNode roseNode)
+  {
+    this.roseNode = roseNode;
+  }
+
+  public void traverse(RoseVisitor visitor)
+  {
+    this.visitor = visitor;
+    traverseObject(roseNode);
+  }
+
+  private void traverseObject(RoseNode roseNode)
+  {
+    visitor.visitObject(roseNode);
+    traverse(roseNode);
+  }
+
+  private void traverseList(RoseNode roseNode)
+  {
+    visitor.visitList(roseNode);
+    traverse(roseNode);
+  }
+
+  private void traverse(RoseNode roseNode)
+  {
+    List<RoseNode> nodes = roseNode.getNodes();
+    for (int i = 0; i < nodes.size(); i++)
+    {
+      RoseNode node = nodes.get(i);
+      if (node.getRoseNodeType() == RoseNode.OBJECT)
+      {
+        traverseObject(node);
+      }
+      else if (node.getRoseNodeType() == RoseNode.LIST)
+      {
+        traverseList(node);
+      }
+    }
+  }
+}
---
+++ com.sap.emf.importer.moin.rose/src/com/sap/emf/importer/moin/rose/parser/RoseLoader.java
@@ -0,0 +1,176 @@
+/**
+ * <copyright> 
+ *
+ * Copyright (c) 2002-2006 IBM Corporation and others.
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 
+ *   IBM - Initial API and implementation
+ *
+ * </copyright>
+ *
+ * $Id: RoseLoader.java,v 1.7 2006/12/28 06:56:06 marcelop Exp $
+ */
+package com.sap.emf.importer.moin.rose.parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.URIConverter;
+
+import com.sap.emf.importer.moin.rose.RoseImporterPlugin;
+
+
+/**
+ * A loader that creates Buffered Reader.
+ *
+ */
+public class RoseLoader extends RoseComponent
+{
+  public static final String PROGRESS = "ROSE_LOADER_PROGRESS";
+
+  protected boolean valid;
+  protected BufferedReader bufferedReader;
+  protected long length = 0;
+  protected long currentLength = 0;
+  protected int progressIncrement = 10;
+  protected int oldValue;
+  protected int lower = 0;
+  protected int upper = 100;
+
+  public RoseLoader(String fileName, URIConverter uriConverter) throws Exception
+  {
+    try
+    {
+      if (!fileName.startsWith("\\\\"))
+      {
+        fileName = Util.updateFileName(fileName, "\\\\");
+      }
+      fileName = Util.updateFileName(fileName, "\\");
+      fileName = Util.updateFileName(fileName, "/");
+
+      bufferedReader = new BufferedReader(new FileReader(fileName));
+      valid = true;
+      length = (new File(fileName)).length();
+      currentLength = 0;
+      oldValue = lower;
+    }
+    catch (Exception exception)
+    {
+      Pattern pattern = Pattern.compile(".*/([^/]*?)(?:_)?[0-9.]*/((src/)?model/[^/]*\\.cat)$");
+      Matcher matcher = pattern.matcher(fileName.replaceAll("\\\\", "/"));
+      if (matcher.matches())
+      {
+        String name = matcher.group(1);
+        String tail = matcher.group(2);
+        InputStream inputStream = null;
+        try
+        {
+          inputStream = uriConverter.createInputStream(URI.createPlatformResourceURI(name + "/" + tail, true));
+        }
+        catch (Exception resourceException)
+        {
+          try
+          {
+            inputStream = uriConverter.createInputStream(URI.createPlatformPluginURI(name + "/" + tail, true));
+          }
+          catch (Exception pluginException)
+          {
+            // Ignore
+          }
+        }
+        if (inputStream != null)
+        {
+          bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+          valid = true;
+          length = inputStream.available();
+          currentLength = 0;
+          oldValue = lower;
+        }
+        else
+        {
+          RoseImporterPlugin.INSTANCE.log(exception);
+        }
+      }
+      else
+      {
+        RoseImporterPlugin.INSTANCE.log(exception);
+      }
+    }
+  }
+
+  public boolean isValid()
+  {
+    return valid;
+  }
+
+  public void setProgressIncrement(int progressIncrement)
+  {
+    this.progressIncrement = progressIncrement;
+  }
+
+  public void setLower(int lower)
+  {
+    this.lower = lower;
+    oldValue = lower;
+  }
+
+  public int getLower()
+  {
+    return lower;
+  }
+
+  public void setUpper(int upper)
+  {
+    this.upper = upper;
+  }
+
+  public int getUpper()
+  {
+    return upper;
+  }
+
+  public String readLine()
+  {
+    try
+    {
+      String line = bufferedReader.readLine();
+      if (line != null)
+      {
+        currentLength += line.length();
+        if (length > 0)
+        {
+          int newValue = lower + (int)(currentLength * (upper - lower) / length);
+          if (newValue >= oldValue + progressIncrement && newValue < upper)
+          {
+            firePropertyChange(PROGRESS, oldValue, newValue);
+            oldValue = newValue;
+          }
+        }
+      }
+      return line;
+    }
+    catch (Exception e)
+    {
+      return null;
+    }
+  }
+
+  public void close() throws IOException
+  {
+    if (bufferedReader != null)
+    {
+      bufferedReader.close();
+    }
+  }
+}
