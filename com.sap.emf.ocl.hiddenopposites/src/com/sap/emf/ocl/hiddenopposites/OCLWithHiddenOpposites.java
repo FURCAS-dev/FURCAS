f4fdf46ec996901597c8cd10c954ba52392014ed
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-30 16:13:32 +0000

    more preparations for an OCL redefinition with hidden opposites
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1079 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ com.sap.emf.ocl.hiddenopposites/src/com/sap/emf/ocl/hiddenopposites/OCLWithHiddenOpposites.java
@@ -0,0 +1,44 @@
+package com.sap.emf.ocl.hiddenopposites;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EParameter;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.ocl.Environment;
+import org.eclipse.ocl.EnvironmentFactory;
+import org.eclipse.ocl.ecore.CallOperationAction;
+import org.eclipse.ocl.ecore.Constraint;
+import org.eclipse.ocl.ecore.OCL;
+import org.eclipse.ocl.ecore.SendSignalAction;
+
+public class OCLWithHiddenOpposites extends OCL {
+
+	protected OCLWithHiddenOpposites(
+			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
+		super(env);
+	}
+
+	protected OCLWithHiddenOpposites(
+			EnvironmentFactory<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> envFactory,
+			Resource resource) {
+		super(envFactory, resource);
+	}
+
+	protected OCLWithHiddenOpposites(
+			EnvironmentFactory<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> envFactory) {
+		super(envFactory);
+	}
+
+	@Override
+	public Helper createOCLHelper() {
+		return new OCLHelperWithOppositesImpl(super.createOCLHelper());
+	}
+	
+	
+
+}
