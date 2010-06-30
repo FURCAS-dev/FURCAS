c03b0b616a1542a157194c7ad5840116d428152b
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-06-30 15:53:20 +0000

    added validation for opposite property call exp's type
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1077 3ca8e212-6386-4b1e-b09d-39f68028012c

--- com.sap.emf.ocl.hiddenopposites/src/com/sap/emf/ocl/oclwithhiddenopposites/expressions/impl/OppositePropertyCallExpImpl.java
+++ com.sap.emf.ocl.hiddenopposites/src/com/sap/emf/ocl/oclwithhiddenopposites/expressions/impl/OppositePropertyCallExpImpl.java
@@ -20,6 +20,7 @@ import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.common.util.DiagnosticChain;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.InternalEObject;
 
@@ -29,7 +30,10 @@ import org.eclipse.emf.ecore.plugin.EcorePlugin;
 
 import org.eclipse.emf.ecore.util.EObjectValidator;
 
+import org.eclipse.ocl.ecore.CollectionType;
 import org.eclipse.ocl.ecore.impl.NavigationCallExpImpl;
+import org.eclipse.ocl.util.OCLUtil;
+import org.eclipse.ocl.util.TypeUtil;
 
 /**
  * <!-- begin-user-doc -->
@@ -118,11 +122,15 @@ public class OppositePropertyCallExpImpl extends NavigationCallExpImpl implement
 	 * @generated NOT
 	 */
 	public boolean property_type(DiagnosticChain diagnostics, Map<Object, Object> context) {
-		// TODO: implement this method
-		// -> specify the condition that violates the invariant
-		// -> verify the details of the diagnostic, including severity and message
-		// Ensure that you remove @generated or mark it @generated NOT
-		if (false) {
+		boolean good = false;
+		EClass eClass = (EClass) getReferredOppositeProperty().eContainer();
+		EClassifier type = getType();
+		if (type instanceof CollectionType) {
+			CollectionType collectionType = (CollectionType) type;
+			EClassifier elementType = collectionType.getElementType();
+			good = TypeUtil.exactTypeMatch(OCLUtil.getValidationEnvironment(this, context), elementType, eClass);
+		}
+		if (!good) {
 			if (diagnostics != null) {
 				diagnostics.add
 					(new BasicDiagnostic
@@ -132,9 +140,8 @@ public class OppositePropertyCallExpImpl extends NavigationCallExpImpl implement
 						 EcorePlugin.INSTANCE.getString("_UI_GenericInvariant_diagnostic", new Object[] { "property_type", EObjectValidator.getObjectLabel(this, context) }),
 						 new Object [] { this }));
 			}
-			return false;
 		}
-		return true;
+		return good;
 	}
 
 	/**
