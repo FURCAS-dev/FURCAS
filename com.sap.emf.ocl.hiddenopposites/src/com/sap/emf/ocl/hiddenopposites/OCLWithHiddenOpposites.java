c0a9650615be87ac7004c15ff299ea8e47b1714f
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-01 15:35:38 +0000

    next compilable version with EReference specialization of opposite-related APIs, specialized OCLAnalyzer which now holds the functionality to produce OppositePropertyCallExp expressions
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1099 3ca8e212-6386-4b1e-b09d-39f68028012c

--- com.sap.emf.ocl.hiddenopposites/src/com/sap/emf/ocl/hiddenopposites/OCLWithHiddenOpposites.java
+++ com.sap.emf.ocl.hiddenopposites/src/com/sap/emf/ocl/hiddenopposites/OCLWithHiddenOpposites.java
@@ -14,89 +14,104 @@ import org.eclipse.ocl.ecore.CallOperationAction;
 import org.eclipse.ocl.ecore.Constraint;
 import org.eclipse.ocl.ecore.OCL;
 import org.eclipse.ocl.ecore.SendSignalAction;
+import org.eclipse.ocl.parser.OCLAnalyzer;
+import org.eclipse.ocl.parser.backtracking.OCLBacktrackingLexer;
+import org.eclipse.ocl.parser.backtracking.OCLBacktrackingParser;
 
 public class OCLWithHiddenOpposites extends OCL {
 
-	protected OCLWithHiddenOpposites(
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
-		super(env);
-	}
+    protected OCLWithHiddenOpposites(
+	    Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env) {
+	super(env);
+    }
 
-	protected OCLWithHiddenOpposites(
-			EcoreEnvironmentFactoryWithHiddenOpposites envFactory,
-			Resource resource) {
-		super(envFactory, resource);
-	}
+    protected OCLWithHiddenOpposites(EcoreEnvironmentFactoryWithHiddenOpposites envFactory, Resource resource) {
+	super(envFactory, resource);
+    }
 
-	protected OCLWithHiddenOpposites(EcoreEnvironmentFactoryWithHiddenOpposites envFactory) {
-		super(envFactory);
-	}
+    protected OCLWithHiddenOpposites(EcoreEnvironmentFactoryWithHiddenOpposites envFactory) {
+	super(envFactory);
+    }
 
-	@Override
-	public Helper createOCLHelper() {
-		return new OCLHelperWithOppositesImpl(super.createOCLHelper());
-	}
-	
-	/**
-     * Creates a new <code>OCL</code> using the shared Ecore environment
-     * factory instance.
+    @Override
+    public Helper createOCLHelper() {
+	return new OCLHelperWithOppositesImpl(super.createOCLHelper());
+    }
+
+    /**
+     * Creates a new <code>OCL</code> using the shared Ecore environment factory instance.
      * 
      * @return the new <code>OCL</code>
      */
-	public static OCL newInstance() {
-		return new OCLWithHiddenOpposites(EcoreEnvironmentFactoryWithHiddenOpposites.INSTANCE);
-	}
-	
+    public static OCL newInstance() {
+	return new OCLWithHiddenOpposites(EcoreEnvironmentFactoryWithHiddenOpposites.INSTANCE);
+    }
+
     /**
-     * Creates a new <code>OCL</code> creating a new Ecore environment
-     * factory instance that uses the <code>oppositeEndFinder</code>
-     * specified.
+     * Creates a new <code>OCL</code> creating a new Ecore environment factory instance that uses the
+     * <code>oppositeEndFinder</code> specified.
      * 
      * @return the new <code>OCL</code>
      * @since 3.0
      */
-	public static OCL newInstance(OppositeEndFinder oppositeEndFinder) {
-		return new OCLWithHiddenOpposites(new EcoreEnvironmentFactoryWithHiddenOpposites(oppositeEndFinder));
-	}
-	
-    /**
-     * Creates a new <code>OCL</code> using the specified Ecore environment
-     * factory.
-     * 
-     * @param envFactory an environment factory for Ecore
-     * @return the new <code>OCL</code>
-     */
-	public static OCL newInstance(EcoreEnvironmentFactoryWithHiddenOpposites envFactory) {
-		
-		return new OCLWithHiddenOpposites(envFactory);
-	}
-	
-    /**
-     * Creates a new <code>OCL</code> using the specified initial Ecore
-     * environment.
-     * 
-     * @param env an environment for Ecore
-     * @return the new <code>OCL</code>
-     */
-	public static OCL newInstance(EcoreEnvironmentWithHiddenOpposites env) {
-		
-		return new OCLWithHiddenOpposites(env);
-	}
-	
-    /**
-     * Creates a new <code>OCL</code> using the specified Ecore environment
-     * factory and a resource from which to load the initial environment.
-     * 
-     * @param envFactory an environment factory for Ecore
-     * @param resource the resource containing a persistent environment
-     *    (which may be empty for an initially empty environment)
-     * @return the new <code>OCL</code>
-     */
-	public static OCL newInstance(EcoreEnvironmentFactoryWithHiddenOpposites envFactory,
-			Resource resource) {
-		
-		return new OCLWithHiddenOpposites(envFactory, resource);
-	}
-    
+    public static OCL newInstance(OppositeEndFinder oppositeEndFinder) {
+	return new OCLWithHiddenOpposites(new EcoreEnvironmentFactoryWithHiddenOpposites(oppositeEndFinder));
+    }
 
+    /**
+     * Creates a new <code>OCL</code> using the specified Ecore environment factory.
+     * 
+     * @param envFactory
+     *            an environment factory for Ecore
+     * @return the new <code>OCL</code>
+     */
+    public static OCL newInstance(EcoreEnvironmentFactoryWithHiddenOpposites envFactory) {
+
+	return new OCLWithHiddenOpposites(envFactory);
+    }
+
+    /**
+     * Creates a new <code>OCL</code> using the specified initial Ecore environment.
+     * 
+     * @param env
+     *            an environment for Ecore
+     * @return the new <code>OCL</code>
+     */
+    public static OCL newInstance(EcoreEnvironmentWithHiddenOpposites env) {
+
+	return new OCLWithHiddenOpposites(env);
+    }
+
+    /**
+     * Creates a new <code>OCL</code> using the specified Ecore environment factory and a resource from which to load
+     * the initial environment.
+     * 
+     * @param envFactory
+     *            an environment factory for Ecore
+     * @param resource
+     *            the resource containing a persistent environment (which may be empty for an initially empty
+     *            environment)
+     * @return the new <code>OCL</code>
+     */
+    public static OCL newInstance(EcoreEnvironmentFactoryWithHiddenOpposites envFactory, Resource resource) {
+
+	return new OCLWithHiddenOpposites(envFactory, resource);
+    }
+
+    @Override
+    public OCLAnalyzer<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> createAnalyzer(
+	    String input) {
+	OCLAnalyzerWithHiddenOpposites analyzer;
+	if (getParserRepairCount() != 0) {
+		OCLBacktrackingLexer lexer = new OCLBacktrackingLexer(
+			getEnvironment(), input.toCharArray());
+		OCLBacktrackingParser parser = new OCLBacktrackingParser(lexer);
+		parser.setDefaultRepairCount(getParserRepairCount());
+		lexer.lexer(parser.getIPrsStream());
+		analyzer = new OCLAnalyzerWithHiddenOpposites(parser);
+	} else {
+		analyzer = new OCLAnalyzerWithHiddenOpposites(getEnvironment(), input);
+	}
+	return analyzer;
+    }
 }
