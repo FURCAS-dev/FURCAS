163ad3486375e9aa5ba8587e12f2da2fc3a32801
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-06 19:55:04 +0000

    Fixed errors on parsing Class.tcs expressions with hidden opposites
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1148 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/preparation/ocl/OCLExpressionFromClassTcsPicker.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/preparation/ocl/OCLExpressionFromClassTcsPicker.java
@@ -11,6 +11,8 @@ import org.eclipse.ocl.ecore.OCL;
 import org.eclipse.ocl.ecore.OCLExpression;
 import org.eclipse.ocl.utilities.ExpressionInOCL;
 
+import com.sap.emf.ocl.hiddenopposites.OCLWithHiddenOpposites;
+
 import de.hpi.sam.bp2009.solution.impactAnalyzer.tests.helper.OclTestExpressionContainer;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.tests.helper.OclTestExpressionContainer.OclExpressionWithPackage;
 
@@ -32,26 +34,26 @@ public class OCLExpressionFromClassTcsPicker implements OCLExpressionPicker {
      * @return a list of {@link Constraint}s parsed from given expression
      */
     protected OCLExpressionWithContext parse(String expression, EPackage basePackage) {
-	OCLInput exp = new OCLInput(expression);
-	String nsPrefix = basePackage.getNsPrefix();
-	EPackage.Registry.INSTANCE.put(nsPrefix, basePackage);
-	ArrayList<String> path = new ArrayList<String>();
-	path.add(nsPrefix);
-	OCL ocl = OCL.newInstance();
-	ocl = OCL.newInstance(ocl.getEnvironment().getFactory().createPackageContext(ocl.getEnvironment(), path));
-	OCLExpressionWithContext result = null;
-	try {
-	    ExpressionInOCL specification = ocl.parse(exp).iterator().next().getSpecification();
-	    OCLExpression expr = (OCLExpression) specification.getBodyExpression();
-
-	    result = new OCLExpressionWithContext(expr, (EClass) specification.getContextVariable().getType());
-
-	} catch (ParserException e) {
-	    System.err.println("Error while parsing Expression:" + exp);
-	    e.printStackTrace();
-	    System.exit(0);
-	}
-	return result;
+		OCLInput exp = new OCLInput(expression);
+		String nsPrefix = basePackage.getNsPrefix();
+		EPackage.Registry.INSTANCE.put(nsPrefix, basePackage);
+		ArrayList<String> path = new ArrayList<String>();
+		path.add(nsPrefix);
+		OCL ocl = OCLWithHiddenOpposites.newInstance();
+		ocl = OCLWithHiddenOpposites.newInstance(ocl.getEnvironment().getFactory().createPackageContext(ocl.getEnvironment(), path));
+		OCLExpressionWithContext result = null;
+		try {
+		    ExpressionInOCL specification = ocl.parse(exp).iterator().next().getSpecification();
+		    OCLExpression expr = (OCLExpression) specification.getBodyExpression();
+	
+		    result = new OCLExpressionWithContext(expr, (EClass) specification.getContextVariable().getType());
+	
+		} catch (ParserException e) {
+		    System.err.println("Error while parsing Expression:" + exp);
+		    e.printStackTrace();
+		    System.exit(0);
+		}
+		return result;
     }
 
 }
