3e6d153bab284971ac09dd6540b6c3a064e692f2
tobiashoppe <tobiashoppe@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-07 21:59:53 +0000

    moved new scope provider version to correct folder
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1153 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ de.hpi.sam.bp2009.solution.queryContextScopeProvider/src/de/hpi/sam/bp2009/solution/queryContextScopeProvider/QueryContextProvider.java
@@ -0,0 +1,17 @@
+package de.hpi.sam.bp2009.solution.queryContextScopeProvider;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.query2.QueryContext;
+
+/**
+ * Can provide an EMF query2 context that describes what's visible for a query,
+ * starting at a given element. The provider implementation may use the element
+ * to determine, based on visibility and modularization rules, what can be seen
+ * by the query.
+ * 
+ * @author Axel Uhl
+ *
+ */
+public interface QueryContextProvider {
+        QueryContext getQueryContext(EObject eObject);
+}
