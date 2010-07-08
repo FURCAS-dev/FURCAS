42d22a12e0256a6fd178f8dd1b02cba952f67e62
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-08 06:20:38 +0000

    Restructured classes for performing micro-measurements
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1158 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/BenchmarkMeasurements.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/BenchmarkMeasurements.java
@@ -4,36 +4,77 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class BenchmarkMeasurements {
-    private static HashMap<String, ArrayList<MicroMeasurement>> measurementList = new HashMap<String, ArrayList<MicroMeasurement>>();
+	static int aggregateCounter = 0;
+	
+    private static HashMap<String, ArrayList<Measurement>> measurementList = new HashMap<String, ArrayList<Measurement>>();
 
-    public static MicroMeasurement createNewMeasurement(String measurePointId) {
-	MicroMeasurement measure = new MicroMeasurement();
+    public static Measurement createNewMeasurement(String measurePointId) {
+	Measurement measure = new MicroMeasurement();
 
 	if (getMeasurementList().containsKey(measurePointId)) {
 	    getMeasurementList().get(measurePointId).add(measure);
 	} else {
-	    getMeasurementList().put(measurePointId, new ArrayList<MicroMeasurement>());
+	    getMeasurementList().put(measurePointId, new ArrayList<Measurement>());
 	    getMeasurementList().get(measurePointId).add(measure);
 	}
 
 	return measure;
     }
+    
+    public static Measurement createNewAdditiveMeasurement(String measurePointId) {
+    	Measurement measure = new AdditiveMeasurement();
 
-    public static MicroMeasurement createSubMeasurement(String id, String subId){
+    	if (getMeasurementList().containsKey(measurePointId)) {
+    	    getMeasurementList().get(measurePointId).add(measure);
+    	} else {
+    	    getMeasurementList().put(measurePointId, new ArrayList<Measurement>());
+    	    getMeasurementList().get(measurePointId).add(measure);
+    	}
+
+    	return measure;
+    }
+
+    public static Measurement createSubMeasurement(String id, String subId){
 	MicroMeasurement measure = new MicroMeasurement();
 
-	ArrayList<MicroMeasurement> allMeasuresForId = getMeasurementList().get(id);
-	MicroMeasurement parent = allMeasuresForId.get(allMeasuresForId.size() - 1);
+	ArrayList<Measurement> allMeasuresForId = getMeasurementList().get(id);
+	Measurement parent = allMeasuresForId.get(allMeasuresForId.size() - 1);
 	parent.addSubMeasurement(subId, measure);
 
 	return measure;
     }
 
-    public static HashMap<String, ArrayList<MicroMeasurement>> getMeasurementList() {
+    public static HashMap<String, ArrayList<Measurement>> getMeasurementList() {
 	return measurementList;
     }
 
     public static void reset(){
-	measurementList = new HashMap<String, ArrayList<MicroMeasurement>>();
+	measurementList = new HashMap<String, ArrayList<Measurement>>();
+	aggregateCounter = 0;
+    }
+    
+    public static void aggregate(){
+    	
+    	for(String key : measurementList.keySet()){
+    		ArrayList<Measurement> value = measurementList.get(key);
+    		ArrayList<Measurement> newValue = new ArrayList<Measurement>();
+    		
+    		if(value.size() > (aggregateCounter + 1)){
+    			for(int i = 0; i < aggregateCounter; i++){
+    				newValue.add(value.get(i));
+    			}
+    			
+    			AdditiveMeasurement additiveMeasure = new AdditiveMeasurement();
+    			for(int i = aggregateCounter; i < value.size(); i++){
+    				additiveMeasure.addMeasurement(value.get(i));
+    			}
+    			
+    			newValue.add(additiveMeasure);
+    		}
+    		
+    		measurementList.put(key, newValue);
+    	}
+    	
+    	aggregateCounter++;
     }
 }
