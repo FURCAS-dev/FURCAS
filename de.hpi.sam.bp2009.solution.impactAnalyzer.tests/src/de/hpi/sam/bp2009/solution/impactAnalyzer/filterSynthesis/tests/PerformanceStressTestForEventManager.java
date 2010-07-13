7feb417dd52210f8e034c96bc20014fe29e5f50a
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-13 11:07:30 +0000

    added benchmarks for cases with different yes-set sizes, and with/without composition
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1196 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/filterSynthesis/tests/PerformanceStressTestForEventManager.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/filterSynthesis/tests/PerformanceStressTestForEventManager.java
@@ -18,9 +18,16 @@ import org.junit.Test;
 
 import persistence.expressions.ExpressionsFactory;
 import persistence.expressions.ExpressionsPackage;
-
+import behavioral.actions.ActionsFactory;
+import behavioral.actions.ActionsPackage;
 import data.classes.ClassesFactory;
 import data.classes.ClassesPackage;
+import data.classes.MethodSignature;
+import data.classes.SapClass;
+import dataaccess.analytics.AnalyticsFactory;
+import dataaccess.analytics.AnalyticsPackage;
+import dataaccess.expressions.literals.LiteralsFactory;
+import dataaccess.expressions.literals.StringLiteral;
 import de.hpi.sam.bp2009.solution.eventManager.EventManager;
 import de.hpi.sam.bp2009.solution.eventManager.Statistics;
 import de.hpi.sam.bp2009.solution.eventManager.filters.EventFilter;
@@ -55,9 +62,12 @@ public class PerformanceStressTestForEventManager extends TestCase implements Ad
 
     @Test
     public void testSingleAttributeValueChange() {
+        
+        // first some attributes:
+        
         // name has 290 entries in TableForAttributeFilter
         InternalEObject notifier = (InternalEObject) ClassesFactory.eINSTANCE.createSapClass();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000; i++) {
             Notification n = new ENotificationImpl(notifier, Notification.SET,
                     ModelmanagementPackage.eINSTANCE.getNamedElement_Name(), "humba", "trala");
             Statistics.getInstance().begin("Notify_Attribute_Name_290", ""+i);
@@ -66,7 +76,7 @@ public class PerformanceStressTestForEventManager extends TestCase implements Ad
         }
         // upperMultiplicity has 487 entries in TableForAttributeFilter
         notifier = (InternalEObject) ClassesFactory.eINSTANCE.createClassTypeDefinition();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000; i++) {
             Notification n = new ENotificationImpl(notifier, Notification.SET,
                     ClassesPackage.eINSTANCE.getMultiplicity_UpperMultiplicity(), 1, -1);
             Statistics.getInstance().begin("Notify_Attribute_UpperMultiplicity_487", ""+i);
@@ -75,13 +85,59 @@ public class PerformanceStressTestForEventManager extends TestCase implements Ad
         }
         // snapshot has 1 entry in TableForAttributeFilter
         notifier = (InternalEObject) ExpressionsFactory.eINSTANCE.createAll();
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 1000; i++) {
             Notification n = new ENotificationImpl(notifier, Notification.SET,
                     ExpressionsPackage.eINSTANCE.getAll_Snapshot(), 1, -1);
             Statistics.getInstance().begin("Notify_Attribute_Snapshot_1", ""+i);
             eventManager.handleEMFEvent(n);
             Statistics.getInstance().end("Notify_Attribute_Snapshot_1", ""+i);
         }
+        
+        // now some references:
+        
+        // clazz has 264 entries in TableForAssociationFilter
+        notifier = (InternalEObject) ClassesFactory.eINSTANCE.createClassTypeDefinition();
+        SapClass c = ClassesFactory.eINSTANCE.createSapClass();
+        for (int i = 0; i < 1000; i++) {
+            Notification n = new ENotificationImpl(notifier, Notification.SET,
+                    ClassesPackage.eINSTANCE.getClassTypeDefinition_Clazz(), null, c);
+            Statistics.getInstance().begin("Notify_Reference_Clazz_264", ""+i);
+            eventManager.handleEMFEvent(n);
+            Statistics.getInstance().end("Notify_Reference_Clazz_264", ""+i);
+        }
+        
+        // facts has 1 entry in TableForAssociationFilter
+        notifier = (InternalEObject) AnalyticsFactory.eINSTANCE.createDimensionExpression();
+        StringLiteral s = LiteralsFactory.eINSTANCE.createStringLiteral();
+        for (int i = 0; i < 1000; i++) {
+            Notification n = new ENotificationImpl(notifier, Notification.SET,
+                    AnalyticsPackage.eINSTANCE.getDimensionExpression_Facts(), null, s);
+            Statistics.getInstance().begin("Notify_Reference_Facts_1", ""+i);
+            eventManager.handleEMFEvent(n);
+            Statistics.getInstance().end("Notify_Reference_Facts_1", ""+i);
+        }
+        
+        // initExpression has 478 entries in TableForAssociationFilter
+        notifier = (InternalEObject) ActionsFactory.eINSTANCE.createNamedValueDeclaration();
+        for (int i = 0; i < 1000; i++) {
+            Notification n = new ENotificationImpl(notifier, Notification.SET,
+                    ActionsPackage.eINSTANCE.getNamedValueWithOptionalInitExpression_InitExpression(), null, s);
+            Statistics.getInstance().begin("Notify_Reference_InitExpression_478", ""+i);
+            eventManager.handleEMFEvent(n);
+            Statistics.getInstance().end("Notify_Reference_InitExpression_478", ""+i);
+        }
+        
+        // ownerSignatures has 41 entries in TableForAssociationFilter and is composite
+        notifier = (InternalEObject) ClassesFactory.eINSTANCE.createSapClass();
+        MethodSignature m = ClassesFactory.eINSTANCE.createMethodSignature();
+        for (int i = 0; i < 1000; i++) {
+            Notification n = new ENotificationImpl(notifier, Notification.SET,
+                    ClassesPackage.eINSTANCE.getSignatureOwner_OwnedSignatures(), null, m);
+            Statistics.getInstance().begin("Notify_Reference_ownedSignatures_41", ""+i);
+            eventManager.handleEMFEvent(n);
+            Statistics.getInstance().end("Notify_Reference_ownedSignatures_41", ""+i);
+        }
+        
 
         System.out.println("Notification count: "+notificationCount);
         System.out.println(Statistics.getInstance().averageTimeAsSV("\t"));
