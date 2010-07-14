8025b258ae32d9ecdc8e4a0f74e82cc7f8c48fee
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-14 14:02:13 +0000

    Made changes to aggregation of benchmark results
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1217 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/test/BenchmarkAsPluginTest.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/test/BenchmarkAsPluginTest.java
@@ -1,14 +1,15 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.test;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.BenchmarkProcessor;
-import junit.framework.TestCase;
 
 
 public class BenchmarkAsPluginTest extends TestCase{
 	@Test
 	public void testStartBenchmarks(){
-		BenchmarkProcessor.start();
+		BenchmarkProcessor.start("/home/manuel/workspace/de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/results/result.csv");
 	}
 }
