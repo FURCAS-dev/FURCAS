43c5044d381c02d4d446d3d2b8debc8882170280
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-16 23:13:24 +0000

    re-formatted
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1261 3ca8e212-6386-4b1e-b09d-39f68028012c

--- com.sap.emf.ocl.hiddenopposites/src/com/sap/emf/ocl/hiddenopposites/EvaluationVisitorWithHiddenOppositesImpl.java
+++ com.sap.emf.ocl.hiddenopposites/src/com/sap/emf/ocl/hiddenopposites/EvaluationVisitorWithHiddenOppositesImpl.java
@@ -1,8 +1,6 @@
 package com.sap.emf.ocl.hiddenopposites;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -27,72 +25,46 @@ import org.eclipse.ocl.util.CollectionUtil;
 
 import com.sap.emf.ocl.oclwithhiddenopposites.expressions.OppositePropertyCallExp;
 
-public class EvaluationVisitorWithHiddenOppositesImpl extends
-		EvaluationVisitorImpl<EPackage, EClassifier, EOperation, EStructuralFeature,
-		EEnumLiteral, EParameter,
-		EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject>
-		implements EvaluationVisitorWithHiddenOpposites {
+public class EvaluationVisitorWithHiddenOppositesImpl
+        extends
+        EvaluationVisitorImpl<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject>
+        implements EvaluationVisitorWithHiddenOpposites {
 
-	public EvaluationVisitorWithHiddenOppositesImpl(
-			Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
-			EvaluationEnvironment<EClassifier, EOperation, EStructuralFeature, EClass, EObject> evalEnv,
-			Map<? extends EClass, ? extends Set<? extends EObject>> extentMap) {
-		super(env, evalEnv, extentMap);
-	}
+    public EvaluationVisitorWithHiddenOppositesImpl(
+            Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
+            EvaluationEnvironment<EClassifier, EOperation, EStructuralFeature, EClass, EObject> evalEnv,
+            Map<? extends EClass, ? extends Set<? extends EObject>> extentMap) {
+        super(env, evalEnv, extentMap);
+    }
 
-	public Object visitOppositePropertyCallExp(OppositePropertyCallExp opce) {
-		EStructuralFeature property = opce.getReferredOppositeProperty();
-		OCLExpression<EClassifier> source = opce.getSource();
-
-		// evaluate source
-		Object context = source.accept(getVisitor());
-
-		// if source is undefined, result is OclInvalid
-		if (isUndefined(context)) {
+    public Object visitOppositePropertyCallExp(OppositePropertyCallExp opce) {
+        EStructuralFeature property = opce.getReferredOppositeProperty();
+        OCLExpression<EClassifier> source = opce.getSource();
+        // evaluate source
+        Object context = source.accept(getVisitor());
+        // if source is undefined, result is OclInvalid
+        if (isUndefined(context)) {
             return getInvalid();
         }
+        Object result = getEvaluationEnvironment().navigateOppositeProperty(property, context);
+        if ((opce.getType() instanceof CollectionType<?, ?>) && !(result instanceof Collection<?>)) {
+            // this was an XSD "unspecified multiplicity". Now that we know what
+            // the multiplicity is, we can coerce it to a collection value
+            @SuppressWarnings("unchecked")
+            CollectionKind kind = ((CollectionType<EClassifier, EOperation>) opce.getType()).getKind();
+            Collection<Object> collection = CollectionUtil.createNewCollection(kind);
+            if (result != null) {
+                collection.add(result);
+            }
+            result = collection;
+        }
 
-		OCLExpression<EClassifier> derivation = getPropertyBody(property);
-		if (derivation != null) {
-			// this is an additional property
-			
-			return navigate(property, derivation, context);
-		}
-		
-		List<Object> qualifiers;
-		
-		if (opce.getQualifier().isEmpty()) {
-			qualifiers = Collections.emptyList();
-		} else {
-			// handle qualified association navigation
-			qualifiers = new java.util.ArrayList<Object>();
-			
-			for (OCLExpression<EClassifier> q : opce.getQualifier()) {
-				qualifiers.add(q.accept(getVisitor()));
-			}
-		}
-		
-		Object result = getEvaluationEnvironment().navigateOppositeProperty(property, context);
-		
-		if ((opce.getType() instanceof CollectionType<?, ?>) && !(result instanceof Collection<?>)) {
-			// this was an XSD "unspecified multiplicity".  Now that we know what
-			//    the multiplicity is, we can coerce it to a collection value
-			@SuppressWarnings("unchecked")
-			CollectionKind kind = ((CollectionType<EClassifier, EOperation>) opce.getType()).getKind();
-			
-			Collection<Object> collection = CollectionUtil.createNewCollection(kind);
-			if (result != null) {
-			    collection.add(result);
-			}
-			result = collection;
-		}
-		
-		return result;
-	}
-	
-	@Override
-	public EvaluationEnvironmentWithHiddenOpposites getEvaluationEnvironment() {
-		return (EvaluationEnvironmentWithHiddenOpposites) super.getEvaluationEnvironment();
-	}
+        return result;
+    }
+
+    @Override
+    public EvaluationEnvironmentWithHiddenOpposites getEvaluationEnvironment() {
+        return (EvaluationEnvironmentWithHiddenOpposites) super.getEvaluationEnvironment();
+    }
 
 }
