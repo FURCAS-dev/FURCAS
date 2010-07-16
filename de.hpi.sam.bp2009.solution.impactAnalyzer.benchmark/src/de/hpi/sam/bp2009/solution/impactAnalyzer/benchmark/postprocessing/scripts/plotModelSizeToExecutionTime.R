a4b0897a276eb9a461252a92b06f7f013d9c9ded
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-16 13:53:02 +0000

    Added R script for aggregating and plotting model size to execution time benchmark results
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1250 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/plotModelSizeToExecutionTime.R
@@ -0,0 +1,18 @@
+require(outliers)
+results = read.table("/tmp/result.data", header=TRUE)
+
+length(results$executionTime)
+
+summary(results)
+
+for(i in 1:5){
+results$cleanExecutionTime = ifelse(results$executionTime == outlier(results$executionTime), NA, results$executionTime)
+results = subset(results, !is.na(results$cleanExecutionTime))
+results$cleanExecutionTime = NULL;
+}
+summary(results)
+aggregatedResults = aggregate(results, by=list(results$modelSize), FUN=mean)
+attach(aggregatedResults)
+plot(x=modelSize, y=executionTime)
+xyplot(executionTime~modelSize, type=c("p","r"))
+detach(aggregatedResults)
