83eae22b868e64953b38c7609497da6d1bcbff3c
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-27 11:17:43 +0000

    Add option to be able to dump all exceptions which have been occurred during benchmarks into a specified file
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1334 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/BenchmarkResultWriter.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/BenchmarkResultWriter.java
@@ -1,13 +1,6 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.postprocessing;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,35 +8,12 @@ import java.util.Map;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.OutputOptions;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.execution.measurements.Measurement;
 
-public class BenchmarkResultWriter {
-
-	private Writer writer;
+public class BenchmarkResultWriter extends Writer {
 	private boolean withHeadline = true;
 	private int lineOffset = 1;
 
-	private static final String BREAK = System.getProperty("line.separator");
-
 	public BenchmarkResultWriter() {
-		File f = new File(OutputOptions.getOutputPath());
-		createFileIfNeeded(f);
-		OutputStream os = null;
-
-		try {
-			os = new FileOutputStream(f);
-			writer = new BufferedWriter(new OutputStreamWriter(os), 81920);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	public synchronized void close(){
-		try {
-			writer.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		super(OutputOptions.getOutputPath());
 	}
 
     public synchronized void writeDataSet(Map<String, String> additionalInformation, ArrayList<Long> executionTimeList, ArrayList<Map<String, String>> additionalMeasurementInformationList, HashMap<String, ArrayList<Measurement>>microMeasurementList){
@@ -62,7 +32,7 @@ public class BenchmarkResultWriter {
 	    for(String label : additionalMeasurementInformationList.get(0).keySet()){
 		writer.write(TAB + label);
 	    }
-	    
+
 
 	    writer.write(TAB + "executionIndex" + TAB + "executionTime" + BREAK);
 	    withHeadline = false;
@@ -94,15 +64,6 @@ public class BenchmarkResultWriter {
 		}
     }
 
-    private void createFileIfNeeded(File f) {
-    	if(!f.exists()){
-    	    try {
-    		f.getParentFile().mkdirs();
-    	    	f.createNewFile();
-    	    } catch (IOException e) {
-    		e.printStackTrace();
-    	    }
-    	}
-        }
+
 
 }
---
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/Writer.java
@@ -0,0 +1,50 @@
+package de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.postprocessing;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public abstract class Writer {
+    protected java.io.Writer writer;
+
+    protected static final String BREAK = System.getProperty("line.separator");
+
+
+    public Writer(String path) {
+    	File f = new File(path);
+    	createFileIfNeeded(f);
+    	OutputStream os = null;
+
+    	try {
+    		os = new FileOutputStream(f);
+    		writer = new BufferedWriter(new OutputStreamWriter(os), 81920);
+    	} catch (FileNotFoundException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+    }
+
+    protected void createFileIfNeeded(File f) {
+    	if(!f.exists()){
+    	    try {
+    		f.getParentFile().mkdirs();
+    	    	f.createNewFile();
+    	    } catch (IOException e) {
+    		e.printStackTrace();
+    	    }
+    	}
+   }
+
+	public synchronized void close(){
+		try {
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
