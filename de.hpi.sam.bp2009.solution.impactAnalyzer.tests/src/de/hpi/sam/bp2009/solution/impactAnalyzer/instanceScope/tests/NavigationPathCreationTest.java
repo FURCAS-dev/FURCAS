afec3998f0db0912c46bf12a26ac769a034c6198
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-07-30 12:02:22 +0000

    Get attribute call expressions and association end call expressions with dirty reflection out of filter synthesizer
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1352 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/NavigationPathCreationTest.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/NavigationPathCreationTest.java
@@ -1,106 +1,96 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.tests;
 
+import java.lang.reflect.Field;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import junit.framework.TestCase;
 
-import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EReference;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.ocl.ecore.NavigationCallExp;
-import org.eclipse.ocl.ecore.OCLExpression;
+import org.eclipse.ocl.ecore.PropertyCallExp;
 import org.junit.Test;
 
 import com.sap.emf.ocl.hiddenopposites.DefaultOppositeEndFinder;
 
-import de.hpi.sam.bp2009.solution.eventManager.NotificationHelper;
-import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.notifications.NotificationResourceLoader;
-import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.notifications.RawNotification;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.ocl.OCLExpressionFromClassTcsPicker;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.ocl.OCLExpressionFromModelPicker;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.ocl.OCLExpressionWithContext;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.filterSynthesis.FilterSynthesisImpl;
-import de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.NavigationStep;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.PathCache;
 
 public class NavigationPathCreationTest extends TestCase{
     private Collection<OCLExpressionWithContext> classTcsExpressionList = null;
     private Collection<OCLExpressionWithContext> metamodelExpressionList = null;
 
-    Resource ngpmModel = null;
-    List<RawNotification> modifyElementaryTypesTrace = null;
-
-
     @Override
     public void setUp(){
 	classTcsExpressionList = new OCLExpressionFromClassTcsPicker().pickUpExpressions();
 	metamodelExpressionList = new OCLExpressionFromModelPicker().pickUpExpressions();
-
-	ngpmModel = NotificationResourceLoader.loadModel("NgpmModel.xmi");
-	modifyElementaryTypesTrace = NotificationResourceLoader.loadTrace("modifyElementaryTypesEventTrace.trace");
     }
 
     @Test
     public void testProducingNavigationStepsForTcsExpressions(){
 	for (OCLExpressionWithContext expression : classTcsExpressionList) {
-	    assertNotNull(createNavigationPath(expression));
+	    tryToCreateNavigationPaths(expression);
 	}
     }
 
     @Test
     public void testProducingNavigationStepsForMetamodelExpressions(){
 	for(OCLExpressionWithContext expression : metamodelExpressionList){
-	    assertNotNull(createNavigationPath(expression));
+	    tryToCreateNavigationPaths(expression);
 	}
     }
 
-    @Test
-    public void testProducingNavigationStepsForTcsExpressionAttributeOrAssociationEndCalls(){
-	for (OCLExpressionWithContext expression : classTcsExpressionList) {
-	    FilterSynthesisImpl filterSynthesizer = new FilterSynthesisImpl(expression.getExpression(), false);
-	    filterSynthesizer.getSynthesisedFilter();
-
-	    for(RawNotification raw : modifyElementaryTypesTrace){
-        	    for(OCLExpression expr : getAttributeOrAssociationEndCalls(raw.convertToNotification(ngpmModel), filterSynthesizer)){
-              		PathCache cache = new PathCache(DefaultOppositeEndFinder.getInstance());
-                	assertNotNull(cache.getOrCreateNavigationPath(expr, expression.getContext(), filterSynthesizer, null));
-        	    }
-	    }
-	}
-    }
-
-    private NavigationStep createNavigationPath(OCLExpressionWithContext expression) {
+    @SuppressWarnings("unchecked")
+    private void tryToCreateNavigationPaths(OCLExpressionWithContext expression) {
 	FilterSynthesisImpl filterSynthesizer = new FilterSynthesisImpl(expression.getExpression(), false);
 	filterSynthesizer.getSynthesisedFilter();
 
-	PathCache cache = new PathCache(DefaultOppositeEndFinder.getInstance());
-	return cache.getOrCreateNavigationPath(expression.getExpression(), expression.getContext(), filterSynthesizer, null);
+	Map<EAttribute, Set<PropertyCallExp>> attributeCallExpressions = (Map<EAttribute, Set<PropertyCallExp>>)dirtyReflectionAttributeReader("attributeCallExpressions", filterSynthesizer);
+	Map<EReference, Set<NavigationCallExp>> associationEndCallExpressions = (Map<EReference, Set<NavigationCallExp>>)dirtyReflectionAttributeReader("associationEndCallExpressions", filterSynthesizer);
+
+	for(Set<PropertyCallExp> callExpressionSet : attributeCallExpressions.values()){
+	    for(PropertyCallExp callExpression : callExpressionSet){
+		PathCache cache = new PathCache(DefaultOppositeEndFinder.getInstance());
+		assertNotNull(cache.getOrCreateNavigationPath(callExpression, expression.getContext(), filterSynthesizer, null));
+	    }
+	}
+
+	for(Set<NavigationCallExp> callExpressionSet : associationEndCallExpressions.values()){
+	    for(NavigationCallExp callExpression : callExpressionSet){
+		PathCache cache = new PathCache(DefaultOppositeEndFinder.getInstance());
+		assertNotNull(cache.getOrCreateNavigationPath(callExpression, expression.getContext(), filterSynthesizer, null));
+	    }
+	}
+
     }
 
-    /**
-     * Finds all attribute and association end call expressions in <tt>expression</tt> that are affected by the
-     * <tt>changeEvent</tt>. The result is always non-<tt>null</tt> but may be empty.
-     */
-    private Set<? extends NavigationCallExp> getAttributeOrAssociationEndCalls(Notification changeEvent, FilterSynthesisImpl filterSynthesizer) {
-        Set<? extends NavigationCallExp> result;
-        if (NotificationHelper.isAttributeValueChangeEvent(changeEvent)) {
-            result = filterSynthesizer.getAttributeCallExpressions((EAttribute) NotificationHelper
-                    .getNotificationFeature(changeEvent));
-        } else if (NotificationHelper.isLinkLifeCycleEvent(changeEvent)) {
-            EReference ref = (EReference) NotificationHelper.getNotificationFeature(changeEvent);
 
-            Set<NavigationCallExp> localResult = new HashSet<NavigationCallExp>();
-            localResult.addAll(filterSynthesizer.getAssociationEndCallExpressions(ref));
-            result = localResult;
-        } else {
-            result = Collections.emptySet();
-        }
-        return result;
+    private Object dirtyReflectionAttributeReader(String attribute, Object target){
+	try {
+	    Field field = target.getClass().getDeclaredField(attribute);
+	    field.setAccessible(true);
+	    return field.get(target);
+
+	} catch (SecurityException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IllegalArgumentException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IllegalAccessException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (NoSuchFieldException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+	throw new RuntimeException("Cannot reflect on " + target);
     }
 
 }
