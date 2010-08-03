f681778088a0a4cd188f19771097ec3e196a3e86
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-08-03 14:45:09 +0000

    don't use the extent map anymore
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1376 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ de.hpi.sam.bp2009.solution.oclToMqlMapping/src/de/hpi/sam/bp2009/solution/oclToMqlMapping/impl/MappingOCLEnvironmentFactory.java
@@ -0,0 +1,34 @@
+package de.hpi.sam.bp2009.solution.oclToMqlMapping.impl;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EOperation;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EParameter;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.ocl.Environment;
+import org.eclipse.ocl.EvaluationEnvironment;
+import org.eclipse.ocl.EvaluationVisitor;
+import org.eclipse.ocl.ecore.CallOperationAction;
+import org.eclipse.ocl.ecore.Constraint;
+import org.eclipse.ocl.ecore.SendSignalAction;
+
+import com.sap.emf.ocl.hiddenopposites.EcoreEnvironmentFactoryWithHiddenOpposites;
+
+public class MappingOCLEnvironmentFactory extends EcoreEnvironmentFactoryWithHiddenOpposites {
+    public static MappingOCLEnvironmentFactory INSTANCE = new MappingOCLEnvironmentFactory();
+    
+    @Override
+    public EvaluationVisitor<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> createEvaluationVisitor(
+            Environment<EPackage, EClassifier, EOperation, EStructuralFeature, EEnumLiteral, EParameter, EObject, CallOperationAction, SendSignalAction, Constraint, EClass, EObject> env,
+            EvaluationEnvironment<EClassifier, EOperation, EStructuralFeature, EClass, EObject> evalEnv,
+            Map<? extends EClass, ? extends Set<? extends EObject>> extentMap) {
+        return new MappingEvaluationVisitor(env, evalEnv, extentMap);
+    }
+
+}
