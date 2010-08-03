355e82dc4fde79bd80734c45939b1b97075b88c0
Armagan Kilic <kilicarmagan@gmail.com>
2010-08-03 02:31:03 +0200

    model changes

---
+++ DSLEngineering/com.sap.furcas.metamodel/src/com/sap/furcas/metamodel/TCS/DisambiguatePArg.java
@@ -0,0 +1,53 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.sap.furcas.metamodel.TCS;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Disambiguate PArg</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link com.sap.furcas.metamodel.TCS.DisambiguatePArg#getDisambiguation <em>Disambiguation</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see com.sap.furcas.metamodel.TCS.TCSPackage#getDisambiguatePArg()
+ * @model
+ * @generated
+ */
+public interface DisambiguatePArg extends PropertyArg {
+
+	/**
+	 * Returns the value of the '<em><b>Disambiguation</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Disambiguation</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Disambiguation</em>' attribute.
+	 * @see #setDisambiguation(String)
+	 * @see com.sap.furcas.metamodel.TCS.TCSPackage#getDisambiguatePArg_Disambiguation()
+	 * @model
+	 * @generated
+	 */
+	String getDisambiguation();
+
+	/**
+	 * Sets the value of the '{@link com.sap.furcas.metamodel.TCS.DisambiguatePArg#getDisambiguation <em>Disambiguation</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Disambiguation</em>' attribute.
+	 * @see #getDisambiguation()
+	 * @generated
+	 */
+	void setDisambiguation(String value);
+} // DisambiguatePArg
---
+++ DSLEngineering/com.sap.furcas.metamodel/src/com/sap/furcas/metamodel/TCS/impl/PartialPArgImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package com.sap.furcas.metamodel.TCS.impl;
+
+import com.sap.furcas.metamodel.TCS.PartialPArg;
+import com.sap.furcas.metamodel.TCS.TCSPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Partial PArg</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class PartialPArgImpl extends PropertyArgImpl implements PartialPArg {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected PartialPArgImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return TCSPackage.Literals.PARTIAL_PARG;
+	}
+
+} //PartialPArgImpl
