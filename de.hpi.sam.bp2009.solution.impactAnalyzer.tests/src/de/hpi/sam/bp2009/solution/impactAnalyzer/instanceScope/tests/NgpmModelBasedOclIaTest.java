4b1056736fa9cc71b568818c9ec568b2ea0dd746
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-08-05 13:54:56 +0000

    Added test case for a specific situation where the impact analysis seems to be slow due to large navigation step graphs.
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1391 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/NgpmModelBasedOclIaTest.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.tests/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/tests/NgpmModelBasedOclIaTest.java
@@ -14,9 +14,11 @@ import org.junit.Test;
 import data.classes.ClassesPackage;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.ImpactAnalyzer;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.ImpactAnalyzerFactory;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.model.ModelCloner;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.notifications.NotificationResourceLoader;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.notifications.RawNotification;
-
+import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.ocl.OCLExpressionFromClassTcsPicker;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.ocl.OCLExpressionWithContext;
 
 public class NgpmModelBasedOclIaTest extends TestCase {
     Resource ngpmModel = null;
@@ -27,17 +29,50 @@ public class NgpmModelBasedOclIaTest extends TestCase {
 	ngpmModel = NotificationResourceLoader.loadModel("NgpmModel.xmi");
 	modifyElementaryTypesTrace = NotificationResourceLoader.loadTrace("modifyElementaryTypesEventTrace.trace");
     }
-
     @Test
     public void testVariableExpressionWithCollectionType() {
 	OCLExpression exp = (OCLExpression) OclIaTest.parse("context NestedTypeDefinition inv: self.getNamedValuesInScope()", ClassesPackage.eINSTANCE).iterator().next().getSpecification().getBodyExpression();
 
 	RawNotification rawNotification = modifyElementaryTypesTrace.get(1);
-	Notification notification = rawNotification.convertToNotification(ngpmModel);
+	Notification notification = rawNotification.convertToNotification(ModelCloner.cloneResource(ngpmModel,"1"));
 
 	ImpactAnalyzer ia = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(exp, data.classes.ClassesPackage.eINSTANCE.getNestedTypeDefinition());
 	Collection<EObject> impact = ia.getContextObjects(notification);
 	assertEquals(impact.size(), 0);
     }
+
+    public void testSlowExpressions(){
+	OCLExpressionWithContext expr = new OCLExpressionFromClassTcsPicker().pickUpExpression(44);
+
+	System.out.println(expr);
+
+	Notification notification = getNotification(10, ngpmModel);
+
+	ImpactAnalyzer ia = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expr.getExpression(), expr.getContext());
+	Collection<EObject> impact = ia.getContextObjects(notification);
+	assertNotNull(impact);
+    }
+
+
+    private Notification getNotification(int id, Resource model){
+         Resource clonedModel = ModelCloner.cloneResource(model, String.valueOf(Math.random()));
+
+        for(int i = 0; i < (id - 1); i++) {
+                changeModel(i, clonedModel);
+        }
+        return changeModel(id - 1, clonedModel);
+    }
+
+        private Notification changeModel(int id, Resource clonedModel) {
+                RawNotification raw = modifyElementaryTypesTrace.get(id);
+
+        System.out.println(raw.getEventType());
+        if(raw.isSplitCandidate()){
+                System.out.println("candidate");
+                raw.split().get(0).convertToNotification(clonedModel);
+                return raw.split().get(1).convertToNotification(clonedModel);
+        } else
+	    return raw.convertToNotification(clonedModel);
+        }
 
 }
