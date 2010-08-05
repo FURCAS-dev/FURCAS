4dee9115dcfb8f82d2a054a0edf11a5eddacc84a
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-08-05 09:01:12 +0000

    Added new R scripts
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1387 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/generateAllPlots.R
@@ -0,0 +1,63 @@
+require(lattice)
+require(outliers)
+
+result = read.table("~/Dropbox/fullResult8.data", header = TRUE)
+
+result$iaEvalAndExecTime = result$executionTime + result$evaluationTimeAfter
+result$allInstancesEvalAndExecTime = result$allInstanceEvalTime + result$allInstanceExecTime
+
+attach(result)
+strippedResult = result[noAllInstances != allInstanceNoInvalidEvals, ]
+detach(result)
+
+attach(strippedResult)
+png("/tmp/plotScatterModelSize.png", width=1200, height=800)
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize | optionId, type=c("p", "r"))
+dev.off()
+
+png("/tmp/plotScatterOclId.png", width=1200, height=800)
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ oclId | optionId, type=c("p", "r"))
+dev.off()
+
+png("/tmp/plotScatterNotificationId.png", width=1200, height=800)
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ notificationId | optionId, type=c("p", "r"))
+dev.off()
+
+png("/tmp/plotScatterNoAllInstances.png", width=1200, height=800)
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ noAllInstances | optionId, type=c("p", "r"))
+dev.off()
+
+aggregatedResult = aggregate(strippedResult, by=list(modelSize, optionId), FUN=mean)
+detach(strippedResult)
+attach(aggregatedResult)
+
+png("/tmp/plotScatterModelSizeMean.png", width=1200, height=800)
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize | optionId, type=c("p", "r"))
+dev.off()
+
+detach(aggregatedResult)
+attach(strippedResult)
+aggregatedResult = aggregate(strippedResult, by=list(oclId, optionId), FUN = mean)
+detach(strippedResult)
+attach(aggregatedResult)
+
+png("/tmp/plotScatterOclMean.png", width=1200, height=800)
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ oclId | optionId, type=c("p", "r"))
+dev.off()
+
+detach(aggregatedResult)
+attach(strippedResult)
+aggregatedResult = aggregate(strippedResult, by=list(notificationId, optionId), FUN=mean)
+detach(strippedResult)
+attach(aggregatedResult)
+
+png("/tmp/plotScatterNotificationMean.png", width=1200, height=800)
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ notificationId | optionId, type=c("p", "r"))
+dev.off()
+
+detach(aggregatedResult)
+attach(strippedResult)
+png("/tmp/plot3dScatterOclNotification.png", width=1200, height=1200)
+cloud(allInstacesEvalAndExecTime + iaEvalAndExecTime ~ modelSize * oclId | optionId, data = strippedResult)
+dev.off()
+
