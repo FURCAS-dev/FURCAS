2db92fb7a2e315f64e9f6c0efc9cc01faa9c5a2c
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-08-08 17:27:10 +0000

    Modified R script for producing result plots
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1405 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/generateAllPlots.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/generateAllPlots.R
@@ -10,21 +10,31 @@ attach(result)
 strippedResult = result[noAllInstances != allInstanceNoInvalidEvals, ]
 detach(result)
 
+ylabEvalTime = "Evaluation time (total) in nanoseconds"
+
 attach(strippedResult)
 png("/tmp/plotScatterModelSize.png", width=1200, height=800)
-xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize | optionId, type=c("p", "r"))
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize | optionId, type=c("p", "r"),
+xlab = "Number of model elements (model size)",
+ylab = ylabEvalTime)
 dev.off()
 
 png("/tmp/plotScatterOclId.png", width=1200, height=800)
-xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ oclId | optionId, type=c("p", "r"))
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ oclId | optionId, type=c("p", "r"),
+xlab = "Different OCL Expressions",
+ylab = ylabEvalTime)
 dev.off()
 
 png("/tmp/plotScatterNotificationId.png", width=1200, height=800)
-xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ notificationId | optionId, type=c("p", "r"))
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ notificationId | optionId, type=c("p", "r"),
+xlab = "Different model change notifications",
+ylab = ylabEvalTime)
 dev.off()
 
 png("/tmp/plotScatterNoAllInstances.png", width=1200, height=800)
-xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ noAllInstances | optionId, type=c("p", "r"))
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ noAllInstances | optionId, type=c("p", "r"),
+xlab = "Number of all instances of context of OCL Expression"
+ylab = ylabEvalTime)
 dev.off()
 
 aggregatedResult = aggregate(strippedResult, by=list(modelSize, optionId), FUN=mean)
@@ -32,7 +42,10 @@ detach(strippedResult)
 attach(aggregatedResult)
 
 png("/tmp/plotScatterModelSizeMean.png", width=1200, height=800)
-xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize | optionId, type=c("p", "r"))
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize | optionId, type=c("p", "r"),
+xlab = "Number of model elements (model size)",
+ylab = "Meaned evaluation time (total) in nanoseconds"
+)
 dev.off()
 
 detach(aggregatedResult)
@@ -42,7 +55,9 @@ detach(strippedResult)
 attach(aggregatedResult)
 
 png("/tmp/plotScatterOclMean.png", width=1200, height=800)
-xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ oclId | optionId, type=c("p", "r"))
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ oclId | optionId, type=c("p", "r"),
+xlab = "Different OCL Expressions"
+ylab = "Meaned evaluation time (total) in nanoseconds"
 dev.off()
 
 detach(aggregatedResult)
@@ -52,7 +67,9 @@ detach(strippedResult)
 attach(aggregatedResult)
 
 png("/tmp/plotScatterNotificationMean.png", width=1200, height=800)
-xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ notificationId | optionId, type=c("p", "r"))
+xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ notificationId | optionId, type=c("p", "r"),
+xlab = "Different model change notifications",
+ylab = "Meaned evaluation time (total) in nanoseconds")
 dev.off()
 
 detach(aggregatedResult)
