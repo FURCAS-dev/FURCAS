252b7c81b8c993161cbd5fdeb84b2e032c312389
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-08-17 22:42:12 +0000

    utilities and test classes for NGPM metamodel
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1450 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ com.sap.ngpm.utils/src/com/sap/ap/metamodel/utils/MetamodelTests.java
@@ -0,0 +1,350 @@
+package com.sap.ap.metamodel.utils;
+
+import java.util.HashMap;
+
+import junit.framework.TestCase;
+import modelmanagement.ModelmanagementFactory;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+
+import behavioral.actions.ActionsFactory;
+import behavioral.actions.Block;
+import data.classes.Association;
+import data.classes.AssociationEnd;
+import data.classes.ClassesFactory;
+import data.classes.ClassesPackage;
+import data.classes.LinkSetting;
+import data.classes.LinkTraversal;
+import data.classes.MethodSignature;
+import data.classes.Parameter;
+import data.classes.SapClass;
+import data.classes.TypeAdapter;
+import data.classes.util.ClassesValidator;
+import de.hpi.sam.bp2009.solution.eventManager.EventManager;
+import de.hpi.sam.bp2009.solution.eventManager.EventManagerFactory;
+import de.hpi.sam.bp2009.solution.eventManager.filters.AssociationFilter;
+
+public class MetamodelTests extends TestCase {
+    private ResourceSet resourceSet;
+
+    public void setUp() {
+	resourceSet = new ResourceSetImpl();
+    }
+    
+    public void tearDown() {
+    }
+    
+    public void testMetamodelAvailability() {
+	EClass typeAdapter = ClassesPackage.eINSTANCE.getTypeAdapter();
+	assertEquals("TypeAdapter", typeAdapter.getName());
+    }
+
+    public void testLinkRemoveEventUponElementDelete() {
+	final boolean[] ok = new boolean[1];
+	
+	final SapClass clazz = ClassesFactory.eINSTANCE.createSapClass();
+	final TypeAdapter ta = ClassesFactory.eINSTANCE.createTypeAdapter();
+	EventManager em = EventManagerFactory.eINSTANCE.getEventManagerFor(resourceSet);
+	AssociationFilter filter = EventManagerFactory.eINSTANCE.createAssociationFilter();
+	filter.setFeature(ClassesPackage.eINSTANCE.getTypeAdapter_To());
+	em.subscribe(filter, new AdapterImpl() {
+	    public void notifyChanged(Notification event) {
+			if (event.getEventType() == Notification.UNSET) {
+			    ok[0] = ok[0] || event.getNotifier().equals(clazz) &&
+			    	             event.getOldValue().equals(ta);
+			}
+		    }
+	});
+	ta.setTo(clazz);
+	ta.setTo(null);
+	// If this test fails, check if in the target IDE there is an editor or the Runlet console open;
+	// this may turn off events during creating a deep copy / snapshot of the object under edit
+	// which temporarily disables the event handling required / tested here
+	assertTrue(ok[0]);
+    }
+
+    /**
+     * Asserts that two types don't just conform based on their signature structures
+     */
+    public void testNoStructuralConformance() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();;
+	c1.setName("C1");
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2");
+	assertFalse("Two classes that don't have an adapter between them must not conform",
+		c1.conformsTo(c2));
+    }
+
+    /**
+     * Asserts that two types conform if there is one adapter between them
+     */
+    public void testConformanceWithOneAdapter() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();
+	c1.setName("C1");
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2");
+	TypeAdapter adapter = ClassesFactory.eINSTANCE.createTypeAdapter();
+	adapter.setAdapted(c1);
+	adapter.setTo(c2);
+	assertTrue("Classes with no signatures and an adapter between them should conform",
+		c1.conformsTo(c2));
+    }
+    
+    /**
+     * Asserts that two types conform if there is one adapter between them
+     */
+    public void testConformanceWithSignaturesAndOneAdapter() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();
+	c1.setName("C1");
+	MethodSignature s1 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s1.setName("someSignature");
+	c1.getOwnedSignatures().add(s1);
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2");
+	MethodSignature s2 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s2.setName("someSignature");
+	c2.getOwnedSignatures().add(s2);
+	TypeAdapter adapter = ClassesFactory.eINSTANCE.createTypeAdapter();
+	adapter.setName("FromC1ToC2");
+	adapter.setAdapted(c1);
+	adapter.setTo(c2);
+	BasicDiagnostic diagnostics = new BasicDiagnostic();
+	ClassesValidator.INSTANCE.validate(adapter, diagnostics, new HashMap<Object, Object>());
+	assertTrue("Adapter should be valid", diagnostics.getSeverity() == Diagnostic.OK);
+	assertTrue("Classes with no signatures and an adapter between them should conform",
+		c1.conformsTo(c2));
+    }
+    
+    /**
+     * Check that adapter invariants are really enforced
+     */
+    public void testAdapterInvariantsEnforcement() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();
+	c1.setName("C1");
+	MethodSignature s1 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s1.setName("s1");
+	c1.getOwnedSignatures().add(s1);
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2");
+	MethodSignature s2 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s2.setName("s2");
+	c2.getOwnedSignatures().add(s2);
+	TypeAdapter adapter = ClassesFactory.eINSTANCE.createTypeAdapter();
+	adapter.setName("FromC1ToC2");
+	adapter.setAdapted(c1);
+	adapter.setTo(c2);
+        BasicDiagnostic diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(adapter, diagnostics, new HashMap<Object, Object>());
+	assertTrue("Adapter should be considered invalid and should violate the IsFullAdaptationToTo constraint",
+		diagnostics.getSeverity() != Diagnostic.OK &&
+		diagnostics.getChildren().iterator().next().getMessage().equals(
+			"[data, classes, TypeAdapter, IsFullAdaptationToTo]")
+		);
+    }
+    
+    /**
+     * Check that the constraint works that requires an association between two value classes to
+     * have at least one non-equality-relevant end
+     */
+    public void testDoubleEqualityRelevantValueClassAssociation() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();
+	c1.setName("C1");
+	c1.setValueType(true);
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2");
+	c2.setValueType(true);
+	Association a = MetamodelUtils.createAssociation(resourceSet, c1, "c1", 0, 1, true, false, false, false, c2, "c2", 0, 1, true, false, false, false);
+	a.setName("a");
+	modelmanagement.Package p = ModelmanagementFactory.eINSTANCE.createPackage();
+	p.setName("p");
+	c1.setPackage_(p);
+	c2.setPackage_(p);
+	p.getAssociations().add(a);
+	for (AssociationEnd ae:a.getEnds()) {
+	    ae.setContributesToEquality(true);
+	}
+        BasicDiagnostic diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(a, diagnostics, new HashMap<Object, Object>());
+	assertEquals("Expected exactly one constraint violation", 1, diagnostics.getChildren().size());
+	assertEquals("Associations between two value types that affect equality of both types are currently not supported. It would lead to a recursive equality definition and therefore to recursive value constructors.",
+		diagnostics.getChildren().iterator().next().getMessage());
+    }
+    
+    /**
+     * Check that no modifying feature of an association end can be exposed if that
+     * would modify a value
+     */
+    public void testValueImmutability() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();
+	c1.setName("C1Value");
+	c1.setValueType(true);
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2Entity");
+	c2.setValueType(false);
+	Association a = MetamodelUtils.createAssociation(resourceSet, c1, "c1", 0, 1, true, false, false, false, c2, "c2", 0, 1, true, false, false, false);
+	a.setName("a");
+	modelmanagement.Package p = ModelmanagementFactory.eINSTANCE.createPackage();
+	p.setName("p");
+	c1.setPackage_(p);
+	c2.setPackage_(p);
+	p.getAssociations().add(a);
+	assertEquals("c1", a.getEnds().get(0).getName());
+	a.getEnds().get(0).setContributesToEquality(true);
+	
+	// expose eqality-relevant association as getter (required!)
+	MethodSignature getter = ClassesFactory.eINSTANCE.createMethodSignature();
+	getter.setName("getC2");
+	getter.setOutput(a.getEnds().get(1).getType());
+	LinkTraversal linkTraversal = ClassesFactory.eINSTANCE.createLinkTraversal();
+	linkTraversal.setEnd(a.getEnds().get(1));
+	linkTraversal.setImplements_(getter);
+	
+	
+	c1.getOwnedSignatures().add(getter);
+	
+        BasicDiagnostic diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(a, diagnostics, new HashMap<Object, Object>());
+	assertEquals(Diagnostic.OK, diagnostics.getSeverity());
+        diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(c1, diagnostics, new HashMap<Object, Object>());
+        assertEquals(Diagnostic.OK, diagnostics.getSeverity());
+        diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(c2, diagnostics, new HashMap<Object, Object>());
+        assertEquals(Diagnostic.OK, diagnostics.getSeverity());
+	
+	// expose equality-relevant association as setter (illegal!)
+	MethodSignature setter = ClassesFactory.eINSTANCE.createMethodSignature();
+	setter.setName("setC2");
+	LinkSetting linkSetting = ClassesFactory.eINSTANCE.createLinkSetting();
+	linkSetting.setEnd(a.getEnds().get(1));
+	linkSetting.setImplements_(setter);
+	Parameter setterParam = ClassesFactory.eINSTANCE.createParameter();
+	setterParam.setName("c2Param");
+	setter.getInput().add(setterParam);
+	c1.getOwnedSignatures().add(setter);
+	
+        diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(c1, diagnostics, new HashMap<Object, Object>());
+        assertEquals(1, diagnostics.getChildren().size());
+	assertEquals("[data, classes, ExtentModifyingAssociationEndSignatureImplementation, MustNotModifyExtentIfEqualityRelevantForValueClass]",
+		diagnostics.getChildren().iterator().next().getMessage());
+    }
+    
+    /**
+     * Check that adapter invariants are really enforced
+     */
+    public void testEnforcementNoAbstractSignaturesInAdapter() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();
+	c1.setName("C1");
+	MethodSignature s1 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s1.setName("s1");
+	c1.getOwnedSignatures().add(s1);
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2");
+	MethodSignature s2 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s2.setName("s2");
+	c2.getOwnedSignatures().add(s2);
+	TypeAdapter adapter = ClassesFactory.eINSTANCE.createTypeAdapter();
+	adapter.setName("FromC1ToC2");
+	adapter.setAdapted(c1);
+	adapter.setTo(c2);
+	MethodSignature adapterSig = ClassesFactory.eINSTANCE.createMethodSignature();
+	adapterSig.setName("s2");
+	adapter.getOwnedSignatures().add(adapterSig);
+        BasicDiagnostic diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(adapter, diagnostics, new HashMap<Object, Object>());
+	assertTrue("Adapter should be considered invalid and should violate the SignatureCannotBeAbstract constraint",
+		diagnostics.getChildren().size() == 1 && diagnostics.getChildren().iterator().next().getMessage().equals(
+			"Signatures can not be abstract"));
+    }
+    
+    /**
+     * Check that adapter invariants are really enforced
+     */
+    public void testConformanceWithoutSignatureAdaptation() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();
+	c1.setName("C1");
+	MethodSignature s1 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s1.setName("s1");
+	c1.getOwnedSignatures().add(s1);
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2");
+	MethodSignature s2 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s2.setName("s2");
+	c2.getOwnedSignatures().add(s2);
+	TypeAdapter adapter = ClassesFactory.eINSTANCE.createTypeAdapter();
+	adapter.setName("FromC1ToC2");
+	adapter.setAdapted(c1);
+	adapter.setTo(c2);
+	MethodSignature adapterSig = ClassesFactory.eINSTANCE.createMethodSignature();
+	adapterSig.setName("s2");
+	Block impl = ActionsFactory.eINSTANCE.createBlock();
+	impl.setImplements_(adapterSig);
+	
+	adapter.getOwnedSignatures().add(adapterSig);
+        BasicDiagnostic diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(adapter, diagnostics, new HashMap<Object, Object>());
+	assertEquals("Adapter should be valid", Diagnostic.OK, diagnostics.getSeverity());
+	assertTrue("Classes with no signatures and an adapter between them should conform",
+		c1.conformsTo(c2));
+    }
+    
+    /**
+     * Check that adapter invariants are really enforced
+     */
+    public void testConformanceAlongAdapterChain() {
+	SapClass c1 = ClassesFactory.eINSTANCE.createSapClass();
+	c1.setName("C1");
+	MethodSignature s1 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s1.setName("s1");
+	c1.getOwnedSignatures().add(s1);
+	SapClass c2 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C2");
+	MethodSignature s2 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s2.setName("s2");
+	c2.getOwnedSignatures().add(s2);
+	TypeAdapter adapter = ClassesFactory.eINSTANCE.createTypeAdapter();
+	adapter.setName("FromC1ToC2");
+	adapter.setAdapted(c1);
+	adapter.setTo(c2);
+	MethodSignature adapterSig = ClassesFactory.eINSTANCE.createMethodSignature();
+	adapterSig.setName("s2");
+	Block impl = ActionsFactory.eINSTANCE.createBlock();
+	
+	impl.setImplements_(adapterSig);
+		
+	adapter.getOwnedSignatures().add(adapterSig);
+	SapClass c3 = ClassesFactory.eINSTANCE.createSapClass();
+	c2.setName("C3");
+	MethodSignature s3 = ClassesFactory.eINSTANCE.createMethodSignature();
+	s2.setName("s3");
+	c3.getOwnedSignatures().add(s3);
+	TypeAdapter adapter2 = ClassesFactory.eINSTANCE.createTypeAdapter();
+	adapter2.setName("FromC2ToC3");
+	adapter2.setAdapted(c2);
+	adapter2.setTo(c3);
+	MethodSignature adapter2Sig = ClassesFactory.eINSTANCE.createMethodSignature();
+	adapterSig.setName("s3");
+	Block impl2 = ActionsFactory.eINSTANCE.createBlock();
+	
+	impl2.setImplements_(adapter2Sig);
+	
+	
+	adapter2.getOwnedSignatures().add(adapter2Sig);
+        BasicDiagnostic diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(adapter, diagnostics, new HashMap<Object, Object>());
+        assertEquals("Adapter should be valid", Diagnostic.OK, diagnostics.getSeverity());
+        diagnostics = new BasicDiagnostic();
+        ClassesValidator.INSTANCE.validate(adapter2, diagnostics, new HashMap<Object, Object>());
+        assertEquals("Adapter should be valid", Diagnostic.OK, diagnostics.getSeverity());
+	assertTrue("Classes with no signatures and an adapter between them should conform",
+		c1.conformsTo(c3));
+    }
+    
+}
---
+++ com.sap.ngpm.utils/src/com/sap/ap/metamodel/utils/MetamodelUtils.java
@@ -0,0 +1,164 @@
+package com.sap.ap.metamodel.utils;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.query.index.ui.IndexFactory;
+import org.eclipse.emf.query2.QueryProcessor;
+import org.eclipse.emf.query2.QueryProcessorFactory;
+import org.eclipse.emf.query2.ResultSet;
+
+import data.classes.Association;
+import data.classes.AssociationEnd;
+import data.classes.ClassTypeDefinition;
+import data.classes.ClassesFactory;
+import data.classes.FunctionSignature;
+import data.classes.FunctionSignatureTypeDefinition;
+import data.classes.MethodSignature;
+import data.classes.SapClass;
+import dataaccess.expressions.ExpressionsFactory;
+import dataaccess.expressions.FunctionCallExpression;
+import dataaccess.expressions.SignatureCallExpression;
+import dataaccess.expressions.fp.AnonymousFunctionExpr;
+import dataaccess.expressions.fp.FpFactory;
+import dataaccess.expressions.literals.LiteralsFactory;
+import dataaccess.expressions.literals.NumberLiteral;
+import dataaccess.expressions.literals.StringLiteral;
+import de.hpi.sam.bp2009.solution.queryContextScopeProvider.impl.ProjectBasedQueryContextScopeProviderImpl;
+
+/**
+ * Some utility / helper methods for the AP/NGPM/Runlet metamodel
+ * 
+ * @author Axel Uhl D043530
+ *
+ */
+public class MetamodelUtils {
+    public static NumberLiteral createNumberLiteral(ResourceSet resourceSet, String numberAsString) {
+	NumberLiteral result = LiteralsFactory.eINSTANCE.createNumberLiteral();
+	result.setLiteral(numberAsString);
+	SapClass numberClass = MetamodelUtils.findClass(resourceSet, "Number");
+	ClassTypeDefinition ctd = MetamodelUtils.createClassTypeDefinitionExactlyOne(resourceSet, numberClass);
+	result.setOwnedTypeDefinition(ctd);
+	return result;
+    }
+    
+    public static StringLiteral createStringLiteral(ResourceSet resourceSet, String string) {
+	StringLiteral result = LiteralsFactory.eINSTANCE.createStringLiteral();
+	result.setLiteral(string);
+	SapClass stringClass = MetamodelUtils.findClass(resourceSet, "String");
+	ClassTypeDefinition ctd = MetamodelUtils.createClassTypeDefinitionExactlyOne(resourceSet, stringClass);
+	result.setOwnedTypeDefinition(ctd);
+	return result;
+    }
+    
+    public static ClassTypeDefinition createClassTypeDefinition(ResourceSet resourceSet, SapClass c1, int lower, int upper,
+	    boolean ordered, boolean unique) {
+	ClassTypeDefinition c1TD = ClassesFactory.eINSTANCE.createClassTypeDefinition();
+	c1TD.setClazz(c1);
+	c1TD.setLowerMultiplicity(lower);
+	c1TD.setUpperMultiplicity(upper);
+	c1TD.setOrdered(ordered);
+	c1TD.setUnique(unique);
+	return c1TD;
+    }
+
+    /**
+     * orderedness and uniqueness are left at their initial values, which means both are <tt>false</tt>
+     */
+    public static ClassTypeDefinition createClassTypeDefinition(ResourceSet resourceSet, SapClass c1, int lower, int upper) {
+	return createClassTypeDefinition(resourceSet, c1, lower, upper, /* ordered */ false, /* unique */ false);
+    }
+
+    public static ClassTypeDefinition createClassTypeDefinitionExactlyOne(
+	    ResourceSet resourceSet, SapClass theClass) {
+	return createClassTypeDefinition(resourceSet, theClass, 1, 1);
+    }
+
+    public static Association createAssociation(ResourceSet resourceSet, SapClass end1Class, String end1Name, int end1Lower,
+	    int end1Upper, boolean end1Navigability, boolean end1Composite, boolean end1Ordered,
+	    boolean end1Unique, SapClass end2Class, String end2Name, int end2Lower, int end2Upper,
+	    boolean end2Navigable, boolean end2Composite, boolean end2Ordered, boolean end2Unique) {
+	Association assoc = ClassesFactory.eINSTANCE.createAssociation();
+	AssociationEnd end1 = ClassesFactory.eINSTANCE.createAssociationEnd();
+	end1.setName(end1Name);
+	end1.setNavigable(end1Navigability);
+	end1.setComposite(end1Composite);
+	AssociationEnd end2 = ClassesFactory.eINSTANCE.createAssociationEnd();
+	end2.setName(end2Name);
+	end2.setNavigable(end2Navigable);
+	end2.setComposite(end2Composite);
+	assoc.getEnds().add(end1);
+	assoc.getEnds().add(end2);
+	ClassTypeDefinition end1TD = MetamodelUtils.createClassTypeDefinition(resourceSet, end1Class, end1Lower, end1Upper, end1Ordered, end1Unique);
+	end1.setType(end1TD);
+	ClassTypeDefinition end2TD = MetamodelUtils.createClassTypeDefinition(resourceSet, end2Class, end2Lower, end2Upper, end2Ordered, end2Unique);
+	end2.setType(end2TD);
+	return assoc;
+    }
+
+    public static Association createAssociation(ResourceSet resourceSet, SapClass end1Class, int end1Lower, int end1Upper,
+	    boolean end1Navigability, SapClass end2Class, int end2Lower, int end2Upper, boolean end2Navigable) {
+	Association assoc = ClassesFactory.eINSTANCE.createAssociation();
+	AssociationEnd end1 = ClassesFactory.eINSTANCE.createAssociationEnd();
+	end1.setNavigable(end1Navigability);
+	AssociationEnd end2 = ClassesFactory.eINSTANCE.createAssociationEnd();
+	end2.setNavigable(end2Navigable);
+	assoc.getEnds().add(end1);
+	assoc.getEnds().add(end2);
+	ClassTypeDefinition end1TD = MetamodelUtils.createClassTypeDefinition(resourceSet, end1Class, end1Lower, end1Upper);
+	end1.setType(end1TD);
+	ClassTypeDefinition end2TD = MetamodelUtils.createClassTypeDefinition(resourceSet, end2Class, end2Lower, end2Upper);
+	end2.setType(end2TD);
+	return assoc;
+    }
+
+    // TODO may need a combination of ResourceSet and scope as argument
+    public static MethodSignature findMethod(ResourceSet resourceSet, String classname, String methodname) {
+        QueryProcessor mql = QueryProcessorFactory.getDefault().createQueryProcessor(IndexFactory.getInstance());
+	ResultSet methodSignatureQueryResult = mql.execute(
+	    "select s from data::classes::MethodSignature as s, "+
+	    "data::classes::SapClass as c where s.owner = c where s.name='"+methodname+"' "+
+	    "where c.name='"+classname+"'",
+	    new ProjectBasedQueryContextScopeProviderImpl(resourceSet).getForwardScopeAsQueryContext());
+	MethodSignature methodSignature = null;
+	URI[] array = methodSignatureQueryResult.getUris("s");
+	if (array.length > 0) {
+	    methodSignature = (MethodSignature) resourceSet.getEObject(array[0], /* loadOnDemand */ true);
+	}
+	return methodSignature;
+    }
+
+    // TODO may need a combination of ResourceSet and scope as argument
+    public static SapClass findClass(ResourceSet resourceSet, String classname) {
+        QueryProcessor mql = QueryProcessorFactory.getDefault().createQueryProcessor(IndexFactory.getInstance());
+	ResultSet queryResult = mql.execute(
+	    "select c from data::classes::SapClass as c where for c(name='"+classname+"')",
+            new ProjectBasedQueryContextScopeProviderImpl(resourceSet).getForwardScopeAsQueryContext());
+	SapClass clazz = (SapClass) resourceSet.getEObject(queryResult.getUris("c")[0], /* loadOnDemand */ true);
+	return clazz;
+    }
+
+    /**
+     * Creates a {@link FunctionCallExpression} for the <tt>funcSig</tt> signature.
+     * For this purpose, this method creates a {@link FunctionSignatureTypeDefinition}
+     * that it initializes as required, and an {@link AnonymousFunctionExpr} which computes
+     * as its value the implementation of <tt>funcSig</tt>. This anonymous function expression
+     * is then used as the {@link FunctionCallExpression#setCalledBlock(dataaccess.expressions.Expression)}
+     * feature.<p>
+     * 
+     * The caller will have to set parameters for the call using
+     * {@link SignatureCallExpression#getParameters()}.
+     */
+    public static FunctionCallExpression createFunctionCallExpression(
+	    ResourceSet resourceSet, FunctionSignature funcSig) {
+	FunctionCallExpression fce = ExpressionsFactory.eINSTANCE.createFunctionCallExpression();
+	FunctionSignatureTypeDefinition fstd = ClassesFactory.eINSTANCE.createFunctionSignatureTypeDefinition();
+	fstd.setLowerMultiplicity(1);
+	fstd.setUpperMultiplicity(1);
+	fstd.setSignature(funcSig);
+	AnonymousFunctionExpr afe = FpFactory.eINSTANCE.createAnonymousFunctionExpr();
+	afe.setOwnedTypeDefinition(fstd);
+	fce.setCalledBlock(afe);
+	return fce;
+    }
+
+}
