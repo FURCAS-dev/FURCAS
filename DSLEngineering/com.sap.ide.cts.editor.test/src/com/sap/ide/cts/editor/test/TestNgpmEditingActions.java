63c77c52f2574ae66ca90664cea6641323df14a0
unknown <goldschm@.fzi.de>
2010-08-18 08:29:33 +0200

    moved fixed test

--- DSLEngineering/com.sap.ide.cts.editor.test/src/com/sap/ide/cts/editor/test/TestNgpmEditingActions.java
+++ DSLEngineering/com.sap.ide.cts.editor.test/src/com/sap/ide/cts/editor/test/TestNgpmEditingActions.java
@@ -53,12 +53,54 @@ import data.classes.Parameter;
 import data.classes.SapClass;
 import data.classes.TypeDefinition;
 import dataaccess.expressions.Equals;
+import dataaccess.expressions.Expression;
 import dataaccess.expressions.MethodCallExpression;
 import dataaccess.expressions.This;
 import dataaccess.expressions.VariableExpression;
+import dataaccess.expressions.literals.NumberLiteral;
 
 public class TestNgpmEditingActions extends RunletEditorTest {
-    
+    /**
+    I tried to add a 2.times(...) around an existing Number expression
+
+    */
+    @Test
+    public void testFailingExpressionTransformation() throws PartInitException, BadLocationException, CoreException {
+        // Source / Copy of: PF.IDE:E012BF1E67C2AFF0A53B11DFBFC6C9820A128A57
+        final SapClass refObject = findClass("SebastiansCCpy0");
+        assertNotNull(refObject); 
+        assertTrue(refObject.is___Alive()); 
+        AbstractGrammarBasedEditor editor = openEditor(refObject);
+        CtsDocument document = getDocument(editor);
+        document.replace(56, 0, "2");
+        document.replace(57, 0, ".");
+        document.replace(58, 0, "t");
+        document.replace(59, 0, "i");
+        document.replace(60, 0, "m");
+        document.replace(61, 0, "e");
+        document.replace(62, 0, "s");
+        document.replace(63, 0, "()");
+        document.replace(80, 1, ")");
+        document.replace(64, 1, "");
+        document.replace(80, 0, "l");
+        document.replace(80, 1, "");
+        document.replace(80, 0, ";");
+        saveAll(editor);
+        failOnError(editor);
+        assertTrue(refObject.is___Alive());
+        MethodSignature sLength = null;
+        for (MethodSignature ms : refObject.getOwnedSignatures()) {
+            if (ms.getName().equals("sLength")) {
+                sLength = ms;
+                break;
+            }
+        }
+        Expression sourceForMethodCall = ((MethodCallExpression) ((Return) ((Block) sLength.getImplementation()).getStatements().iterator().next()).
+                getArgument()).getObject();
+        assertTrue(sourceForMethodCall instanceof NumberLiteral);
+        assertEquals("2", ((NumberLiteral) sourceForMethodCall).getLiteral());
+        close(editor);
+    };
     /**
      * When a parameter that is used as object for a method call changes its multiplicity from 0..1 to 1..1, the output
      * multiplicity of calling a method with 1..1 output multiplicity should change to 1..1.
