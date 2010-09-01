0970a468baccdd1bc18a523a5a56e42a54da4e1f
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-01 10:24:20 +0000

    added a test case which actually is more a query2 test than an opposite end finder test; it tests looking up an opposite end using the old code based on query2 rather than an EPackageRegistry-based caching approach.
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1512 3ca8e212-6386-4b1e-b09d-39f68028012c

--- com.sap.ocl.oppositefinder.query2.test/src/com/sap/ocl/oppositefinder/query2/test/TestQuery2OppositeEndFinder.java
+++ com.sap.ocl.oppositefinder.query2.test/src/com/sap/ocl/oppositefinder/query2/test/TestQuery2OppositeEndFinder.java
@@ -1,17 +1,29 @@
 package com.sap.ocl.oppositefinder.query2.test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import junit.framework.TestCase;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.emf.query.index.ui.IndexFactory;
+import org.eclipse.emf.query2.QueryProcessorFactory;
+import org.eclipse.emf.query2.ResultSet;
+import org.eclipse.ocl.ecore.VoidType;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.sap.ocl.oppositefinder.query2.DefaultQueryContextProvider;
 import com.sap.ocl.oppositefinder.query2.Query2OppositeEndFinder;
 import company.CompanyFactory;
 import company.CompanyPackage;
@@ -58,4 +70,50 @@ public class TestQuery2OppositeEndFinder extends TestCase {
         assertTrue(allEmployees.contains(student));
         assertTrue(allEmployees.contains(freelance));
     }
+    
+    @Test
+    public void testFindOppositeEnds() {
+        List<EStructuralFeature> ends = new ArrayList<EStructuralFeature>();
+        findOppositeEnds(CompanyPackage.eINSTANCE.getDepartment(), "department2division", ends);
+        assertEquals(1, ends.size());
+        assertEquals(CompanyPackage.eINSTANCE.getDivision_Department(), ends.get(0));
+    }
+    
+    private void findOppositeEnds(EClassifier classifier, String name, List<EStructuralFeature> ends) {
+
+        if (!(classifier instanceof VoidType)) { // OclVoid has no properties, not even opposites
+            ResourceSet rs = classifier.eResource().getResourceSet();
+            if (rs == null) {
+                rs = new ResourceSetImpl();
+            }
+            StringBuilder allClassifierSupertypeUris = new StringBuilder();
+            allClassifierSupertypeUris.append('[');
+            allClassifierSupertypeUris.append(EcoreUtil.getURI(classifier));
+            allClassifierSupertypeUris.append("]"); //$NON-NLS-1$
+            if (classifier instanceof EClass) { // don't do this for the VoidType
+                for (EClass supertype : ((EClass) classifier).getEAllSuperTypes()) {
+                    allClassifierSupertypeUris.append(',');
+                    allClassifierSupertypeUris.append('[');
+                    allClassifierSupertypeUris.append(EcoreUtil.getURI(supertype));
+                    allClassifierSupertypeUris.append(']');
+                }
+            }
+            final ResultSet result = QueryProcessorFactory.getDefault().createQueryProcessor(IndexFactory.getInstance()).execute(
+                    "select oppositeParent from [http://www.eclipse.org/emf/2002/Ecore#//EReference] as oppositeParent, " + //$NON-NLS-1$
+                            "[http://www.eclipse.org/emf/2002/Ecore#//EAnnotation] as annotation, " + //$NON-NLS-1$
+                            "[http://www.eclipse.org/emf/2002/Ecore#//EStringToStringMapEntry] as detail, " + //$NON-NLS-1$
+                            "[http://www.eclipse.org/emf/2002/Ecore#//EClassifier] as classifier in elements {" + //$NON-NLS-1$
+                            allClassifierSupertypeUris + "} " + //$NON-NLS-1$
+                            "where oppositeParent.eAnnotations = annotation " + //$NON-NLS-1$
+                            "where annotation.details = detail " + //$NON-NLS-1$
+                            "where detail.key = 'Property.oppositeRoleName'" + //$NON-NLS-1$
+                            "where detail.value = '" + name + "' " + //$NON-NLS-1$ //$NON-NLS-2$
+                            "where oppositeParent.eType = classifier", //$NON-NLS-1$
+                    new DefaultQueryContextProvider().getForwardScopeQueryContext(rs));
+            for (int i = 0; i < result.getSize(); i++) {
+                ends.add((EReference) rs.getEObject(result.getUri(i, "oppositeParent"), /* loadOnDemand */true)); //$NON-NLS-1$
+            }
+        }
+    }
+    
 }
