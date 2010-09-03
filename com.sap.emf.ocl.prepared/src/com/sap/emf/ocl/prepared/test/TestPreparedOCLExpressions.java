44159a12e286ee673f7e90b816a35c05d003f7e2
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-03 20:42:15 +0000

    added more tests and a launch config
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1534 3ca8e212-6386-4b1e-b09d-39f68028012c

--- com.sap.emf.ocl.prepared/src/com/sap/emf/ocl/prepared/test/TestPreparedOCLExpressions.java
+++ com.sap.emf.ocl.prepared/src/com/sap/emf/ocl/prepared/test/TestPreparedOCLExpressions.java
@@ -5,10 +5,12 @@ import junit.framework.TestCase;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.ocl.ParserException;
+import org.eclipse.ocl.ecore.BooleanLiteralExp;
 import org.eclipse.ocl.ecore.IntegerLiteralExp;
 import org.eclipse.ocl.ecore.OCL.Helper;
 import org.eclipse.ocl.ecore.OCLExpression;
 import org.eclipse.ocl.ecore.OperationCallExp;
+import org.eclipse.ocl.ecore.StringLiteralExp;
 import org.junit.Test;
 
 import com.sap.emf.ocl.hiddenopposites.OCLWithHiddenOpposites;
@@ -16,7 +18,7 @@ import com.sap.emf.ocl.prepared.PreparedOCLExpression;
 
 public class TestPreparedOCLExpressions extends TestCase {
     @Test
-    public void testUnparameterizedExpression() throws ParserException {
+    public void testIntegerParameterizedExpression() throws ParserException {
         Helper helper = OCLWithHiddenOpposites.newInstance().createOCLHelper();
         EClass eClassClass = EcorePackage.eINSTANCE.getEClass();
         helper.setContext(eClassClass);
@@ -26,4 +28,29 @@ public class TestPreparedOCLExpressions extends TestCase {
         assertEquals(true, prepared.evaluate(eClassClass, 3));
         assertEquals(false, prepared.evaluate(eClassClass, 100));
     }
+
+    @Test
+    public void testBooleanParameterizedExpression() throws ParserException {
+        Helper helper = OCLWithHiddenOpposites.newInstance().createOCLHelper();
+        EClass eClassClass = EcorePackage.eINSTANCE.getEClass();
+        helper.setContext(eClassClass);
+        OCLExpression e = helper.createQuery("true and self.eStructuralFeatures->size() > 3");
+        BooleanLiteralExp ble = (BooleanLiteralExp) ((OperationCallExp) e).getSource();
+        PreparedOCLExpression prepared = new PreparedOCLExpression(e, ble);
+        assertEquals(true, prepared.evaluate(eClassClass, true));
+        assertEquals(false, prepared.evaluate(eClassClass, false));
+    }
+
+    @Test
+    public void testStringParameterizedExpression() throws ParserException {
+        Helper helper = OCLWithHiddenOpposites.newInstance().createOCLHelper();
+        EClass eClassClass = EcorePackage.eINSTANCE.getEClass();
+        helper.setContext(eClassClass);
+        OCLExpression e = helper.createQuery("self.name = '?'");
+        StringLiteralExp ble = (StringLiteralExp) ((OperationCallExp) e).getArgument().get(0);
+        PreparedOCLExpression prepared = new PreparedOCLExpression(e, ble);
+        assertEquals(true, prepared.evaluate(eClassClass, "EClass"));
+        assertEquals(false, prepared.evaluate(eClassClass, "SomethingElse"));
+    }
+
 }
