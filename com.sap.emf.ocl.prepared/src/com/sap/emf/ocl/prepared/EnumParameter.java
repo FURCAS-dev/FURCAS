73d466f5824d5ab2e5439052721638f6f3dcd4e2
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-03 19:44:53 +0000

    First steps towards prepared OCL expressions, required as a prerequisite for FURCAS migration. Moved the OclHelper which knows how to compute an expression's root expression to here.
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1531 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ com.sap.emf.ocl.prepared/src/com/sap/emf/ocl/prepared/EnumParameter.java
@@ -0,0 +1,21 @@
+package com.sap.emf.ocl.prepared;
+
+import org.eclipse.emf.ecore.EEnumLiteral;
+import org.eclipse.ocl.ecore.EnumLiteralExp;
+
+public class EnumParameter implements Parameter {
+    private EnumLiteralExp param;
+
+    public EnumParameter(EnumLiteralExp param) {
+        this.param = param;
+    }
+
+    @Override
+    public void set(Object object) {
+        setLiteral((EEnumLiteral) object);
+    }
+
+    private void setLiteral(EEnumLiteral value) {
+        param.setReferredEnumLiteral(value);
+    }
+}
---
+++ com.sap.emf.ocl.prepared/src/com/sap/emf/ocl/prepared/OclHelper.java
@@ -0,0 +1,53 @@
+package com.sap.emf.ocl.prepared;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+
+import org.eclipse.emf.ecore.EAnnotation;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.ocl.ecore.Constraint;
+import org.eclipse.ocl.ecore.ExpressionInOCL;
+import org.eclipse.ocl.ecore.OCLExpression;
+
+public class OclHelper {
+    private static OclHelper instance = new OclHelper();
+    
+    public static OclHelper getInstance() {
+        return instance;
+    }
+
+    public static OCLExpression getRootExpression(EObject x) {
+        EObject parent = (EObject) x.eContainer();
+        // The root expression could be contained in an Constraint or an EAnnotation. Therefore stop ascending
+        // if parent is instance of a container type.
+        while (parent != null && !(parent instanceof Constraint) && !(parent instanceof EAnnotation)
+                && !(parent instanceof ExpressionInOCL)) {
+            x = parent;
+            parent = (EObject) x.eContainer();
+        }
+        if (x instanceof OCLExpression) {
+            return (OCLExpression) x;
+        }
+        throw new IllegalArgumentException();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static Collection<Object> flatten(Object o) {
+        Collection<Object> result;
+        if (o instanceof Collection<?>) {
+            if (((Collection<?>) o).isEmpty()) {
+                result = (Collection<Object>) o;
+            } else {
+                result = new HashSet<Object>();
+                for (Object subO : (Collection<?>) o) {
+                    result.addAll(flatten(subO));
+                }
+            }
+        } else {
+            result = Collections.singleton(o);
+        }
+        return result;
+    }
+
+}
