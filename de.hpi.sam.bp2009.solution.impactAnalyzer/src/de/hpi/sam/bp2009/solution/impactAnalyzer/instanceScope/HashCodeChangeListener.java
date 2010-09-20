efb95ca33bb1048873ea2fdb5d47a07cc02152d6
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-20 13:57:05 +0000

    updated Javadoc
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1594 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/HashCodeChangeListener.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/HashCodeChangeListener.java
@@ -1,11 +1,13 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope;
 
+import de.hpi.sam.bp2009.solution.impactAnalyzer.util.SemanticIdentity;
+
 public interface HashCodeChangeListener {
     /**
-     * Invoked before <code>step</code> changes its hash code. When this method is called for a step, the
-     * {@link #afterHashCodeChange(NavigationStep, int)} is guaranteed to be called on the same listener later, immediately after
-     * the hash code has changed. Receiving this call gives the receiver the opportunity to remove the step from hashed structures
-     * in order to re-insert it later when the hash code has assumed the new value.
+     * Invoked before <code>step</code>'s {@link SemanticIdentity} changes its hash code. When this method is called for a step,
+     * the {@link #afterHashCodeChange(NavigationStep, int)} is guaranteed to be called on the same listener later, immediately
+     * after the hash code has changed. Receiving this call gives the receiver the opportunity to remove the step from hashed
+     * structures in order to re-insert it later when the hash code has assumed the new value.
      * 
      * @param token
      *            a token value identifying the root cause of the change by a monotonously-increasing number. This can be used in
@@ -15,8 +17,8 @@ public interface HashCodeChangeListener {
     void beforeHashCodeChange(NavigationStep step, int token);
 
     /**
-     * Called after the <code>step</code>'s hash code has changed. This gives hash structures the opportunity to enter the step
-     * again.
+     * Called after the <code>step</code>'s {@link SemanticIdentity} hash code has changed. This gives hash structures the
+     * opportunity to enter the step again.
      * 
      * @param token
      *            a token value identifying the root cause of the change by a monotonously-increasing number. This can be used in
