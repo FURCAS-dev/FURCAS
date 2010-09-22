4debade2bab296d953e8ae871463e1ddaab1825e
mhanysz <mhanysz@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-22 12:07:47 +0000

    Implemented LetTracebackStep and further developed AbstractTracebackStep.
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1621 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/traceback/IteratorTracebackStep.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/traceback/IteratorTracebackStep.java
@@ -1,15 +1,27 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.traceback;
 
+import java.util.Set;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.ocl.ecore.OCLExpression;
 
 import de.hpi.sam.bp2009.solution.impactAnalyzer.impl.OperationBodyToCallMapper;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.TracebackCache;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.unusedEvaluation.UnusedEvaluationRequest;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.util.AnnotatedEObject;
 
 public class IteratorTracebackStep extends AbstractTracebackStep {
 
     public IteratorTracebackStep(OCLExpression sourceExpression, EClass context,
             OperationBodyToCallMapper operationBodyToCallMapper, String[] tupleLiteralNamesToLookFor, TracebackStepCache tracebackStepCache) {
         // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected Set<AnnotatedEObject> performSubsequentTraceback(AnnotatedEObject source,
+            Set<UnusedEvaluationRequest> pendingUnusedEvalRequests, TracebackCache tracebackCache) {
+        // TODO implement AbstractTracebackStep#performSubsequentTraceback()
+        return null;
     }
 
 }
--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/traceback/PropertyCallTracebackStep.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/traceback/PropertyCallTracebackStep.java
@@ -1,15 +1,33 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.traceback;
 
+import java.util.HashMap;
+import java.util.Set;
+
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.ocl.ecore.OCLExpression;
+import org.eclipse.ocl.ecore.PropertyCallExp;
+import org.eclipse.ocl.ecore.TupleType;
 
 import de.hpi.sam.bp2009.solution.impactAnalyzer.impl.OperationBodyToCallMapper;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.TracebackCache;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.unusedEvaluation.UnusedEvaluationRequest;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.util.AnnotatedEObject;
 
 public class PropertyCallTracebackStep extends AbstractTracebackStep {
 
     public PropertyCallTracebackStep(OCLExpression sourceExpression, EClass context,
             OperationBodyToCallMapper operationBodyToCallMapper, String[] tupleLiteralNamesToLookFor, TracebackStepCache tracebackStepCache) {
-        // TODO Auto-generated constructor stub
+        nextSteps = new HashMap<String, TracebackStep>();
+        if (((PropertyCallExp)sourceExpression).getSource().getType() instanceof TupleType){
+            
+        }
+    }
+
+    @Override
+    protected Set<AnnotatedEObject> performSubsequentTraceback(AnnotatedEObject source,
+            Set<UnusedEvaluationRequest> pendingUnusedEvalRequests, TracebackCache tracebackCache) {
+        // TODO implement AbstractTracebackStep#performSubsequentTraceback()
+        return null;
     }
 
 }
