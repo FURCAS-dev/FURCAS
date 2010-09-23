95e496fe172d025377c69b91c76589bf195558b3
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-23 12:29:58 +0000

    set required type appropriately for tuple literal step
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1647 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/traceback/TupleLiteralTracebackStep.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/traceback/TupleLiteralTracebackStep.java
@@ -25,6 +25,7 @@ public class TupleLiteralTracebackStep extends BranchingTracebackStep {
                     getSteps().add(
                             createTracebackStepAndScopeChange(sourceExpression, (OCLExpression) part.getValue(), context,
                                     operationBodyToCallMapper, clonedTupleLiteralNamesToLookFor, tracebackStepCache));
+                    requiredType = getInnermostElementType(part.getValue().getType());
                     break;
                 }
             }
