e328666806e7c06afaebfb0c595ca91c65923325
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-23 11:52:54 +0000

    added a first TupleLiteralTracebackStep implementation
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1640 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/traceback/TupleLiteralTracebackStep.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/instanceScope/traceback/TupleLiteralTracebackStep.java
@@ -1,29 +1,37 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.traceback;
 
-import java.util.Set;
 import java.util.Stack;
 
-import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.ocl.ecore.OCLExpression;
 import org.eclipse.ocl.ecore.TupleLiteralExp;
+import org.eclipse.ocl.expressions.TupleLiteralPart;
 
 import de.hpi.sam.bp2009.solution.impactAnalyzer.impl.OperationBodyToCallMapper;
-import de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.unusedEvaluation.UnusedEvaluationRequestSet;
-import de.hpi.sam.bp2009.solution.impactAnalyzer.util.AnnotatedEObject;
 
-public class TupleLiteralTracebackStep extends AbstractTracebackStep {
+public class TupleLiteralTracebackStep extends BranchingTracebackStep {
 
     public TupleLiteralTracebackStep(TupleLiteralExp sourceExpression, EClass context,
             OperationBodyToCallMapper operationBodyToCallMapper, Stack<String> tupleLiteralNamesToLookFor, TracebackStepCache tracebackStepCache) {
         super(sourceExpression);
-        // TODO Auto-generated constructor stub
+        if (tupleLiteralNamesToLookFor != null && !tupleLiteralNamesToLookFor.isEmpty()) {
+            String partName = tupleLiteralNamesToLookFor.peek();
+            for (TupleLiteralPart<EClassifier, EStructuralFeature> part : sourceExpression.getPart()) {
+                if (part.getName().equals(partName)) {
+                    Stack<String> clonedTupleLiteralNamesToLookFor = cloneWithTypeCheck(tupleLiteralNamesToLookFor);
+                    clonedTupleLiteralNamesToLookFor.pop();
+                    getSteps().add(
+                            createTracebackStepAndScopeChange(sourceExpression, (OCLExpression) part.getValue(), context,
+                                    operationBodyToCallMapper, clonedTupleLiteralNamesToLookFor, tracebackStepCache));
+                    break;
+                }
+            }
+        }
+        if (getSteps().isEmpty()) {
+            throw new RuntimeException("Internal error regarding tuple literal and tuple access nesting: couldn't find requested part "+
+                    tupleLiteralNamesToLookFor.peek()+" in tuple parts "+sourceExpression.getPart());
+        }
     }
-
-    @Override
-    protected Set<AnnotatedEObject> performSubsequentTraceback(AnnotatedEObject source,
-            UnusedEvaluationRequestSet pendingUnusedEvalRequests, de.hpi.sam.bp2009.solution.impactAnalyzer.instanceScope.traceback.TracebackCache tracebackCache, Notification changeEvent) {
-        // TODO implement AbstractTracebackStep#performSubsequentTraceback()
-        return null;
-    }
-
 }
