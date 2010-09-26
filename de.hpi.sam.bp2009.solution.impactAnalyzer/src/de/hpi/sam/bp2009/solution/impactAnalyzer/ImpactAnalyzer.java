ebda76a8aa597161520ffd2f104de13f7b3214d7
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-26 21:00:47 +0000

    added Javadoc explaining the configuration options
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1664 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/ImpactAnalyzer.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer/src/de/hpi/sam/bp2009/solution/impactAnalyzer/ImpactAnalyzer.java
@@ -14,23 +14,33 @@ import org.eclipse.ocl.ecore.OCLExpression;
 
 import de.hpi.sam.bp2009.solution.eventManager.EventManager;
 import de.hpi.sam.bp2009.solution.eventManager.filters.EventFilter;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.configuration.ActivationOption;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.configuration.OptimizationActivation;
 
 /**
  * 
- * The Impact Analyzer (IA) analyzes the impact of {@link Notification}s on
- * a single {@link OCLExpression}.
+ * The Impact Analyzer (IA) analyzes the impact of {@link Notification}s on a single {@link OCLExpression} in the sense that it
+ * determines those context objects of the expression for which the expression's evaluation result may have changed because of the
+ * change indicated by the notification. Imagine this as "spreadsheet" functionality for models: model changes can be propagated
+ * efficiently, even for large to huge models. Possible application areas include OCL constraint re-evaluation after model changes
+ * as well as attribute grammars based on OCL.
  * <p>
- * The interaction with the IA typically happens in two steps. First call
- * {@link #createFilterForExpression(boolean)}.
- * This returns an {@link EventFilter} which can be used to register a listener
- * in the {@link EventManager}. The subscriber will then be supplied with all
- * relevant {@link Notification}s relevant to the supplied {@link OCLExpression}.
+ * 
+ * The interaction with the IA typically happens in two steps. First call {@link #createFilterForExpression(boolean)}. This
+ * returns an {@link EventFilter} which can be used to register a listener in the {@link EventManager}. The subscriber will then
+ * be supplied with all relevant {@link Notification}s relevant to the supplied {@link OCLExpression}.
  * <p>
- * Once a relevant <code>Notification</code> occurs it can be passed to the IA
- * by calling {@link #getContextObjects(Notification)} which returns a
- * set of {@link EObject}s for which the expression's value may have changed due to the
- * change notification. These context objects can be used to instruct the evaluator to
- * evaluate affected <code>OCLExpressions</code> for a certain set of instances.
+ * 
+ * Once a relevant <code>Notification</code> occurs it can be passed to the IA by calling {@link #getContextObjects(Notification)}
+ * which returns a set of {@link EObject}s for which the expression's value may have changed due to the change notification. These
+ * context objects can be used to instruct the evaluator to evaluate affected <code>OCLExpressions</code> for a certain set of
+ * instances.
+ * <p>
+ * 
+ * The impact analyzer has a few configuration options, affecting the choice of implementation. We currently
+ * use those to tune and performance-benchmark variants and flavors of the implementation. If you find that
+ * the default configuration doesn't work well for your case, you may try different configuration options.
+ * See {@link OptimizationActivation} and {@link ActivationOption} for details.
  * 
  */
 public interface ImpactAnalyzer {
@@ -70,4 +80,4 @@ public interface ImpactAnalyzer {
 	 * @return all relevant context objects
 	 */
 	Collection<EObject> getContextObjects(Notification event);
-} // ImpactAnalyzer
+}
