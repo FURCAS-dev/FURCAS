86f3d46bd860a67cfa3ffde3ad41edbf8316e3d9
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-29 16:16:14 +0000

    Updated 3dPlots.R in order to generate plots for all available ActivationOptions
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1696 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/3dPlots.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/3dPlots.R
@@ -1,25 +1,21 @@
 library(rgl)
 require(DAAG)
 
-# select subsets for different ActivationOptions
-resultAllOptimizations = result[result$optionId == 0, ]
-resultWoDeltaProp = result[result$optionId == 1, ]
-
 zlabEvalTime = "Evaluation time (total) in nanoseconds"
 
-attach(resultAllOptimizations)
-# create graph / canvas
-plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = zlabEvalTime)
-# add to existing graph
-points3d(oclId, modelSize, allInstancesEvalAndExecTime, col = "blue", size=2)
-pause()
-detach(resultAllOptimizations)
-
-attach(resultWoDeltaProp)
-plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = zlabEvalTime)
-points3d(oclId, modelSize, allInstancesEvalAndExecTime, col = "blue", size=2)
-pause()
-detach(resultWoDeltaProp)
+for(i in unique(result$optionId)){
+	# select subsets for different ActivationOptions
+	resultOptimization = result[result$optionId == i, ]
+	
+	attach(resultAllOptimizations)
+	# create graph / canvas
+	plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = zlabEvalTime)
+	
+	# add to existing graph
+	points3d(oclId, modelSize, allInstancesEvalAndExecTime, col = "blue", size=2)
+	pause()
+	detach(resultOptimization)
+}
 
 attach(resultAllOptimizations)
 plot3d(oclId, modelSize, noContextObjects, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = "Number of Context Objects")
