516e089c97838124e5c2bcf2dc616d3dffe26664
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-29 14:32:09 +0000

    Added command-line progress bar for ia benchmark runs.
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1689 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/execution/BenchmarkExecutionJob.java
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/execution/BenchmarkExecutionJob.java
@@ -1,5 +1,7 @@
 package de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.execution;
 
+import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Queue;
 
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -7,7 +9,6 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 
-import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.ProcessingOptions;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.postprocessing.BenchmarkResultWriter;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark.preparation.tasks.BenchmarkTaskContainer;
 
@@ -17,6 +18,8 @@ public class BenchmarkExecutionJob extends Job {
     private final BenchmarkResultWriter writer;
     private final Queue<BenchmarkTaskContainer> containerList;
 
+    private final int containerSizeFull;
+
     public BenchmarkExecutionJob(String name, BenchmarkExecutor executor, Queue<BenchmarkTaskContainer> containerList,
 	    BenchmarkResultWriter writer) {
 	super(name);
@@ -24,12 +27,20 @@ public class BenchmarkExecutionJob extends Job {
 	this.executor = executor;
 	this.containerList = containerList;
 	this.writer = writer;
+
+	this.containerSizeFull = containerList.size();
     }
 
     @Override
     protected IStatus run(IProgressMonitor monitor) {
-	int i = 0;
+	int i = 11;
 	while (!containerList.isEmpty()) {
+	    if (i++ > 10) {
+		printProgress(containerSizeFull, containerList.size());
+
+		i = 0;
+	    }
+
 	    BenchmarkTaskContainer container = containerList.remove();
 
 	    container.beforeBenchmark();
@@ -37,16 +48,47 @@ public class BenchmarkExecutionJob extends Job {
 		executor.execute(container.remove(), writer);
 	    }
 	    container.afterBenchmark();
-
-	    if (i++ > 200) {
-		if (ProcessingOptions.isVerbose()) {
-		    System.out.println("Job[" + this.getName() + "]:" + containerList.size());
-		}
-		i = 0;
-	    }
 	}
 
 	return Status.OK_STATUS;
+    }
+
+    private void printProgress(int fullSize, int currentSize){
+	double percent = ((double)currentSize) / ((double)fullSize);
+	int progressBarWidth = 60;
+
+	StringBuffer strBuff = new StringBuffer();
+	DecimalFormat f = new DecimalFormat("##0.00");
+
+	strBuff.append("\r");
+	strBuff.append(f.format(((1.0 - percent) * 100.0)));
+	strBuff.append("% ");
+	strBuff.append("[");
+
+	int currentState = progressBarWidth - (int)(percent * progressBarWidth);
+
+	for(int i = 0; i < currentState; i++){
+	    strBuff.append("=");
+	}
+	strBuff.append(">");
+	for(int i = 0; i < progressBarWidth - currentState; i++){
+	    strBuff.append(" ");
+	}
+
+	strBuff.append("]");
+
+	strBuff.append("(");
+	strBuff.append(currentSize);
+	strBuff.append("/");
+	strBuff.append(fullSize);
+	strBuff.append(")");
+
+        try {
+	    System.out.write(strBuff.toString().getBytes());
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
     }
 
 }
