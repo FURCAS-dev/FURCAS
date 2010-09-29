29b7f8a24f976a71c166659aa4110d05d5723e18
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-29 12:17:16 +0000

    added comments
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1687 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/3dPlots.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/3dPlots.R
@@ -3,16 +3,21 @@ require(DAAG)
 
 result = read.table("~/Dropbox/fullResult8.data", header = TRUE)
 
+# add derived columns:
 result$iaEvalAndExecTime = result$executionTime + result$evaluationTimeAfter
 result$allInstancesEvalAndExecTime = result$allInstanceEvalTime + result$allInstanceExecTime
 
+# filter for only those rows that contain not only invalid results
 result = result[result$noAllInstances != result$allInstanceNoInvalidEvals, ]
 
+# select subsets for different ActivationOptions
 resultAllOptimizations = result[result$optionId == 0, ]
 resultWoDeltaProp = result[result$optionId == 1, ]
 
 attach(resultAllOptimizations)
+# create graph / canvas
 plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2)
+# add to existing graph
 points3d(oclId, modelSize, allInstancesEvalAndExecTime, col = "blue", size=2)
 pause()
 detach(resultAllOptimizations)
