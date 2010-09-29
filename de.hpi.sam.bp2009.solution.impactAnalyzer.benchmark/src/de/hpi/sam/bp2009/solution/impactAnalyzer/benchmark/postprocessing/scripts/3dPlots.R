f6e0c391f0f6e055214f46dd6a8c32fc372971e9
manuelh <manuelh@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-29 15:39:59 +0000

    Added labels to R script
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1695 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/3dPlots.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/3dPlots.R
@@ -5,22 +5,24 @@ require(DAAG)
 resultAllOptimizations = result[result$optionId == 0, ]
 resultWoDeltaProp = result[result$optionId == 1, ]
 
+zlabEvalTime = "Evaluation time (total) in nanoseconds"
+
 attach(resultAllOptimizations)
 # create graph / canvas
-plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2)
+plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = zlabEvalTime)
 # add to existing graph
 points3d(oclId, modelSize, allInstancesEvalAndExecTime, col = "blue", size=2)
 pause()
 detach(resultAllOptimizations)
 
 attach(resultWoDeltaProp)
-plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2)
+plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = zlabEvalTime)
 points3d(oclId, modelSize, allInstancesEvalAndExecTime, col = "blue", size=2)
 pause()
 detach(resultWoDeltaProp)
 
 attach(resultAllOptimizations)
-plot3d(oclId, modelSize, noContextObjects, col = "red", size=2)
+plot3d(oclId, modelSize, noContextObjects, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = "Number of Context Objects")
 points3d(oclId, modelSize, noAllInstances, col = "blue", size=2)
 pause()
 detach(resultAllOptimizations)
