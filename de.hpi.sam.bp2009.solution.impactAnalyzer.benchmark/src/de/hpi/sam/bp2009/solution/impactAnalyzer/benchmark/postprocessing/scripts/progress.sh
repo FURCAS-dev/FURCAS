127a090038590f88e36fa80ebd38e6b611f7e9be
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-29 13:11:46 +0000

    naive progress indication "script" showing last optionId from results file
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1688 3ca8e212-6386-4b1e-b09d-39f68028012c

---
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/progress.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ "$1" = "" ]; then
+  echo Usage: progress \<result.data filename\>
+  echo    outputs the number of the job currently being executed \(counted
+  echo    starting with 0\)
+else
+  cat "$1" | awk '{ print $2; }' | sort -u | grep [0-9][0-9]* | tail -1
+fi
