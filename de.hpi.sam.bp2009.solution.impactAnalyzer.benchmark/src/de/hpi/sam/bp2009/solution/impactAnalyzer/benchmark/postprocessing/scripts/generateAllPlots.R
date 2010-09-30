797d55f63e1c957a2d0d9a5d0ac1f109d1ecc1ad
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-30 19:32:44 +0000

    added different optimization options to histogram
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1705 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/generateAllPlots.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/generateAllPlots.R
@@ -7,25 +7,29 @@ attach(strippedResult)
 png("plotScatterModelSize.png", width=1200, height=800, res = 110)
 xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize | optionId, type=c("p", "r"),
 xlab = "Number of model elements (model size)",
-ylab = ylabEvalTime)
+ylab = ylabEvalTime,
+auto.key=TRUE)
 dev.off()
 
 png("plotScatterOclId.png", width=1200, height=800, res = 110)
 xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ oclId | optionId, type=c("p"),
 xlab = "Different OCL Expressions",
-ylab = ylabEvalTime)
+ylab = ylabEvalTime,
+auto.key=TRUE)
 dev.off()
 
 png("plotScatterNotificationId.png", width=1200, height=800, res = 110)
 xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ notificationId | optionId, type=c("p"),
 xlab = "Different model change notifications",
-ylab = ylabEvalTime)
+ylab = ylabEvalTime,
+auto.key=TRUE)
 dev.off()
 
 png("plotScatterNoAllInstances.png", width=1200, height=800, res = 110)
 xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ noAllInstances | optionId, type=c("p", "r"),
 xlab = "Number of all instances of context of OCL Expression",
-ylab = ylabEvalTime)
+ylab = ylabEvalTime,
+auto.key=TRUE)
 dev.off()
 
 aggregatedResult = aggregate(strippedResult, by=list(modelSize, optionId), FUN=mean)
@@ -35,7 +39,8 @@ attach(aggregatedResult)
 png("plotScatterModelSizeMean.png", width=1200, height=800, res = 110)
 xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize | optionId, type=c("p", "r"),
 xlab = "Number of model elements (model size)",
-ylab = "Meaned evaluation time (total) in nanoseconds"
+ylab = "Meaned evaluation time (total) in nanoseconds,
+auto.key=TRUE"
 )
 dev.off()
 
@@ -48,7 +53,8 @@ attach(aggregatedResult)
 png("plotScatterOclMean.png", width=1200, height=800, res = 110)
 xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ oclId | optionId, type=c("p"),
 xlab = "Different OCL Expressions",
-ylab = "Meaned evaluation time (total) in nanoseconds")
+ylab = "Meaned evaluation time (total) in nanoseconds",
+auto.key=TRUE)
 dev.off()
 
 detach(aggregatedResult)
@@ -60,12 +66,13 @@ attach(aggregatedResult)
 png("plotScatterNotificationMean.png", width=1200, height=800, res = 110)
 xyplot(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ notificationId | optionId, type=c("p"),
 xlab = "Different model change notifications",
-ylab = "Meaned evaluation time (total) in nanoseconds")
+ylab = "Meaned evaluation time (total) in nanoseconds",
+auto.key=TRUE)
 dev.off()
 
 detach(aggregatedResult)
 attach(strippedResult)
-png("plot3dScatterOclNotification.png", width=1200, height=1200)
-cloud(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize * oclId | optionId, data = strippedResult)
+png("plot3dScatterOclNotification.png", width=2400, height=2400)
+cloud(allInstancesEvalAndExecTime + iaEvalAndExecTime ~ modelSize * oclId | optionId, data = strippedResult, auto.key=TRUE)
 dev.off()
 detach(strippedResult)
--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/generateHistogram.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/generateHistogram.R
@@ -1,4 +1,7 @@
 require(epicalc)
+# read different optimization options from data file:
+options=read.table("optionDescription.data", sep="\t", header=TRUE)
+
 # Pick only every second model size
 result = result[result$modelId %% 2 == 0, ]
 
@@ -17,54 +20,26 @@ aggrAllInstanceUnfiltered$measurement = 1
 aggrAllInstanceFiltered = aggrFiltered
 aggrAllInstanceFiltered$measureTime = aggrAllInstanceFiltered$aiExecAndEvalTime
 aggrAllInstanceFiltered$measurement = 2
+mergeAll = merge(aggrAllInstanceUnfiltered, aggrAllInstanceFiltered, all = TRUE)
 
-aggrIaFiltered1 = aggrFiltered[aggrFiltered$Group.4 == 1, ]
-aggrIaFiltered1$measureTime = aggrIaFiltered1$iaExecAndEvalTime
-aggrIaFiltered1$measurement = 3
-
-aggrIaFiltered = aggrFiltered[aggrFiltered$Group.4 == 0, ]
-aggrIaFiltered$measureTime = aggrIaFiltered$iaExecAndEvalTime
-aggrIaFiltered$measurement = 4
-
-mergeAllInstance = merge(aggrAllInstanceUnfiltered, aggrAllInstanceFiltered, all = TRUE)
-mergeIa = merge(aggrIaFiltered, aggrIaFiltered1, all = TRUE)
-mergeAll = merge(mergeAllInstance, mergeIa, all = TRUE)
-
-mergeAll$modelSize = -mergeAll$Group.1
-
-pdf("ciHistogram.pdf", width = 8, height = 6)
-aggregate.plot(mergeAll$measureTime, by=list(mergeAll$measurement, mergeAll$modelSize), FUN = "mean", error = "ci", alpha = 0.1, legend = FALSE, main = NULL)
-legend(x = "topleft", legend = c("All instances w/o event filter", "All instances with event filter", "Impact Analysis w/o Delta Propagation", "Impact Analysis with Delta Propagation"), fill=grey.colors(5))
-title("Total re-evaluation time meaned with a 90% CI", ylab="Total re-evaluation time in nanoseconds", xlab="Scaled models from small to large")
-dev.off()
-
-
-
-
-aggrAllInstanceFiltered = aggrFiltered
-aggrAllInstanceFiltered$measureTime = aggrAllInstanceFiltered$aiExecAndEvalTime
-aggrAllInstanceFiltered$measurement = 2
-
-aggrIaFiltered1 = aggrFiltered[aggrFiltered$Group.4 == 1, ]
-aggrIaFiltered1$measureTime = aggrIaFiltered1$iaExecAndEvalTime
-aggrIaFiltered1$measurement = 3
-
-aggrIaFiltered = aggrFiltered[aggrFiltered$Group.4 == 0, ]
-aggrIaFiltered$measureTime = aggrIaFiltered$iaExecAndEvalTime
-aggrIaFiltered$measurement = 4
-
-mergeAllInstance = aggrAllInstanceFiltered
-mergeIa = merge(aggrIaFiltered, aggrIaFiltered1, all = TRUE)
-mergeAll = merge(mergeAllInstance, mergeIa, all = TRUE)
-
+for (i in 0:(dim(options)[1]-1)) {
+    aggrIaFiltered = aggrFiltered[aggrFiltered$Group.4 == i, ]
+    aggrIaFiltered$measureTime = aggrIaFiltered$iaExecAndEvalTime
+    aggrIaFiltered$measurement = i+3
+    mergeAll = merge(mergeAll, aggrIaFiltered, all = TRUE)
+}
 mergeAll$modelSize = -mergeAll$Group.1
 
 pdf("ciHistogram1.pdf", width = 8, height = 6)
 aggregate.plot(mergeAll$measureTime, by=list(mergeAll$measurement, mergeAll$modelSize), FUN = "mean", error = "ci", alpha = 0.1, legend = FALSE, main = NULL)
-legend(x = "topleft", legend = c("All instances with event filter", "Impact Analysis w/o Delta Propagation", "Impact Analysis with Delta Propagation"), fill=grey.colors(5))
+legend(x = "topleft", legend =  c("All instances with event filter", as.character(options$description)), fill=grey.colors(5))
 title("Total re-evaluation time meaned with a 90% CI", ylab="Total re-evaluation time in nanoseconds", xlab="Scaled models from small to large")
 dev.off()
 
+mergeAll = merge(mergeAll, aggrAllInstanceUnfiltered, all = TRUE)
 
-
-
+pdf("ciHistogram.pdf", width = 8, height = 6)
+aggregate.plot(mergeAll$measureTime, by=list(mergeAll$measurement, mergeAll$modelSize), FUN = "mean", error = "ci", alpha = 0.1, legend = FALSE, main = NULL)
+legend(x = "topleft", legend = c("All instances w/o event filter", "All instances with event filter", as.character(options$description)), fill=grey.colors(5))
+title("Total re-evaluation time meaned with a 90% CI", ylab="Total re-evaluation time in nanoseconds", xlab="Scaled models from small to large")
+dev.off()
