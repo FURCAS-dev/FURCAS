209340e3ea567b86257abf58ad2277ca31d82dac
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-30 14:10:02 +0000

    minor fixes
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1703 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/plotModelSizeToExecutionTime.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/plotModelSizeToExecutionTime.R
@@ -1,19 +1,19 @@
 require(outliers)
 require(lattice)
-#results = read.table("result.data", header=TRUE)
+#result = read.table("result.data", header=TRUE)
 
-length(results$executionTime)
+length(result$executionTime)
 
-summary(results)
+summary(result)
 
 for(i in 1:5){
-results$cleanExecutionTime = ifelse(results$executionTime == outlier(results$executionTime), NA, results$executionTime)
-results = subset(results, !is.na(results$cleanExecutionTime))
-results$cleanExecutionTime = NULL;
+result$cleanExecutionTime = ifelse(result$executionTime == outlier(result$executionTime), NA, result$executionTime)
+result = subset(result, !is.na(result$cleanExecutionTime))
+result$cleanExecutionTime = NULL;
 }
-summary(results)
-aggregatedResults = aggregate(results, by=list(results$modelSize), FUN=mean)
-attach(aggregatedResults)
+summary(result)
+aggregatedresult = aggregate(result, by=list(result$modelSize), FUN=mean)
+attach(aggregatedresult)
 plot(x=modelSize, y=executionTime)
 xyplot(executionTime~modelSize, type=c("p","r"))
-detach(aggregatedResults)
+detach(aggregatedresult)
