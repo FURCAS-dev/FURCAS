3f0891274a5f47af2ce8b52b84542ce9d29ae9f7
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-09-30 07:59:31 +0000

    only use "tail" instead of "cat" to just analyze the last bits of the possibly huge result file
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1697 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/progress.sh
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/progress.sh
@@ -4,5 +4,5 @@ if [ "$1" = "" ]; then
   echo    outputs the number of the job currently being executed \(counted
   echo    starting with 0\)
 else
-  cat "$1" | awk '{ print $2; }' | sort -u | grep [0-9][0-9]* | tail -1
+  tail "$1" | awk '{ print $2; }' | sort -u | grep [0-9][0-9]* | tail -1
 fi
