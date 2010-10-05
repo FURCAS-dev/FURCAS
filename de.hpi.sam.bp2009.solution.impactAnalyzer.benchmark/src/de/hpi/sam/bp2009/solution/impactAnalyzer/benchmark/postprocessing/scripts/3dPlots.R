d543c0bbee994043219c076a1438c710389fa536
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-10-05 23:11:17 +0000

    fixed misspelled variable names
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1716 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/3dPlots.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/3dPlots.R
@@ -4,21 +4,21 @@ require(DAAG)
 zlabEvalTime = "Evaluation time (total) in nanoseconds"
 
 for(i in unique(result$optionId)){
-	# select subsets for different ActivationOptions
-	resultOptimization = result[result$optionId == i, ]
-	
-	attach(resultAllOptimizations)
-	# create graph / canvas
-	plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = zlabEvalTime)
-	
-	# add to existing graph
-	points3d(oclId, modelSize, allInstancesEvalAndExecTime, col = "blue", size=2)
-	pause()
-	detach(resultOptimization)
+        # select subsets for different ActivationOptions
+        resultOptimizations = result[result$optionId == i, ]
+
+        attach(resultOptimizations)
+        # create graph / canvas
+        plot3d(oclId, modelSize, iaEvalAndExecTime, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = zlabEvalTime)
+
+        # add to existing graph
+        points3d(oclId, modelSize, allInstancesEvalAndExecTime, col = "blue", size=2)
+        pause()
+        detach(resultOptimizations)
 }
 
-attach(resultAllOptimizations)
+attach(resultOptimizations)
 plot3d(oclId, modelSize, noContextObjects, col = "red", size=2, xlab = "Different OCL Expressions", ylab = "Number of Model Elements", zlab = "Number of Context Objects")
 points3d(oclId, modelSize, noAllInstances, col = "blue", size=2)
 pause()
-detach(resultAllOptimizations)
+detach(resultOptimizations)
