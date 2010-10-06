7bf9ea8102c28a2d1c6b22f8da259d7159386cc4
d043530 <d043530@3ca8e212-6386-4b1e-b09d-39f68028012c>
2010-10-06 12:32:56 +0000

    added comment
    git-svn-id: https://www.hpi.uni-potsdam.de/giese/gforge/svn/bp2009/sources/trunk@1720 3ca8e212-6386-4b1e-b09d-39f68028012c

--- de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/loadAndPrepareResultData.R
+++ de.hpi.sam.bp2009.solution.impactAnalyzer.benchmark/src/de/hpi/sam/bp2009/solution/impactAnalyzer/benchmark/postprocessing/scripts/loadAndPrepareResultData.R
@@ -2,8 +2,10 @@ result = read.table("result.data", header = TRUE)
 
 result$iaEvalAndExecTime = result$executionTime + result$evaluationTimeAfter
 result$allInstancesEvalAndExecTime = result$allInstanceEvalTime + result$allInstanceExecTime
+result$sloppiness = result$noEqualResultsBeforeAndAfter / result$noContextObjects
 
 attach(result)
+# filter out those results where an expression's evaluation resulted in invalid for all instances of the context type
 strippedResult = result[noAllInstances != allInstanceNoInvalidEvals, ]
 detach(result)
 
