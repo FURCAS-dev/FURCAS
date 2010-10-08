3c26949ab9254a2ac13789698b3e89d87fba613a
Stephan Erb <stephan@dev.static-void.de>
2010-10-08 02:26:09 +0200

    Drop the old moin framework tests.

--- moin_tests/com.sap.mi.gfw.test/src/com/sap/mi/gfw/test/ITestConstants.java
+++
@@ -1,7 +0,0 @@
-package com.sap.mi.gfw.test;
-
-interface ITestConstants {
-	static final String DC_NAME = "TESTPROJECT-CreateDiagramTest";
-
-	static final String TEST_DIAGRAM_PREFIX = "test_diagram_";
-}
