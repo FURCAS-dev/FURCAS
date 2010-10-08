4a2f76b038feb6aa6f7ab8af16a98e26bc01be99
Stephan Erb <stephan@dev.static-void.de>
2010-10-08 02:26:09 +0200

    Drop the old moin framework tests.

--- moin_tests/com.sap.mi.fwk.test.service/src/com/sap/mi/fwk/test/service/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Utilities to ease execution of JUnit tests for Moin based tools. 
- * {@link TestUtil} contains standalone services that are also used by
- * the convenience test bases classes like {@link ProjectBasedTest}.
- */
-package com.sap.mi.fwk.test.service;
-
