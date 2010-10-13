5d472b0e5971b5eb71d0b5d37ed75939163ae7da
Axel Uhl <axel.uhl@sap.com>
2010-10-13 14:13:00 +0200

    making the first simple trivial tests work

--- DSLEngineering/com.sap.emf.ocl.trigger.tests/src/com/sap/emf/ocl/trigger/tests/SimpleTests.java
+++ DSLEngineering/com.sap.emf.ocl.trigger.tests/src/com/sap/emf/ocl/trigger/tests/SimpleTests.java
@@ -1,26 +1,64 @@
 package com.sap.emf.ocl.trigger.tests;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import junit.framework.TestCase;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.ECrossReferenceAdapter;
+import org.eclipse.ocl.ParserException;
+import org.eclipse.ocl.ecore.OCL.Helper;
+import org.eclipse.ocl.ecore.OCLExpression;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.sap.emf.ocl.hiddenopposites.OCLWithHiddenOpposites;
+import com.sap.emf.ocl.trigger.AbstractTriggerable;
+import com.sap.emf.ocl.trigger.TriggerManager;
+import com.sap.emf.ocl.trigger.TriggerManagerFactory;
+import com.sap.emf.ocl.trigger.Triggerable;
+import com.sap.emf.ocl.trigger.Triggerable.ExpressionWithContext;
+import company.CompanyFactory;
+import company.CompanyPackage;
+import company.Department;
 
 public class SimpleTests extends TestCase {
     private ResourceSet resourceSet;
     private Resource resource;
+    private TriggerManager triggerManager;
     
     @Before
     public void setUp() {
         resourceSet = new ResourceSetImpl();
+        resourceSet.eAdapters().add(new ECrossReferenceAdapter());
         resource = resourceSet.createResource(URI.createURI("http://com.sap.emf.ocl.trigger.tests/Resource.xmi"));
+        triggerManager = TriggerManagerFactory.INSTANCE.createTriggerManager();
+        triggerManager.addToObservedResourceSets(resourceSet);
     }
     
     @Test
-    public void testTrivialTrigger() {
-        
+    public void testTrivialTrigger() throws ParserException {
+        final Department d = CompanyFactory.eINSTANCE.createDepartment();
+        resource.getContents().add(d);
+        Helper helper = OCLWithHiddenOpposites.newInstance().createOCLHelper();
+        helper.setContext(CompanyPackage.eINSTANCE.getDepartment());
+        final OCLExpression trivialExpression = helper.createQuery("self.name");
+        final boolean[] result = new boolean[1];
+        Triggerable t = new AbstractTriggerable(Collections.singleton(new ExpressionWithContext(trivialExpression,
+                /* context is implicit because self is used */ null, /* notifyNewContextElements */ false))) {
+            @Override
+            public void notify(OCLExpression expression, Collection<EObject> affectedContextObjects) {
+                result[0] = expression == trivialExpression && affectedContextObjects.size() == 1 &&
+                            affectedContextObjects.contains(d);
+            }
+        };
+        triggerManager.register(t);
+        d.setName("Humba");
+        assertTrue(result[0]);
     }
 }
--- DSLEngineering/com.sap.emf.ocl.trigger/src/com/sap/emf/ocl/trigger/impl/AdapterForExpression.java
+++ DSLEngineering/com.sap.emf.ocl.trigger/src/com/sap/emf/ocl/trigger/impl/AdapterForExpression.java
@@ -29,7 +29,23 @@ public class AdapterForExpression extends AdapterImpl {
     private final OCLExpression expression;
     private final ImpactAnalyzer impactAnalyzer;
     
+    /**
+     * With this constructor, the expression must contain a <code>self</code> occurrence that allows us to infer the context type.
+     */
+    AdapterForExpression(Triggerable triggerableToNotify, OCLExpression expression, OppositeEndFinder oppositeEndFinder, ActivationOption configuration) {
+        this.triggerableToNotify = triggerableToNotify;
+        this.expression = expression;
+        this.impactAnalyzer = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expression, oppositeEndFinder, configuration);
+    }
+    
+    /**
+     * A non-<code>null</code> context must be provided if this constructor is used. Should the <code>expression</code> contain an
+     * occurrence of <code>self</code>, its type must be the same as <code>context</code>.
+     */
     AdapterForExpression(Triggerable triggerableToNotify, OCLExpression expression, EClass context, OppositeEndFinder oppositeEndFinder, ActivationOption configuration) {
+        if (context == null) {
+            throw new IllegalArgumentException("This constructor expects a non-null context type for expression "+expression);
+        }
         this.triggerableToNotify = triggerableToNotify;
         this.expression = expression;
         this.impactAnalyzer = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expression, context, oppositeEndFinder, configuration);
