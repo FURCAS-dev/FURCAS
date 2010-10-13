53ba0522c303c5f265c3c8d34d515b1033fc128b
Axel Uhl <axel.uhl@sap.com>
2010-10-13 12:26:02 +0200

    this first implementation may already do something useful; no tests yet

--- DSLEngineering/com.sap.emf.ocl.trigger/src/com/sap/emf/ocl/trigger/impl/AdapterForExpression.java
+++ DSLEngineering/com.sap.emf.ocl.trigger/src/com/sap/emf/ocl/trigger/impl/AdapterForExpression.java
@@ -11,6 +11,7 @@ import org.eclipse.ocl.ecore.OCLExpression;
 import com.sap.emf.ocl.hiddenopposites.OppositeEndFinder;
 import com.sap.emf.ocl.trigger.Triggerable;
 
+import de.hpi.sam.bp2009.solution.eventManager.filters.EventFilter;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.ImpactAnalyzer;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.ImpactAnalyzerFactory;
 import de.hpi.sam.bp2009.solution.impactAnalyzer.configuration.ActivationOption;
@@ -28,44 +29,18 @@ public class AdapterForExpression extends AdapterImpl {
     private final OCLExpression expression;
     private final ImpactAnalyzer impactAnalyzer;
     
-    AdapterForExpression(Triggerable triggerableToNotify, OCLExpression expression) {
-        this.triggerableToNotify = triggerableToNotify;
-        this.expression = expression;
-        this.impactAnalyzer = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expression);
-    }
-    
-    AdapterForExpression(Triggerable triggerableToNotify, OCLExpression expression, EClass context) {
-        this.triggerableToNotify = triggerableToNotify;
-        this.expression = expression;
-        this.impactAnalyzer = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expression, context);
-    }
-    
-    AdapterForExpression(Triggerable triggerableToNotify, OCLExpression expression, EClass context, ActivationOption configuration) {
-        this.triggerableToNotify = triggerableToNotify;
-        this.expression = expression;
-        this.impactAnalyzer = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expression, context, configuration);
-    }
-    
     AdapterForExpression(Triggerable triggerableToNotify, OCLExpression expression, EClass context, OppositeEndFinder oppositeEndFinder, ActivationOption configuration) {
         this.triggerableToNotify = triggerableToNotify;
         this.expression = expression;
         this.impactAnalyzer = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expression, context, oppositeEndFinder, configuration);
     }
     
-    AdapterForExpression(Triggerable triggerableToNotify, OCLExpression expression, ActivationOption configuration) {
-        this.triggerableToNotify = triggerableToNotify;
-        this.expression = expression;
-        this.impactAnalyzer = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expression, configuration);
-    }
-    
-    AdapterForExpression(Triggerable triggerableToNotify, OCLExpression expression, OppositeEndFinder oppositeEndFinder) {
-        this.triggerableToNotify = triggerableToNotify;
-        this.expression = expression;
-        this.impactAnalyzer = ImpactAnalyzerFactory.INSTANCE.createImpactAnalyzer(expression, oppositeEndFinder);
-    }
-    
     private ImpactAnalyzer getImpactAnalyzer() {
         return impactAnalyzer;
+    }
+    
+    public EventFilter getEventFilter(boolean notifyNewContextElements) {
+        return impactAnalyzer.createFilterForExpression(notifyNewContextElements);
     }
     
     @Override
--- DSLEngineering/com.sap.emf.ocl.trigger/src/com/sap/emf/ocl/trigger/impl/TriggerManagerImpl.java
+++ DSLEngineering/com.sap.emf.ocl.trigger/src/com/sap/emf/ocl/trigger/impl/TriggerManagerImpl.java
@@ -1,24 +1,51 @@
 package com.sap.emf.ocl.trigger.impl;
 
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 
+import com.sap.emf.ocl.hiddenopposites.DefaultOppositeEndFinder;
+import com.sap.emf.ocl.hiddenopposites.OppositeEndFinder;
 import com.sap.emf.ocl.trigger.TriggerManager;
 import com.sap.emf.ocl.trigger.Triggerable;
+import com.sap.emf.ocl.trigger.Triggerable.ExpressionWithContext;
 
 import de.hpi.sam.bp2009.solution.eventManager.EventManager;
 import de.hpi.sam.bp2009.solution.eventManager.EventManagerFactory;
+import de.hpi.sam.bp2009.solution.eventManager.filters.EventFilter;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.configuration.ActivationOption;
+import de.hpi.sam.bp2009.solution.impactAnalyzer.configuration.OptimizationActivation;
 
 public class TriggerManagerImpl implements TriggerManager {
     private final EventManager eventManager;
+    private final OppositeEndFinder oppositeEndFinder;
+    private final ActivationOption impactAnalysisConfiguration;
     
     TriggerManagerImpl() {
-        eventManager = EventManagerFactory.eINSTANCE.createEventManager();
+        this(new DefaultOppositeEndFinder(EPackage.Registry.INSTANCE));
     }
     
+    public TriggerManagerImpl(OppositeEndFinder oppositeEndFinder) {
+        this(oppositeEndFinder, OptimizationActivation.getOption());
+    }
+
+    public TriggerManagerImpl(ActivationOption impactAnalysisConfiguration) {
+        this(new DefaultOppositeEndFinder(EPackage.Registry.INSTANCE), impactAnalysisConfiguration);
+    }
+
+    public TriggerManagerImpl(OppositeEndFinder oppositeEndFinder, ActivationOption impactAnalysisConfiguration) {
+        eventManager = EventManagerFactory.eINSTANCE.createEventManager();
+        this.oppositeEndFinder = oppositeEndFinder;
+        this.impactAnalysisConfiguration = impactAnalysisConfiguration;
+    }
+
     @Override
     public void register(Triggerable triggerable) {
-        // TODO Implement TriggerManagerImpl.register(...)
-
+        for (ExpressionWithContext expressionInOCL : triggerable.getTriggerExpressions()) {
+            AdapterForExpression adapter = new AdapterForExpression(triggerable, expressionInOCL.getExpression(),
+                    expressionInOCL.getContext(), oppositeEndFinder, impactAnalysisConfiguration);
+            EventFilter filter = adapter.getEventFilter(expressionInOCL.isNotifyNewContextElements());
+            eventManager.subscribe(filter, adapter);
+        }
     }
 
     @Override
