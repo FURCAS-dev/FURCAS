9e0918d3530e9047175fda3a84ba69611e423af7
Axel Uhl <axel.uhl@sap.com>
2010-10-13 17:09:16 +0200

    started a project that will receive the migrated concepts that used to be DelayedReference, GlobalDelayedReferenceResolver, etc.

---
+++ DSLEngineering/com.sap.furcas.runtime.referenceresolving/src/com/sap/furcas/runtime/referenceresolving/OCLBasedModelUpdater.java
@@ -0,0 +1,17 @@
+package com.sap.furcas.runtime.referenceresolving;
+
+import org.eclipse.ocl.ecore.OCLExpression;
+
+import com.sap.emf.ocl.trigger.Triggerable;
+
+/**
+ * Based on one or more {@link OCLExpression OCL expressions}, this object may update a model whenever the value
+ * of this or these expression(s) change(s). The OCL Impact Analysis helps to contain the ripple effects to
+ * a small superset of the objects really affected by a model change.
+ * 
+ * @author Axel Uhl (D043530)
+ *
+ */
+public interface OCLBasedModelUpdater extends Triggerable {
+
+}
