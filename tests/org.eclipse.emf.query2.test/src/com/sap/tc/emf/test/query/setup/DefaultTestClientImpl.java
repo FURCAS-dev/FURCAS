bd883a242fbfc39fca94f838b9c1869433141726
akrsharma <akrsharma>
2010-10-14 15:17:00 +0000

    removed unused imports

--- tests/org.eclipse.emf.query2.test/src/com/sap/tc/emf/test/query/setup/DefaultTestClientImpl.java
+++ tests/org.eclipse.emf.query2.test/src/com/sap/tc/emf/test/query/setup/DefaultTestClientImpl.java
@@ -1,27 +1,14 @@
-/*******************************************************************************
- * Copyright (c) 2006, 2009 SAP AG and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     SAP AG - initial API and implementation
- *******************************************************************************/
 package com.sap.tc.emf.test.query.setup;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 
-import junit.framework.Assert;
-
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 
 public class DefaultTestClientImpl implements TestClient {
 
