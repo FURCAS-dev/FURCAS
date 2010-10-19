5c6b2cd2fbffa711bc56c7a21ec4643271b6603a
Thomas Goldschmidt <thomas.goldschmidt@de.abb.com>
2010-10-19 13:03:17 +0200

    moved type attribute from LexedToken to AbstractToken

--- DSLEngineering/com.sap.furcas.metamodel/src/com/sap/furcas/metamodel/textblocks/impl/LexedTokenImpl.java
+++ DSLEngineering/com.sap.furcas.metamodel/src/com/sap/furcas/metamodel/textblocks/impl/LexedTokenImpl.java
@@ -23,7 +23,6 @@ import org.eclipse.emf.ecore.impl.ENotificationImpl;
  * The following features are implemented:
  * <ul>
  *   <li>{@link com.sap.furcas.metamodel.textblocks.impl.LexedTokenImpl#isOperator <em>Operator</em>}</li>
- *   <li>{@link com.sap.furcas.metamodel.textblocks.impl.LexedTokenImpl#getType <em>Type</em>}</li>
  * </ul>
  * </p>
  *
@@ -31,190 +30,137 @@ import org.eclipse.emf.ecore.impl.ENotificationImpl;
  */
 public class LexedTokenImpl extends AbstractTokenImpl implements LexedToken {
         /**
-         * The default value of the '{@link #isOperator() <em>Operator</em>}' attribute.
-         * <!-- begin-user-doc -->
+	 * The default value of the '{@link #isOperator() <em>Operator</em>}' attribute.
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @see #isOperator()
-         * @generated
-         * @ordered
-         */
+	 * @see #isOperator()
+	 * @generated
+	 * @ordered
+	 */
         protected static final boolean OPERATOR_EDEFAULT = false;
 
         /**
-         * The cached value of the '{@link #isOperator() <em>Operator</em>}' attribute.
-         * <!-- begin-user-doc -->
+	 * The cached value of the '{@link #isOperator() <em>Operator</em>}' attribute.
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @see #isOperator()
-         * @generated
-         * @ordered
-         */
+	 * @see #isOperator()
+	 * @generated
+	 * @ordered
+	 */
         protected boolean operator = OPERATOR_EDEFAULT;
 
         /**
-         * The default value of the '{@link #getType() <em>Type</em>}' attribute.
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @see #getType()
-         * @generated
-         * @ordered
-         */
-        protected static final int TYPE_EDEFAULT = 0;
-
-        /**
-         * The cached value of the '{@link #getType() <em>Type</em>}' attribute.
-         * <!-- begin-user-doc -->
-         * <!-- end-user-doc -->
-         * @see #getType()
-         * @generated
-         * @ordered
-         */
-        protected int type = TYPE_EDEFAULT;
-
-        /**
-         * <!-- begin-user-doc -->
-         * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         protected LexedTokenImpl() {
-                super();
-        }
+		super();
+	}
 
         /**
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         @Override
         protected EClass eStaticClass() {
-                return TextblocksPackage.Literals.LEXED_TOKEN;
-        }
+		return TextblocksPackage.Literals.LEXED_TOKEN;
+	}
 
         /**
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         public boolean isOperator() {
-                return operator;
-        }
+		return operator;
+	}
 
         /**
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         public void setOperator(boolean newOperator) {
-                boolean oldOperator = operator;
-                operator = newOperator;
-                if (eNotificationRequired())
-                        eNotify(new ENotificationImpl(this, Notification.SET, TextblocksPackage.LEXED_TOKEN__OPERATOR, oldOperator, operator));
-        }
+		boolean oldOperator = operator;
+		operator = newOperator;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, TextblocksPackage.LEXED_TOKEN__OPERATOR, oldOperator, operator));
+	}
 
         /**
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @generated
-         */
-        public int getType() {
-                return type;
-        }
-
-        /**
-         * <!-- begin-user-doc -->
-         * <!-- end-user-doc -->
-         * @generated
-         */
-        public void setType(int newType) {
-                int oldType = type;
-                type = newType;
-                if (eNotificationRequired())
-                        eNotify(new ENotificationImpl(this, Notification.SET, TextblocksPackage.LEXED_TOKEN__TYPE, oldType, type));
-        }
-
-        /**
-         * <!-- begin-user-doc -->
-         * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         @Override
         public Object eGet(int featureID, boolean resolve, boolean coreType) {
-                switch (featureID) {
-                        case TextblocksPackage.LEXED_TOKEN__OPERATOR:
-                                return isOperator();
-                        case TextblocksPackage.LEXED_TOKEN__TYPE:
-                                return getType();
-                }
-                return super.eGet(featureID, resolve, coreType);
-        }
+		switch (featureID) {
+			case TextblocksPackage.LEXED_TOKEN__OPERATOR:
+				return isOperator();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
 
         /**
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         @Override
         public void eSet(int featureID, Object newValue) {
-                switch (featureID) {
-                        case TextblocksPackage.LEXED_TOKEN__OPERATOR:
-                                setOperator((Boolean)newValue);
-                                return;
-                        case TextblocksPackage.LEXED_TOKEN__TYPE:
-                                setType((Integer)newValue);
-                                return;
-                }
-                super.eSet(featureID, newValue);
-        }
+		switch (featureID) {
+			case TextblocksPackage.LEXED_TOKEN__OPERATOR:
+				setOperator((Boolean)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
 
         /**
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         @Override
         public void eUnset(int featureID) {
-                switch (featureID) {
-                        case TextblocksPackage.LEXED_TOKEN__OPERATOR:
-                                setOperator(OPERATOR_EDEFAULT);
-                                return;
-                        case TextblocksPackage.LEXED_TOKEN__TYPE:
-                                setType(TYPE_EDEFAULT);
-                                return;
-                }
-                super.eUnset(featureID);
-        }
+		switch (featureID) {
+			case TextblocksPackage.LEXED_TOKEN__OPERATOR:
+				setOperator(OPERATOR_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
 
         /**
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         @Override
         public boolean eIsSet(int featureID) {
-                switch (featureID) {
-                        case TextblocksPackage.LEXED_TOKEN__OPERATOR:
-                                return operator != OPERATOR_EDEFAULT;
-                        case TextblocksPackage.LEXED_TOKEN__TYPE:
-                                return type != TYPE_EDEFAULT;
-                }
-                return super.eIsSet(featureID);
-        }
+		switch (featureID) {
+			case TextblocksPackage.LEXED_TOKEN__OPERATOR:
+				return operator != OPERATOR_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
 
         /**
-         * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
          * <!-- end-user-doc -->
-         * @generated
-         */
+	 * @generated
+	 */
         @Override
         public String toString() {
-                if (eIsProxy()) return super.toString();
+		if (eIsProxy()) return super.toString();
 
-                StringBuffer result = new StringBuffer(super.toString());
-                result.append(" (operator: ");
-                result.append(operator);
-                result.append(", type: ");
-                result.append(type);
-                result.append(')');
-                return result.toString();
-        }
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (operator: ");
+		result.append(operator);
+		result.append(')');
+		return result.toString();
+	}
 
 } //LexedTokenImpl
