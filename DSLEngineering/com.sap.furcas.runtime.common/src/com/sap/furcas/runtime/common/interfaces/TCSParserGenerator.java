874dbaaa1488b35eb3dd537baf2629d8b40392cc
Stephan Erb <stephan@dev.static-void.de>
2010-11-04 23:25:01 +0100

    Minor documentation enhancements.

--- DSLEngineering/com.sap.furcas.runtime.common/src/com/sap/furcas/runtime/common/interfaces/TCSParserGenerator.java
+++ DSLEngineering/com.sap.furcas.runtime.common/src/com/sap/furcas/runtime/common/interfaces/TCSParserGenerator.java
@@ -1,5 +1,13 @@
 package com.sap.furcas.runtime.common.interfaces;
 
+/**
+ * Generator facade featuring the generation of ANTLR grammars from TCS syntaxes,
+ * and subsequently, featuring the generation of lexers and parsers from these
+ * grammars.
+ * 
+ * @author Stephan Erb (d049157)
+ *
+ */
 public interface TCSParserGenerator {
     
     public String extTest();
