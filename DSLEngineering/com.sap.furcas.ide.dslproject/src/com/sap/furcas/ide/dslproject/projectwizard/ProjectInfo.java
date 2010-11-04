3536df7c262c932d2ee6ff1bedfedd27f64b8b12
Stephan Erb <stephan@dev.static-void.de>
2010-11-04 11:00:50 +0100

    Revert "Deleted nonworking old wizardproject"
    This reverts commit 6573c8b400fb4f6ad19076b85531a460a1955cea.

---
+++ DSLEngineering/com.sap.furcas.ide.dslproject/src/com/sap/furcas/ide/dslproject/projectwizard/ProjectInfo.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ *
+ *******************************************************************************/
+package com.sap.furcas.ide.dslproject.projectwizard;
+
+public class ProjectInfo {
+    private String projectName;
+    private String languageName;
+    private String fileExtension;
+    private String nsURI;
+    private String basePackage;
+    private boolean createGeneratorProject = false;
+
+    public boolean isCreateGeneratorProject() {
+	return createGeneratorProject;
+    }
+
+    public void setCreateGeneratorProject(boolean createGeneratorProject) {
+	this.createGeneratorProject = createGeneratorProject;
+    }
+
+    public String getBasePackage() {
+	return basePackage;
+    }
+
+    public void setBasePackage(String basePackage) {
+	this.basePackage = basePackage;
+    }
+
+    public String getFileExtension() {
+	return fileExtension;
+    }
+
+    public void setFileExtension(String fileExtension) {
+	this.fileExtension = fileExtension;
+    }
+
+    public String getLanguageName() {
+	return languageName;
+    }
+
+    public void setLanguageName(String languageName) {
+	this.languageName = languageName;
+    }
+
+    public String getNsURI() {
+	return nsURI;
+    }
+
+    public void setNsURI(String nsURI) {
+	this.nsURI = nsURI;
+    }
+
+    public String getProjectName() {
+	return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+	this.projectName = projectName;
+    }
+
+    public String getTCSFileName() {
+	return getLanguageName().replaceAll("\\s", "_") + ".tcs"; // +"xtxt";
+    }
+
+    public String getBasePath() {
+	return getBasePackage().replaceAll("\\.", "/");
+    }
+
+}
