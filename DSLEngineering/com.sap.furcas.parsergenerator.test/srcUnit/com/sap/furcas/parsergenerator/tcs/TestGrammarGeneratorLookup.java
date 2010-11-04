1f46c1937f74049ef81a32cc3ed1af530112537a
Stephan Erb <stephan@dev.static-void.de>
2010-11-04 23:26:35 +0100

    Implement a test for the grammar generator extensionpoint.

---
+++ DSLEngineering/com.sap.furcas.parsergenerator.test/srcUnit/com/sap/furcas/parsergenerator/tcs/TestGrammarGeneratorLookup.java
@@ -0,0 +1,25 @@
+package com.sap.furcas.parsergenerator.tcs;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.sap.furcas.runtime.common.exceptions.ParserGeneratorInvocationException;
+import com.sap.furcas.runtime.common.interfaces.TCSParserGenerator;
+import com.sap.furcas.runtime.common.interfaces.TCSParserGeneratorFactory;
+
+
+/**
+ * Tests that the parsergenerator extensoin point works as expected.
+ * 
+ * @author Stephan Erb (d049157)
+ */
+public class TestGrammarGeneratorLookup {
+
+    
+    @Test
+    public void testLookup() throws ParserGeneratorInvocationException {
+         TCSParserGenerator generator = TCSParserGeneratorFactory.INSTANCE.createTCSParserGenerator();
+         assertNotNull("Parsergenerator project should provide an implementation", generator);
+    }
+}
