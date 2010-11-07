91d4c4e0be77f0f4c4e414e1f88533f734a21964
Stephan Erb <stephan@dev.static-void.de>
2010-11-07 22:44:48 +0100

    Introduce a TCSParserFactory.
    We will later use to run the scenario suite with bootstrapped TCS
    parsers.

---
+++ DSLEngineering/com.sap.furcas.parser.tcs/src/com/sap/furcas/parser/tcs/impl/TCSParserFactoryImpl.java
@@ -0,0 +1,17 @@
+package com.sap.furcas.parser.tcs.impl;
+
+import com.sap.furcas.parser.tcs.TCSParserFactory;
+import com.sap.furcas.runtime.parser.ParserFacade;
+import com.sap.furcas.runtime.parser.exceptions.InvalidParserImplementationException;
+
+public class TCSParserFactoryImpl implements TCSParserFactory {
+
+    @Override
+    public ParserFacade createTCSParserFacade() throws InvalidParserImplementationException {
+        // TODO: make it configurable whether a stable or an unstable version is used        
+        return new ParserFacade(
+                com.sap.furcas.parser.tcs.stable.TCSParser.class,
+                com.sap.furcas.parser.tcs.stable.TCSLexer.class);
+    }
+
+}
