b4076cfde6c3ffe989d8f2100ab75b20e53376c0
Stephan Erb <stephan@dev.static-void.de>
2010-11-08 22:40:55 +0100

    Minor Bootstrap cleanup.

---
+++ DSLEngineering/com.sap.furcas.parser.tcs/bootstrap/com/sap/furcas/parser/tcs/bootstrap/SystemOutErrorHandler.java
@@ -0,0 +1,49 @@
+package com.sap.furcas.parser.tcs.bootstrap;
+
+import org.antlr.tool.Message;
+
+import com.sap.furcas.parsergenerator.GenerationErrorHandler;
+import com.sap.furcas.runtime.parser.ParsingError;
+
+class SystemOutErrorHandler implements GenerationErrorHandler {
+
+    private boolean failedWithError = false;
+
+    @Override
+    public void error(ParsingError exception) {
+        failedWithError = true;
+        System.err.println("ERROR: " + exception.getMessage());
+    }
+
+    @Override
+    public void fatalError(ParsingError exception) {
+        failedWithError = true;
+        System.err.println("FATAL ERROR: " + exception.getMessage());
+    }
+
+    @Override
+    public void warning(ParsingError exception) {
+        System.out.println("WARNING: " + exception.getMessage());
+    }
+
+    @Override
+    public void error(Message err) {
+        failedWithError = true;
+        System.err.println("ERROR: " + err);
+    }
+
+    @Override
+    public void warn(Message warn) {
+        System.out.println("WARNING: " + warn);
+    }
+
+    @Override
+    public void info(String info) {
+        System.out.println("INFO: " + info);
+    }
+
+    public boolean hasFailedWithError() {
+        return failedWithError;
+    }
+
+}
--- DSLEngineering/com.sap.furcas.parser.tcs/src/com/sap/furcas/parser/tcs/unstable/README.txt
+++
@@ -1,3 +0,0 @@
-Contains the most recently bootstrapped TCS-Parser.
-
-If this parsers passes all tests it will be promoted to as stable.
