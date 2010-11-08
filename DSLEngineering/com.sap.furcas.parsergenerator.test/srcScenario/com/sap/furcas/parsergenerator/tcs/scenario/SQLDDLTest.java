04c3c5d04d286ca47ce1597da22019b0d44a47d7
Sebastian Schlag <sebastian@dev.static-void.de>
2010-11-08 14:56:30 +0100

    moved test baseclasses to com.sap.furcas.test

--- DSLEngineering/com.sap.furcas.parsergenerator.test/srcScenario/com/sap/furcas/parsergenerator/tcs/scenario/SQLDDLTest.java
+++ DSLEngineering/com.sap.furcas.parsergenerator.test/srcScenario/com/sap/furcas/parsergenerator/tcs/scenario/SQLDDLTest.java
@@ -5,11 +5,11 @@ import java.io.File;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.sap.furcas.parsergenerator.base.GeneratedParserBasedTest;
-import com.sap.furcas.parsergenerator.base.GeneratedParserTestConfiguration;
-import com.sap.furcas.parsergenerator.base.ParsingHelper;
-import com.sap.furcas.parsergenerator.base.StubModelAdapter;
 import com.sap.furcas.runtime.parser.ParserFacade;
+import com.sap.furcas.test.base.GeneratedParserBasedTest;
+import com.sap.furcas.test.base.GeneratedParserTestConfiguration;
+import com.sap.furcas.test.base.ParsingHelper;
+import com.sap.furcas.test.base.StubModelAdapter;
 import com.sap.furcas.test.fixture.FixtureData;
 
 /**
