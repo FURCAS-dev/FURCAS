278ceb1afacf62e2d5006c4ebd960a3e05036cf8
Sebastian Schlag <sebastian@dev.static-void.de>
2010-11-11 20:34:42 +0100

    created ModelBasedTCSGrammarGenerator and SyntaxParser

---
+++ DSLEngineering/com.sap.furcas.parsergenerator/src/com/sap/furcas/parsergenerator/tcs/generator/SyntaxParser.java
@@ -0,0 +1,74 @@
+package com.sap.furcas.parsergenerator.tcs.generator;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import com.sap.furcas.parsergenerator.GrammarGenerationSourceConfiguration;
+import com.sap.furcas.parsergenerator.emf.tcs.inject.ModelInjectionResult;
+import com.sap.furcas.parsergenerator.emf.tcs.inject.TCSSpecificEMFModelInjector;
+import com.sap.furcas.parsergenerator.tcs.t2m.TCSSyntaxContainerBean;
+import com.sap.furcas.runtime.common.exceptions.ParserInvokationException;
+import com.sap.furcas.runtime.parser.ParsingError;
+import com.sap.furcas.runtime.parser.exceptions.InvalidParserImplementationException;
+import com.sap.furcas.runtime.parser.exceptions.SyntaxParsingException;
+import com.sap.furcas.runtime.parser.exceptions.UnknownProductionRuleException;
+
+/**
+ * 
+ * @author Sebastian Schlag (D049672)
+ * 
+ */
+public class SyntaxParser {
+
+    public static TCSSyntaxContainerBean parse(GrammarGenerationSourceConfiguration sourceConfiguration)
+            throws SyntaxParsingException, ParserInvokationException {
+
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(sourceConfiguration.getSyntaxDefinitionFile());
+
+            // By choosing this injector, we establish the dependency to EMF.
+            ModelInjectionResult result = TCSSpecificEMFModelInjector.parseSyntaxDefinition(inputStream,
+                    sourceConfiguration.getResourceSet(), sourceConfiguration.getReferenceScope(), /* observer */null);
+
+            List<ParsingError> errors = result.getResult().getErrors();
+            if (errors != null && errors.size() > 0) {
+                if (result.getSyntax() != null) {
+                    // also clean up unfinished syntax
+                    EcoreUtil.delete(result.getSyntax(), /* recursive */true);
+                }
+                throw new SyntaxParsingException(errors);
+            }
+
+            TCSSyntaxContainerBean returnBean = new TCSSyntaxContainerBean();
+            returnBean.setSyntax(result.getSyntax());
+            returnBean.setKeywords(result.getKeywords());
+            returnBean.setElementToLocationMap(result.getResult().getLocationMap());
+
+            return returnBean;
+
+        } catch (FileNotFoundException e) {
+            throw new ParserInvokationException(e);
+        } catch (InvalidParserImplementationException e) {
+            throw new ParserInvokationException(e);
+        } catch (IOException e) {
+            throw new ParserInvokationException(e);
+        } catch (UnknownProductionRuleException e) {
+            throw new ParserInvokationException(e);
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e) {
+                throw new ParserInvokationException(e);
+            }
+        }
+
+    }
+}
---
+++ DSLEngineering/com.sap.furcas.parsergenerator/src/com/sap/furcas/parsergenerator/tcs/t2m/ModelBasedTCSGrammarGenerator.java
@@ -0,0 +1,36 @@
+package com.sap.furcas.parsergenerator.tcs.t2m;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+
+import com.sap.furcas.runtime.common.exceptions.ModelAdapterException;
+import com.sap.furcas.runtime.common.exceptions.ParserInvokationException;
+import com.sap.furcas.runtime.common.interfaces.IMetaModelLookup;
+import com.sap.furcas.runtime.parser.exceptions.SyntaxParsingException;
+
+public class ModelBasedTCSGrammarGenerator extends AbstractTCSGrammarGenerator {
+
+    private TCSSyntaxContainerBean containerBean;
+
+    public ModelBasedTCSGrammarGenerator(OutputStream out, IMetaModelLookup<?> lookup, String targetPackage,
+            TCSSyntaxContainerBean containerBean) {
+        super(out, lookup, targetPackage);
+        this.containerBean = containerBean;
+    }
+
+    @Override
+    protected TCSSyntaxContainerBean doGetSyntaxDef(ResourceSet resourceSet, Set<URI> metamodelURIs)
+            throws SyntaxParsingException, IOException, ModelAdapterException, ParserInvokationException {
+        return this.containerBean;
+    }
+
+    @Override
+    protected TCSSyntaxContainerBean doGetSyntaxDef(ResourceSet resourceSet, Set<URI> metamodelURIs, String languageId)
+            throws SyntaxParsingException, IOException, ModelAdapterException, ParserInvokationException {
+        return this.containerBean;
+    }
+}
