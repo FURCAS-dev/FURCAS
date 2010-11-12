4f3e1ad060e76cd59e527946681f15baac019a5b
Frederik Petersen <frederik.petersen@sap.com>
2010-11-12 09:56:01 +0100

    Wizard now uses StringTemplate and text files to generate code

--- DSLEngineering/com.sap.furcas.ide.projectwizard/src/com/sap/furcas/ide/projectwizard/wizards/FurcasWizardMMSelectionPage.java
+++ DSLEngineering/com.sap.furcas.ide.projectwizard/src/com/sap/furcas/ide/projectwizard/wizards/FurcasWizardMMSelectionPage.java
@@ -1,7 +1,6 @@
-	package com.sap.furcas.ide.projectwizard.wizards;
+package com.sap.furcas.ide.projectwizard.wizards;
 
-
-	import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -12,101 +11,108 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
+public class FurcasWizardMMSelectionPage extends WizardPage {
+	Button selectmm1;
+	Button selectmm2;
+	private MMLoadPage page3;
+	FurcasWizard wiz;
 
-	public class FurcasWizardMMSelectionPage extends WizardPage {
-        Button selectmm1;
-        Button selectmm2;
-        private MMLoadPage page3;
-        FurcasWizard wiz;
-        
-		public FurcasWizardMMSelectionPage(ISelection selection, FurcasWizard w) {
-	        super("Metamodel");
-	        page3=null;
-			wiz = w;
-	        setTitle("Metamodelselection");
-	        setDescription("Choose an existing Metamodel or create a new one");
-			setPageComplete(false);
-	    }
-
-		@Override
-		public void createControl(Composite parent) {
-			Composite container = new Composite(parent, SWT.NULL);
-	        GridLayout layout = new GridLayout();
-	        container.setLayout(layout);
-	        layout.numColumns = 2;
-	        layout.verticalSpacing = 9;
-	        
-	       //First option
-	        selectmm1 = new Button(container, SWT.RADIO);
-	        GridData gd = new GridData(GridData.FILL_HORIZONTAL);
-	        selectmm1.setText("Create new METAMODEL-project");
-	        selectmm1.setLayoutData(gd);
-	        selectmm1.addMouseListener(new MouseListener() {
-				public void mouseDoubleClick(MouseEvent e) {}
-				public void mouseDown(MouseEvent e) {
-					if (!selectmm1.getSelection()){
-					  selectmm1.setSelection(true);
-					  selectmm2.setSelection(false);
-					}
-					  getNextPage();
-					  setPageComplete(true);
-				}
-				public void mouseUp(MouseEvent e) {}
-	        });
-	       // second option
-	        selectmm2 = new Button(container, SWT.RADIO);
-	        GridData gd2 = new GridData(GridData.FILL_HORIZONTAL);
-	        selectmm2.setText("Use existing METAMODEL");
-	        selectmm2.setLayoutData(gd2);
-	        selectmm2.addMouseListener(new MouseListener() {
-				public void mouseDoubleClick(MouseEvent e) {}
-				public void mouseDown(MouseEvent e) {
-					if (!selectmm2.getSelection())
-					{
-						  selectmm1.setSelection(false);
-						  selectmm2.setSelection(true);
-						}
-					  getNextPage();
-				}
-				public void mouseUp(MouseEvent e) {}
-	        });
-	        
-	        
-	        setErrorMessage(null);
-	        setControl(container);
-	        
-
-		}
-        public Integer getSel() {
-        	Integer sel = null;
-        	  if (selectmm1.getSelection()==true)
-        		  sel=1;
-        	  else if (selectmm2.getSelection()==true)
-        		  sel=2;
-        	  return sel;
-        }
-        @Override
-        public IWizardPage getNextPage(){
-        	if (getSel()==2){
-        		if (page3 == null){
-        		page3 = new MMLoadPage("LoadPage", wiz);
-        		wiz.addPage(page3);
-        		page3.setPageComplete(false);
-        		this.setPageComplete(true);
-        		wiz.enableLoadPage();
-        		}
-        		if (wiz.createmm){
-            		wiz.createmm=false;
-        		}
-        		return page3;    		
-        	}
-        	else if (getSel()==1)
-        		wiz.addMMP();
-            return null;
-        	}
-        
-       
+	public FurcasWizardMMSelectionPage(ISelection selection, FurcasWizard w) {
+		super("Metamodel");
+		page3 = null;
+		wiz = w;
+		setTitle("Metamodelselection");
+		setDescription("Choose an existing Metamodel or create a new one");
+		setPageComplete(false);
 	}
+
+	@Override
+	public void createControl(Composite parent) {
+		Composite container = new Composite(parent, SWT.NULL);
+		GridLayout layout = new GridLayout();
+		container.setLayout(layout);
+		layout.numColumns = 2;
+		layout.verticalSpacing = 9;
+
+		// First option
+		selectmm1 = new Button(container, SWT.RADIO);
+		GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+		selectmm1.setText("Create new METAMODEL-project");
+		selectmm1.setLayoutData(gd);
+		selectmm1.addMouseListener(new MouseListener() {
+			public void mouseDoubleClick(MouseEvent e) {
+			}
+
+			public void mouseDown(MouseEvent e) {
+				if (!selectmm1.getSelection()) {
+					selectmm1.setSelection(true);
+					selectmm2.setSelection(false);
+				}
+				getNextPage();
+				setPageComplete(true);
+			}
+
+			public void mouseUp(MouseEvent e) {
+			}
+		});
+		// second option
+		selectmm2 = new Button(container, SWT.RADIO);
+		GridData gd2 = new GridData(GridData.FILL_HORIZONTAL);
+		selectmm2.setText("Use existing METAMODEL");
+		selectmm2.setLayoutData(gd2);
+		selectmm2.addMouseListener(new MouseListener() {
+			public void mouseDoubleClick(MouseEvent e) {
+			}
+
+			public void mouseDown(MouseEvent e) {
+				if (!selectmm2.getSelection()) {
+					selectmm1.setSelection(false);
+					selectmm2.setSelection(true);
+				}
+				getNextPage();
+				setPageComplete(true);
+			}
+
+			public void mouseUp(MouseEvent e) {
+			}
+		});
+
+		setErrorMessage(null);
+		setControl(container);
+
+	}
+
+	public Integer getSel() {
+		Integer sel = 0;
+		if (selectmm1.getSelection() == true)
+			sel = 1;
+		else if (selectmm2.getSelection() == true)
+			sel = 2;
+		return sel;
+	}
+
+	@Override
+	public IWizardPage getNextPage() {
+		if (getSel() == 2) {
+			if (page3 == null) {
+				page3 = new MMLoadPage("LoadPage", wiz);
+				wiz.addPage(page3);
+				page3.setPageComplete(false);
+				this.setPageComplete(true);
+				wiz.enableLoadPage();
+			}
+			if (wiz.createmm) {
+				wiz.createmm = false;
+			}
+			return page3;
+		} else if (getSel() == 1)
+			if(page3!=null)
+				page3.setPageComplete(true);
+			wiz.addMMP();
+		return null;
+	}
+
+}
 
 			//Code snippet for starting a wizard
 			
