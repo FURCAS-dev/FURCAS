fad4df339d2e1c4e86b5bad124384831c52d3115
Stephan Erb <stephan@dev.static-void.de>
2010-11-13 14:42:54 +0100

    Remove unused parameter in GeneratedParseBasedTest.

--- DSLEngineering/com.sap.furcas.parsergenerator.test/srcScenario/com/sap/furcas/parsergenerator/tcs/scenario/BibTextModesTest.java
+++ DSLEngineering/com.sap.furcas.parsergenerator.test/srcScenario/com/sap/furcas/parsergenerator/tcs/scenario/BibTextModesTest.java
@@ -32,7 +32,7 @@ public class BibTextModesTest extends GeneratedParserBasedTest {
     @BeforeClass
     public static void setupParser() throws Exception {
         GeneratedParserTestConfiguration testConfig = new GeneratedParserTestConfiguration(LANGUAGE, TCS, METAMODELS);
-        TCSSyntaxContainerBean syntaxBean = parseSyntax(testConfig, testConfig.getSyntaxDefinitionFile());
+        TCSSyntaxContainerBean syntaxBean = parseSyntax(testConfig);
         ParserFacade facade = generateParserForLanguage(syntaxBean, testConfig, new ClassLookupImpl());
         parsingHelper = new ParsingHelper(facade);
     }
