7ac4065b5f0acd4fd3af4e3470c5146b1539eb6d
Stephan Erb <stephan@dev.static-void.de>
2010-11-15 18:59:32 +0100

    Explicitly setup the scope required to run lookups against the FURCAS metamodel

--- DSLEngineering/com.sap.furcas.test/src/com/sap/furcas/test/testutils/ScopeHelper.java
+++ DSLEngineering/com.sap.furcas.test/src/com/sap/furcas/test/testutils/ScopeHelper.java
@@ -1,9 +1,11 @@
 package com.sap.furcas.test.testutils;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 
@@ -21,5 +23,15 @@ public class ScopeHelper {
         resourceSet.getPackageRegistry().put(FURCASPackage.eNS_URI, FURCASPackage.eINSTANCE);
         resourceSet.getPackageRegistry().put(FURCASPackage.eNAME, FURCASPackage.eINSTANCE);
         return resourceSet;
+    }
+
+    public static Set<URI> createFURCASReferenceScope() {
+        Set<URI> referenceScope = new HashSet<URI>();
+        
+        EPackage rootPackage = FURCASPackage.eINSTANCE;
+        
+        referenceScope.add(URI.createURI(rootPackage.eClass().getEPackage().getNsURI()));
+        referenceScope.add(URI.createURI(rootPackage.getNsURI()));
+        return referenceScope;
     }
 }
