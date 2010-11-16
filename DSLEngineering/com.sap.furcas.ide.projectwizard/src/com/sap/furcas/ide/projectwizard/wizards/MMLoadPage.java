07960da45f4cc86052b931e57db42ea434b94dec
Frederik Petersen <frederik.petersen@sap.com>
2010-11-16 12:12:55 +0100

    Small changes to the wizard including a nonessential playaroundwith page

--- DSLEngineering/com.sap.furcas.ide.projectwizard/src/com/sap/furcas/ide/projectwizard/wizards/MMLoadPage.java
+++ DSLEngineering/com.sap.furcas.ide.projectwizard/src/com/sap/furcas/ide/projectwizard/wizards/MMLoadPage.java
@@ -3,10 +3,14 @@ package com.sap.furcas.ide.projectwizard.wizards;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
-
+import org.eclipse.swt.widgets.Text;
 
 /* 
  * This wizardpage is displayed when the user chooses to import some kind of existing MetaModel into his
@@ -27,17 +31,78 @@ public class MMLoadPage extends WizardPage {
         Composite container = new Composite(parent, SWT.NULL);
         GridLayout layout = new GridLayout();
         container.setLayout(layout);
-        layout.numColumns = 1;
-        layout.verticalSpacing = 1;
+        layout.numColumns = 4;
         Label label = new Label(container, SWT.NULL);
-        label.setText("All you have to do is a right click in the editor view");
-        Label label1 = new Label(container, SWT.NULL);
-        label1.setText("after the wizard has finished. Then click on \"Load Resource...\"");
-        Label label2 = new Label(container, SWT.NULL);
-        label2.setText("Now feel free to choose a desired MetaModel for your DSL.");
+        label.setText("Resource URIs:");
+        
+        
+        Button button1 = new Button(container, SWT.PUSH);
+        button1.setText("Browse Registered Packages...");
+        button1.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseDoubleClick(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseDown(MouseEvent e) {
+                browseRegistered();
+            }
+
+            @Override
+            public void mouseUp(MouseEvent e) {
+            }
+
+        });
+        
+        
+        Button button2 = new Button(container, SWT.PUSH);
+        button2.setText("Browse File System...");
+        button1.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseDoubleClick(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseDown(MouseEvent e) {
+                browseRegistered();
+            }
+
+            @Override
+            public void mouseUp(MouseEvent e) {
+            }
+
+        });
+
+        Button button3 = new Button(container, SWT.PUSH);
+        button3.setText("Browse Workspace...");
+        button1.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseDoubleClick(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseDown(MouseEvent e) {
+                browseRegistered();
+            }
+
+            @Override
+            public void mouseUp(MouseEvent e) {
+            }
+
+        });
+        GridData gd = new GridData();
+        gd.horizontalAlignment = GridData.FILL;
+        gd.horizontalSpan = 4;
+        Text text = new Text(container, SWT.NULL | SWT.BORDER);
+        text.setLayoutData(gd);
         setErrorMessage(null);
         setPageComplete(true);
         setControl(container);
+
+    }
+
+    protected void browseRegistered() {
+        // TODO Auto-generated method stub
 
     }
 
