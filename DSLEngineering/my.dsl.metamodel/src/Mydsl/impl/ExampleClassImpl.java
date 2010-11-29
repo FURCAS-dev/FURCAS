323e29f0e8e31f633f76f5120673c3f0595cb463
Frederik Petersen <frederik.petersen@sap.com>
2010-11-29 08:26:13 +0100

    added pom.xml files for the projectwizard test and its metamodel project
    also generated the metamodel project files and entered both projects into
    the parents pom.xml

---
+++ DSLEngineering/my.dsl.metamodel/src/Mydsl/impl/ExampleClassImpl.java
@@ -0,0 +1,45 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package Mydsl.impl;
+
+import Mydsl.ExampleClass;
+import Mydsl.MydslPackage;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.EObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Example Class</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class ExampleClassImpl extends EObjectImpl implements ExampleClass {
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    protected ExampleClassImpl() {
+        super();
+    }
+
+    /**
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    @Override
+    protected EClass eStaticClass() {
+        return MydslPackage.Literals.EXAMPLE_CLASS;
+    }
+
+} //ExampleClassImpl
