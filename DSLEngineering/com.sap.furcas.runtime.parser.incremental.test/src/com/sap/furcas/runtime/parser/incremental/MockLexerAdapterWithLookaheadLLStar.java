61c6a9013d030f11d421fcfe1cbb1afdaaece1cc
Thomas Goldschmidt <thomas.goldschmidt@de.abb.com>
2010-12-06 09:01:32 +0100

    fixed indentation and removed warning

--- DSLEngineering/com.sap.furcas.runtime.parser.incremental.test/src/com/sap/furcas/runtime/parser/incremental/MockLexerAdapterWithLookaheadLLStar.java
+++ DSLEngineering/com.sap.furcas.runtime.parser.incremental.test/src/com/sap/furcas/runtime/parser/incremental/MockLexerAdapterWithLookaheadLLStar.java
@@ -10,46 +10,44 @@ import com.sap.ide.cts.parser.incremental.IncrementalLexer;
 import com.sap.ide.cts.parser.incremental.antlr.ANTLRIncrementalLexerAdapter;
 import com.sap.ide.cts.parser.incremental.antlr.ANTLRLexerAdapter;
 
-
 public class MockLexerAdapterWithLookaheadLLStar extends ANTLRLexerAdapter {
-	
-	public MockLexerAdapterWithLookaheadLLStar(Lexer antlrLexer) {
-		super(antlrLexer, null);
-	}
 
-	protected List<AbstractToken> nextTokens;
-	protected ANTLRIncrementalLexerAdapter callbackLexer;
-	protected int nextTokenIndex = 0;
+    public MockLexerAdapterWithLookaheadLLStar(Lexer antlrLexer) {
+        super(antlrLexer, null);
+    }
 
-	@Override
-	public int getState(AbstractToken tok) {
-		return 0;
-	}
+    protected List<AbstractToken> nextTokens;
+    protected ANTLRIncrementalLexerAdapter callbackLexer;
+    protected int nextTokenIndex = 0;
 
-	@Override
-	public List<AbstractToken> moreTokens() {
-		//simulate a lookahead of 1 within a LL(*) parser
-		int mark = callbackLexer.mark();
-		callbackLexer.LA(1);
-		callbackLexer.consume();
-		callbackLexer.rewind(mark);
-		//simulate token consumption on callbacked lexer
-		callbackLexer.consume();
-		return Collections.singletonList(nextTokens.get(nextTokenIndex++));
-	}
+    @Override
+    public int getState(AbstractToken tok) {
+        return 0;
+    }
 
-	@Override
-	public void setIncrementalLexer(IncrementalLexer incrementalLexer) {
-		this.callbackLexer = (ANTLRIncrementalLexerAdapter) incrementalLexer;
-	}
+    @Override
+    public List<AbstractToken> moreTokens() {
+        // simulate a lookahead of 1 within a LL(*) parser
+        int mark = callbackLexer.mark();
+        callbackLexer.LA(1);
+        callbackLexer.consume();
+        callbackLexer.rewind(mark);
+        // simulate token consumption on callbacked lexer
+        callbackLexer.consume();
+        return Collections.singletonList(nextTokens.get(nextTokenIndex++));
+    }
 
-	@Override
-	public void setState(int state) {
-	}
+    @Override
+    public void setIncrementalLexer(IncrementalLexer incrementalLexer) {
+        this.callbackLexer = (ANTLRIncrementalLexerAdapter) incrementalLexer;
+    }
 
-	public void setNextTokens( List<AbstractToken> nextTokens) {
-		this.nextTokens = nextTokens;
-	}
+    @Override
+    public void setState(int state) {
+    }
 
+    public void setNextTokens(List<AbstractToken> nextTokens) {
+        this.nextTokens = nextTokens;
+    }
 
 }
