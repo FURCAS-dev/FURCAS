1dca58fd6df5d6efbd5208687b257bc7b00cab4c
akrsharma <akrsharma>
2010-12-09 05:33:30 +0000

    Corrected seperator

--- tests/org.eclipse.emf.query.index.test/src/org/eclipse/emf/query/index/test/TestIndexNotSufficient.java
+++ tests/org.eclipse.emf.query.index.test/src/org/eclipse/emf/query/index/test/TestIndexNotSufficient.java
@@ -305,7 +305,7 @@ public class TestIndexNotSufficient extends Assert {
 	}
 
 	private static String getTempDir() {
-		return System.getProperty("java.io.tmpdir") + File.pathSeparator + "index";
+		return System.getProperty("java.io.tmpdir") + File.separator + "index";
 	}
 
 	@Test
