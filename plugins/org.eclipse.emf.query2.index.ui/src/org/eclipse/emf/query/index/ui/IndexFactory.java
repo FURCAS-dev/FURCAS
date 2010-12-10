3e77d5d19e923085734617519391b55a0ac1c75e
akrsharma <akrsharma>
2010-12-10 07:04:44 +0000

    Fix for bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=325827

--- plugins/org.eclipse.emf.query2.index.ui/src/org/eclipse/emf/query/index/ui/IndexFactory.java
+++ plugins/org.eclipse.emf.query2.index.ui/src/org/eclipse/emf/query/index/ui/IndexFactory.java
@@ -1,8 +1,11 @@
 package org.eclipse.emf.query.index.ui;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.query.index.Index;
 import org.eclipse.emf.query.index.internal.impl.PageableIndexImpl;
@@ -11,12 +14,19 @@ import org.eclipse.emf.query.index.update.IndexUpdater;
 import org.eclipse.emf.query.index.update.ResourceIndexer;
 import org.eclipse.emf.query.index.update.UpdateCommandAdapter;
 
+/**
+ * A Singleton factory for {@link Index}. It assumes that index are located
+ * Activator.getDefault().getStateLocation() and tries to load it. While the IDE
+ * is closing, users needs to dump the indexes using dumpIndexes()
+ */
 public class IndexFactory {
 
 	private static final Index index;
 
 	static {
-		index = new PageableIndexImpl(Options.PAGING_AND_DUMPING_DISABLED);
+		// This option enables Paging and Dump. So that it can be used while IDE
+		// starts and indexes would not be built all the time.
+		index = new PageableIndexImpl(getOptions());
 		index.executeUpdateCommand(new UpdateCommandAdapter() {
 
 			public void execute(final IndexUpdater updater) {
@@ -42,6 +52,47 @@ public class IndexFactory {
 	}
 
 	public static Index getInstance() {
+		return index;
+	}
+
+	/**
+	 * @return Options required for paging and dumping the Indexes built.
+	 */
+	private static Options getOptions() {
+		return new Options(getDirectoryToDumpIndices(), Options.DISABLED, Options.DISABLED);
+	}
+
+	/**
+	 * @return The directory for dumping the indexes.
+	 */
+	private static String getDirectoryToDumpIndices() {
+		IPath baseDirectory = Activator.getDefault().getStateLocation().addTrailingSeparator();
+		
+		IPath indexDirectoryPath = new Path(baseDirectory.toString() + Path.SEPARATOR + "index" + Path.SEPARATOR); //$NON-NLS-1$
+		File indexDir = new File(indexDirectoryPath.toString());
+		if(!indexDir.exists()) {
+			indexDir.mkdir();
+		}
+		
+		return indexDirectoryPath.toString();
+	}
+
+	/**
+	 * Dumps the index
+	 */
+	public static void dumpIndexes() {
+		long currentTimeMillis = System.currentTimeMillis();
+		index.save();
+		System.out.println("Time taken to load: "+ (System.currentTimeMillis() - currentTimeMillis));
+	}
+
+	/**
+	 * Dumps the index
+	 */
+	public static Index loadIndexes() {
+		long currentTimeMillis = System.currentTimeMillis();
+		index.load();
+		System.out.println("Time taken to load: "+ (System.currentTimeMillis() - currentTimeMillis));
 		return index;
 	}
 }
