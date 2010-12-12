94a820d30e05d92fd4031036789243191d26d563
Sebastian Schlag <sebastian@dev.static-void.de>
2010-12-12 16:50:40 +0100

    created a feature test for foreach predicate property init that tests if the foreach predicate is correctly executed

--- DSLEngineering/com.sap.furcas.test/src/com/sap/furcas/test/fixture/FeatureFixtureData.java
+++ DSLEngineering/com.sap.furcas.test/src/com/sap/furcas/test/fixture/FeatureFixtureData.java
@@ -15,5 +15,9 @@ public class FeatureFixtureData {
     // QueryByIdentifierPArg Tests
     public static final File PARG_QUERY_BY_IDENTIFIER_METAMODEL = new File("../com.sap.furcas.test/featureFixtures/PropertyArgument_QueryByIdentifier.ecore");
     public static final File PARG_QUERY_BY_IDENTIFIER_TCS = new File("../com.sap.furcas.test/featureFixtures/PropertyArgument_QueryByIdentifier.tcs");
+    
+    //ForeachInOCLExpr Tests
+    public static final File FOREACH_PREDICATE_PROPERTY_INIT_METAMODEL = new File("../com.sap.furcas.test/featureFixtures/ForeachPredicatePropertyInit.ecore");
+    public static final File FOREACH_PREDICATE_PROPERTY_INIT_TCS = new File("../com.sap.furcas.test/featureFixtures/ForeachPredicatePropertyInit.tcs");
 
 }
