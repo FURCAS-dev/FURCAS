278719e1352a194389d3a17a55b882aacebfe0cb
Frederik Petersen <frederik.petersen@sap.com>
2010-12-14 13:29:29 +0100

    changes to wizards test

--- DSLEngineering/com.sap.furcas.ide.projectwizard.test/src/com/sap/furcas/ide/projectwizard/test/OverallWizardTest.java
+++ DSLEngineering/com.sap.furcas.ide.projectwizard.test/src/com/sap/furcas/ide/projectwizard/test/OverallWizardTest.java
@@ -1,5 +1,31 @@
 package com.sap.furcas.ide.projectwizard.test;
 
+import static org.junit.Assert.fail;
+
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.junit.Test;
+
+import com.sap.furcas.ide.projectwizard.util.ProjectInfo;
+import com.sap.furcas.ide.projectwizard.wizards.FurcasWizard;
+
 public class OverallWizardTest {
+
+    @Test
+    public void wizardTest() {
+        ProjectInfo pi = new ProjectInfo();
+        GeneratedClassesTest.configureProjectInfo(pi);
+        try {
+            runWizard(pi);
+        } catch (Exception e) {
+            fail("Test failed due to: " + e.getMessage());
+        }
+    }
+
+    private void runWizard(ProjectInfo pi) {
+        FurcasWizard wizard = new FurcasWizard();
+        NullProgressMonitor monitor = new NullProgressMonitor();
+        wizard.doFinish(pi, monitor);
+
+    }
 
 }
