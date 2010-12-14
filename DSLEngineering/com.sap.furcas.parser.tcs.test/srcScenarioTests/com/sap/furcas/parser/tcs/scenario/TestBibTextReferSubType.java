05b9baa346615773af2980abb0e11019de7a2c2b
Stephan Erb <stephan@dev.static-void.de>
2010-12-14 19:53:48 +0100

    Reorganize the TCS parser tests.
    Those can now be found under com.sap.furcas.parser.tcs.test.
    Only the tests relevant for the actual parsergeneration remain under
    com.sap.furcas.parsergenerator.test.

---
+++ DSLEngineering/com.sap.furcas.parser.tcs.test/srcScenarioTests/com/sap/furcas/parser/tcs/scenario/TestBibTextReferSubType.java
@@ -0,0 +1,55 @@
+package com.sap.furcas.parser.tcs.scenario;
+
+import java.io.File;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.sap.furcas.parsergenerator.TCSSyntaxContainerBean;
+import com.sap.furcas.runtime.parser.ParserFacade;
+import com.sap.furcas.runtime.parser.testbase.GeneratedParserBasedTest;
+import com.sap.furcas.runtime.parser.testbase.GeneratedParserTestConfiguration;
+import com.sap.furcas.runtime.parser.testbase.ParsingHelper;
+import com.sap.furcas.test.fixture.ScenarioFixtureData;
+
+
+
+/**
+ * Simple Test for the custom BibText language
+ */
+public class TestBibTextReferSubType extends GeneratedParserBasedTest {
+
+
+    private static final String LANGUAGE = "BibtextReferSubType";
+    private static final File TCS = ScenarioFixtureData.BIBTEXT_REFER_SUBTYPE_TCS;
+    private static final File[] METAMODELS = { ScenarioFixtureData.BIBTEXT_REFER_SUBTYPE_METAMODEL,  ScenarioFixtureData.BIBTEXT1_METAMODEL };
+    
+    private static ParsingHelper parsingHelper;
+
+    @BeforeClass
+    public static void setupParser() throws Exception {
+        GeneratedParserTestConfiguration testConfig = new GeneratedParserTestConfiguration(LANGUAGE, TCS, METAMODELS);
+        TCSSyntaxContainerBean syntaxBean = parseSyntax(testConfig);
+        ParserFacade facade = generateParserForLanguage(syntaxBean, testConfig, new ClassLookupImpl());
+        parsingHelper = new ParsingHelper(facade);
+    }
+	
+    /**
+     * tests metamodel upper and lower bounds are used and cause errors when violated
+     * @throws Exception
+     */
+    @Test
+    public void testSample() throws Exception {
+        String sample = "";
+  
+        sample = "article{" + 
+        "  Testing, \"John Doe\" : " + 
+        "  year=\"2002\"" + 
+        "}" + 
+        "author = \"John Doe\"." 
+        + "author = \"Jane Doll\"."
+        ;
+
+        parsingHelper.parseString(sample, 0);
+    }
+}
