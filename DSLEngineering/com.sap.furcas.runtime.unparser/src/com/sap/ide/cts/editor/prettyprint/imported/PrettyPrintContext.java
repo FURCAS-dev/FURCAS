cf5e6ce10b4d232625e7a89d79369fadcfbad770
Stephan Erb <stephan@dev.static-void.de>
2011-01-02 00:58:51 +0100

    Add a copyright notice to the pretty printer classes.

--- DSLEngineering/com.sap.furcas.runtime.unparser/src/com/sap/ide/cts/editor/prettyprint/imported/PrettyPrintContext.java
+++ DSLEngineering/com.sap.furcas.runtime.unparser/src/com/sap/ide/cts/editor/prettyprint/imported/PrettyPrintContext.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2011 INRIA and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     INRIA - initial API and implementation
+ *     SAP AG - API and implementation enhancements
+ ******************************************************************************/
 package com.sap.ide.cts.editor.prettyprint.imported;
 
 import static com.sap.ide.cts.editor.prettyprint.imported.PrettyPrintConstants.INDENTATION_PRIMITIVE;
