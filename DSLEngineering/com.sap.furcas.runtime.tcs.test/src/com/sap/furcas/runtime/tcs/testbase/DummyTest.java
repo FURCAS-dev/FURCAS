a627e77f6951aa71b2a04c0952d8a4f19ab785fc
Stephan Erb <stephan@dev.static-void.de>
2011-01-09 17:41:28 +0100

    Add empty testcase so that maven does not fail.

---
+++ DSLEngineering/com.sap.furcas.runtime.tcs.test/src/com/sap/furcas/runtime/tcs/testbase/DummyTest.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ ******************************************************************************/
+package com.sap.furcas.runtime.tcs.testbase;
+
+import org.junit.Test;
+
+/**
+ * So that maven can find a test and doesn't fail
+ */
+public class DummyTest {
+
+    @Test
+    public void testNothing() {
+        
+    }
+}
