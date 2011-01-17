daa65617175bf00efaa4d95a00429b03470e9506
sasarkar <sasarkar>
2011-01-17 06:10:49 +0000

    Moving Public Indexing APIs to top level package https://bugs.eclipse.org/bugs/show_bug.cgi?id=333844

---
+++ org.eclipse.emf.query2/plugins/org.eclipse.emf.query2.index/src/org/eclipse/emf/query/index/Activator.java
@@ -0,0 +1,23 @@
+package org.eclipse.emf.query.index;
+
+
+import org.eclipse.core.runtime.Plugin;
+
+
+
+public class Activator extends Plugin {
+	// The shared instance
+	private static Activator plugin;
+
+
+	
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
