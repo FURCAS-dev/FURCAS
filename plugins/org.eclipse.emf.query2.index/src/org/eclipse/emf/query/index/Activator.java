89b13bb16e7f3cada232870743e96d4b49ec0cd0
sasarkar <sasarkar>
2011-01-18 10:13:37 +0000

    Fixing the build issue

--- plugins/org.eclipse.emf.query2.index/src/org/eclipse/emf/query/index/Activator.java
+++ plugins/org.eclipse.emf.query2.index/src/org/eclipse/emf/query/index/Activator.java
@@ -4,13 +4,53 @@ package org.eclipse.emf.query.index;
 import org.eclipse.core.runtime.Plugin;
 
 
+import org.osgi.framework.BundleContext;
+
+
 
 public class Activator extends Plugin {
+	// The plug-in ID
+	
+
 	// The shared instance
 	private static Activator plugin;
 
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
 
-	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		// Dumping of Indexes at the close of IDE, so that when the IDE is
+		// started for the next time, indices must not be build again and
+		// instead loaded from saved ones.
+		IndexFactory.dumpIndexes();
+		super.stop(context);
+	}
+
 	/**
 	 * Returns the shared instance
 	 * 
@@ -20,4 +60,5 @@ public class Activator extends Plugin {
 		return plugin;
 	}
 
+	
 }
