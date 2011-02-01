84c51e09d2736898cc9938af8afd884ff3be375e
Axel Uhl <axel.uhl@sap.com>
2011-02-01 11:29:51 +0100

    made build in Eclipse by fixing .classpath files; removed a few warnings

--- DSLEngineering/com.sap.furcas.metamodel.edit/src/com/sap/furcas/metamodel/FURCAS/TCS/provider/ClassTemplateItemProvider.java
+++ DSLEngineering/com.sap.furcas.metamodel.edit/src/com/sap/furcas/metamodel/FURCAS/TCS/provider/ClassTemplateItemProvider.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 import org.eclipse.emf.common.notify.AdapterFactory;
 import org.eclipse.emf.common.notify.Notification;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.ComposeableAdapterFactory;
 import org.eclipse.emf.edit.provider.IEditingDomainItemProvider;
 import org.eclipse.emf.edit.provider.IItemLabelProvider;
@@ -24,7 +23,6 @@ import org.eclipse.emf.edit.provider.ItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ViewerNotification;
 
 import com.sap.furcas.metamodel.FURCAS.TCS.ClassTemplate;
-import com.sap.furcas.metamodel.FURCAS.TCS.TCSFactory;
 import com.sap.furcas.metamodel.FURCAS.TCS.TCSPackage;
 
 /**
