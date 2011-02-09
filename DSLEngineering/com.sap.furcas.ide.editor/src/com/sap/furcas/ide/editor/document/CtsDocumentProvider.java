225c8f73ad3d850c41749b8dbb1df0418d1a5638
Stephan Erb <stephan@dev.static-void.de>
2011-02-09 23:45:47 +0100

    Always create a textual backup of textblocks.
    We are to unstable to work otherwise...

--- DSLEngineering/com.sap.furcas.ide.editor/src/com/sap/furcas/ide/editor/document/CtsDocumentProvider.java
+++ DSLEngineering/com.sap.furcas.ide.editor/src/com/sap/furcas/ide/editor/document/CtsDocumentProvider.java
@@ -1,6 +1,8 @@
 package com.sap.furcas.ide.editor.document;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +10,9 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.common.command.BasicCommandStack;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.PlatformResourceURIHandlerImpl.WorkbenchHelper;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.jface.operation.IRunnableContext;
 import org.eclipse.jface.text.IDocument;
@@ -21,6 +25,7 @@ import com.sap.furcas.ide.editor.commands.CleanUpTextBlocksCommand;
 import com.sap.furcas.metamodel.FURCAS.TCS.ConcreteSyntax;
 import com.sap.furcas.metamodel.FURCAS.textblocks.TextBlock;
 import com.sap.furcas.metamodel.FURCAS.textblocks.Version;
+import com.sap.furcas.runtime.textblocks.TbUtil;
 import com.sap.furcas.runtime.textblocks.modifcation.TbChangeUtil;
 import com.sap.furcas.runtime.textblocks.modifcation.TbVersionUtil;
 import com.sap.ide.cts.parser.incremental.PartitionAssignmentHandler;
@@ -44,7 +49,8 @@ public class CtsDocumentProvider extends AbstractDocumentProvider {
         if (info != null) {
             CtsDocument ctsDocument = ((CtsDocument) info.fDocument);
             if (ctsDocument.isCompletelyItitialized()) {
-                return ctsDocument.getRootBlock().eResource().isModified();
+                return ctsDocument.getRootBlock().eResource().isModified() ||
+                    ctsDocument.getRootObject().eResource().isModified();
             }
         }
         return false;
@@ -63,37 +69,55 @@ public class CtsDocumentProvider extends AbstractDocumentProvider {
 
     @Override
     public void doSaveDocument(IProgressMonitor monitor, Object element, IDocument document, boolean overwrite) throws CoreException {
+        monitor.beginTask("Saving connection.", 100);
         try {
             CtsDocument ctsDocument = (CtsDocument) document;
+            createBackup(ctsDocument);
+            
             if (TbVersionUtil.getOtherVersion(ctsDocument.getRootBlock(), Version.CURRENT) != null) {
                 // only clean up if a current version exists, as this is
                 // only the case if the rootBlock was at least lexable
-                monitor.beginTask("Cleaning up textblocks.", 100);
-                CleanUpTextBlocksCommand cleanUpCommand = new CleanUpTextBlocksCommand(ctsDocument.getRootBlock());
-                editingDomain.getCommandStack().execute(cleanUpCommand);
-                TextBlock newRoot = cleanUpCommand.getNewRootBlock();
-                ctsDocument.setRootBlock(newRoot);
-                monitor.worked(50);
+                cleanUpDocument(ctsDocument);
+                monitor.worked(20);
                 ctsDocument.reduceToMinimalState();
-                monitor.worked(50);
+                monitor.worked(40);
             }
-            monitor.beginTask("Saving connection.", 100);
 
             //inform about the upcoming content change
             fireElementStateChanging(element);
 
             // Save only resources that have actually changed.
-            //
-            final Map<Object, Object> saveOptions = new HashMap<Object, Object>();
-            saveOptions.put(Resource.OPTION_SAVE_ONLY_IF_CHANGED, Resource.OPTION_SAVE_ONLY_IF_CHANGED_MEMORY_BUFFER);
-            partitionHandler.saveAllPartitions(saveOptions);
+            saveResourcesIfDirty();
             ((BasicCommandStack) editingDomain.getCommandStack()).saveIsDone();
 
-            fireElementDirtyStateChanged(element, false);
-            monitor.done();
+            fireElementDirtyStateChanged(element, /*isDirty*/ false);
         } catch (IOException e) {
             throw new CoreException(new Status(Status.ERROR, CtsActivator.PLUGIN_ID, e.getMessage(), e));
+        } finally {
+            monitor.done();
         }
+    }
+
+    private void createBackup(CtsDocument ctsDocument) throws IOException {
+        TextBlock rootBlock = (TextBlock) TbUtil.getNewestVersion(ctsDocument.getRootBlock());
+        URI targetURI = rootBlock.eResource().getURI().appendFileExtension("txt");
+        OutputStream stream = WorkbenchHelper.createPlatformResourceOutputStream(targetURI.toPlatformString(true), /*options*/ null);
+        PrintStream backup = new PrintStream(stream);
+        backup.print(rootBlock.getCachedString());
+        backup.close();
+    }
+
+    private void saveResourcesIfDirty() throws IOException {
+        final Map<Object, Object> saveOptions = new HashMap<Object, Object>();
+        saveOptions.put(Resource.OPTION_SAVE_ONLY_IF_CHANGED, Resource.OPTION_SAVE_ONLY_IF_CHANGED_MEMORY_BUFFER);
+        partitionHandler.saveAllPartitions(saveOptions);
+    }
+
+    private void cleanUpDocument(CtsDocument ctsDocument) {
+        CleanUpTextBlocksCommand cleanUpCommand = new CleanUpTextBlocksCommand(ctsDocument.getRootBlock());
+        editingDomain.getCommandStack().execute(cleanUpCommand);
+        TextBlock newRoot = cleanUpCommand.getNewRootBlock();
+        ctsDocument.setRootBlock(newRoot);
     }
 
     @Override
