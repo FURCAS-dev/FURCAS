8f22a5e246fa7a95fc50877dc34eae05de33599e
Thomas Goldschmidt <thomas.goldschmidt@de.abb.com>
2011-02-22 14:29:49 +0100

    added workaround for bug #98 and nsURI package lookup
    -added possibility to use the nsURI to refer to metaReferences for templates
    -workaround for bug #98: added manual two level package nesting so that something like this is possible:
    
    template root::nested::MyClass : .. ;
    -added testcases for nestedPackageLookup

--- DSLEngineering/com.sap.furcas.parser.tcs.test/srcFeatureTests/com/sap/furcas/parser/tcs/template/TestNestedPackageLookUp.java
+++ DSLEngineering/com.sap.furcas.parser.tcs.test/srcFeatureTests/com/sap/furcas/parser/tcs/template/TestNestedPackageLookUp.java
@@ -23,9 +23,14 @@ public class TestNestedPackageLookUp extends GeneratedParserBasedTest {
 
     private static final String LANGUAGE_NESTEDPACKAGE = "NestedPackageLookUp";
     private static final File nestedPackageLookupTCS = FeatureFixtureData.NESTED_PACKAGE_TCS;
+    
+    private static final String LANGUAGE_NESTEDPACKAGEBYURI = "NestedPackageLookUpByURI";
+    private static final File nestedPackageLookupByURITCS = FeatureFixtureData.NESTED_PACKAGE_BY_URI_TCS;
+    
     private static final File METAMODEL = FeatureFixtureData.NESTED_PACKAGE_METAMODEL;
 
     private static StubParsingHelper nestedPackageLookupHelper;
+    private static StubParsingHelper nestedPackageLookupByUriHelper;
     
 
     @BeforeClass
@@ -34,12 +39,23 @@ public class TestNestedPackageLookUp extends GeneratedParserBasedTest {
         TCSSyntaxContainerBean nestedPackageLookupSyntaxBean = parseSyntax(nestedPackageLookupTestConfig);
         ParserFacade nestedPackageLookupFacade = generateParserForLanguage(nestedPackageLookupSyntaxBean, nestedPackageLookupTestConfig, new ClassLookupImpl());
         nestedPackageLookupHelper = new StubParsingHelper(nestedPackageLookupFacade);             
+        
+        GeneratedParserTestConfiguration nestedPackageLookupByUriTestConfig = new GeneratedParserTestConfiguration(LANGUAGE_NESTEDPACKAGEBYURI, nestedPackageLookupByURITCS, METAMODEL);
+        TCSSyntaxContainerBean nestedPackageLookupByUriSyntaxBean = parseSyntax(nestedPackageLookupByUriTestConfig);
+        ParserFacade nestedPackageLookupByUriFacade = generateParserForLanguage(nestedPackageLookupByUriSyntaxBean, nestedPackageLookupByUriTestConfig, new ClassLookupImpl());
+        nestedPackageLookupByUriHelper = new StubParsingHelper(nestedPackageLookupByUriFacade);
     }
     
   
     @Test
-    public void testMappedEnum() throws Exception {
+    public void testNestedPackageLookupParser() throws Exception {
         nestedPackageLookupHelper.parseString("rootTest nestedTest");
+        
+    }
+    
+    @Test
+    public void testNestedPackageLookupByUriParser() throws Exception {
+        nestedPackageLookupByUriHelper.parseString("rootTest nestedTest");
         
     }
     
