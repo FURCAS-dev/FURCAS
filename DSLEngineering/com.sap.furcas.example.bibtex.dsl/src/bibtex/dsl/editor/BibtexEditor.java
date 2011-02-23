42e45f11e4a01dab9e8ef851e2f1be680d73ff8d
Stephan Erb <stephan@dev.static-void.de>
2011-02-24 00:32:35 +0100

    Rename bibtex projects to com.sap.furcas.example.*

---
+++ DSLEngineering/com.sap.furcas.example.bibtex.dsl/src/bibtex/dsl/editor/BibtexEditor.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SAP AG and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     SAP AG - initial API and implementation
+ ******************************************************************************/
+package bibtex.dsl.editor; 
+
+
+import bibtex.dsl.parser.BibtexParserFactory;
+
+import com.sap.furcas.ide.editor.imp.AbstractFurcasEditor;
+
+public class BibtexEditor extends AbstractFurcasEditor { 
+
+    public BibtexEditor() {
+        super(new BibtexParserFactory());
+    }
+
+}
