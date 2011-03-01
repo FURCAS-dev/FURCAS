0762fe78f165edac795a4361669bb28857322fbc
Martin Hanysz <martin.hanysz@sap.com>
2011-03-02 00:38:31 +0100

    Implemented missing testcases for the DerivedPropertyAdapter. Changed the company testmodel to contain some unsettable features to use in the tests and regenerated model source.

--- EMF/mdt/tests/org.eclipse.ocl.examples.impactanalyzer.testmodel.company/src/company/Company.java
+++ EMF/mdt/tests/org.eclipse.ocl.examples.impactanalyzer.testmodel.company/src/company/Company.java
@@ -18,6 +18,7 @@ import org.eclipse.emf.ecore.EObject;
  * <ul>
  *   <li>{@link company.Company#getDivision <em>Division</em>}</li>
  *   <li>{@link company.Company#getEotmDelta <em>Eotm Delta</em>}</li>
+ *   <li>{@link company.Company#getDivisionDirector <em>Division Director</em>}</li>
  * </ul>
  * </p>
  *
@@ -28,47 +29,74 @@ import org.eclipse.emf.ecore.EObject;
  */
 public interface Company extends EObject {
     /**
-     * Returns the value of the '<em><b>Division</b></em>' reference.
-     * It is bidirectional and its opposite is '{@link company.Division#getCompany <em>Company</em>}'.
-     * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Division</b></em>' reference.
+	 * It is bidirectional and its opposite is '{@link company.Division#getCompany <em>Company</em>}'.
+	 * <!-- begin-user-doc -->
      * <p>
      * If the meaning of the '<em>Division</em>' reference isn't clear,
      * there really should be more of a description here...
      * </p>
      * <!-- end-user-doc -->
-     * @return the value of the '<em>Division</em>' reference.
-     * @see #setDivision(Division)
-     * @see company.CompanyPackage#getCompany_Division()
-     * @see company.Division#getCompany
-     * @model opposite="company"
-     * @generated
-     */
+	 * @return the value of the '<em>Division</em>' reference.
+	 * @see #setDivision(Division)
+	 * @see company.CompanyPackage#getCompany_Division()
+	 * @see company.Division#getCompany
+	 * @model opposite="company"
+	 * @generated
+	 */
     Division getDivision();
 
     /**
-     * Sets the value of the '{@link company.Company#getDivision <em>Division</em>}' reference.
-     * <!-- begin-user-doc -->
+	 * Sets the value of the '{@link company.Company#getDivision <em>Division</em>}' reference.
+	 * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-     * @param value the new value of the '<em>Division</em>' reference.
-     * @see #getDivision()
-     * @generated
-     */
+	 * @param value the new value of the '<em>Division</em>' reference.
+	 * @see #getDivision()
+	 * @generated
+	 */
     void setDivision(Division value);
 
     /**
-     * Returns the value of the '<em><b>Eotm Delta</b></em>' attribute.
-     * <!-- begin-user-doc -->
+	 * Returns the value of the '<em><b>Eotm Delta</b></em>' attribute.
+	 * <!-- begin-user-doc -->
      * <p>
      * If the meaning of the '<em>Eotm Delta</em>' attribute isn't clear,
      * there really should be more of a description here...
      * </p>
      * <!-- end-user-doc -->
-     * @return the value of the '<em>Eotm Delta</em>' attribute.
-     * @see company.CompanyPackage#getCompany_EotmDelta()
-     * @model dataType="primitivetypes.Integer" transient="true" changeable="false" volatile="true" derived="true"
-     *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL derivation='let maxEOTMDivision:Division=self.division->sortedBy(i | i.numberEmployeesOfTheMonth)->last() in let minEOTMDivision:Division=self.division->sortedBy(i | i.numberEmployeesOfTheMonth)->first() in maxEOTMDivision.numberEmployeesOfTheMonth - minEOTMDivision.numberEmployeesOfTheMonth'"
-     * @generated
-     */
+	 * @return the value of the '<em>Eotm Delta</em>' attribute.
+	 * @see company.CompanyPackage#getCompany_EotmDelta()
+	 * @model dataType="primitivetypes.Integer" transient="true" changeable="false" volatile="true" derived="true"
+	 *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL derivation='let maxEOTMDivision:Division=self.division->sortedBy(i | i.numberEmployeesOfTheMonth)->last() in let minEOTMDivision:Division=self.division->sortedBy(i | i.numberEmployeesOfTheMonth)->first() in maxEOTMDivision.numberEmployeesOfTheMonth - minEOTMDivision.numberEmployeesOfTheMonth'"
+	 * @generated
+	 */
     int getEotmDelta();
+
+				/**
+	 * Returns the value of the '<em><b>Division Director</b></em>' reference.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Division Director</em>' reference isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Division Director</em>' reference.
+	 * @see #isSetDivisionDirector()
+	 * @see company.CompanyPackage#getCompany_DivisionDirector()
+	 * @model unsettable="true" transient="true" changeable="false" volatile="true" derived="true"
+	 *        annotation="http://www.eclipse.org/emf/2002/Ecore/OCL derivation='self.division.director'"
+	 * @generated
+	 */
+	Employee getDivisionDirector();
+
+				/**
+	 * Returns whether the value of the '{@link company.Company#getDivisionDirector <em>Division Director</em>}' reference is set.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return whether the value of the '<em>Division Director</em>' reference is set.
+	 * @see #getDivisionDirector()
+	 * @generated
+	 */
+	boolean isSetDivisionDirector();
 
 } // Company
