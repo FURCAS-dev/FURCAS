8cf985bfe1670bff6231d4d80e92423b3d985e4b
Thomas Goldschmidt <thomas.goldschmidt@de.abb.com>
2011-03-04 08:55:47 +0100

    fixed construction of referenceScope

--- DSLEngineering/com.sap.furcas.test/src/com/sap/furcas/test/testutils/ResourceTestHelper.java
+++ DSLEngineering/com.sap.furcas.test/src/com/sap/furcas/test/testutils/ResourceTestHelper.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -15,7 +16,7 @@ public class ResourceTestHelper {
     
     public static Set<URI> createEcoreReferenceScope() {
         Set<URI> referenceScope = new HashSet<URI>();
-        referenceScope.add(URI.createURI(FURCASPackage.eINSTANCE.eClass().getEPackage().getNsURI()));
+        referenceScope.add(URI.createURI(EcorePackage.eINSTANCE.getNsURI()));
         return referenceScope;
     }
 
@@ -27,7 +28,6 @@ public class ResourceTestHelper {
     public static Set<URI> createFURCASReferenceScope() {
         Set<URI> referenceScope = new HashSet<URI>();
         referenceScope.add(URI.createURI(FURCASPackage.eINSTANCE.getNsURI()));
-        referenceScope.add(URI.createURI(FURCASPackage.eINSTANCE.eClass().getEPackage().getNsURI()));
         return referenceScope;
     }
     
