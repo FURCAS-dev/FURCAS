845fc7973d2699bef1c5eea6ee065a824ea8816e
auhl <auhl>
2011-03-05 21:48:54 +0000

    added Class.javajet template that generates a @SuppressWarning("unchecked") if unsafe generics cast is required in delegating operations

--- tests/org.eclipse.ocl.examples.impactanalyzer.testmodel.ngpm/src/behavioral/transactions/impl/TransactionsFactoryImpl.java
+++ tests/org.eclipse.ocl.examples.impactanalyzer.testmodel.ngpm/src/behavioral/transactions/impl/TransactionsFactoryImpl.java
@@ -2,7 +2,7 @@
  * <copyright>
  * </copyright>
  *
- * $Id: TransactionsFactoryImpl.java,v 1.1 2011/02/07 17:17:58 auhl Exp $
+ * $Id: TransactionsFactoryImpl.java,v 1.2 2011/03/05 21:48:55 auhl Exp $
  */
 package behavioral.transactions.impl;
 
@@ -24,76 +24,76 @@ import org.eclipse.emf.ecore.plugin.EcorePlugin;
  */
 public class TransactionsFactoryImpl extends EFactoryImpl implements TransactionsFactory {
 	/**
-     * Creates the default factory implementation.
-     * <!-- begin-user-doc -->
+	 * Creates the default factory implementation.
+	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
 	public static TransactionsFactory init() {
-        try {
-            TransactionsFactory theTransactionsFactory = (TransactionsFactory)EPackage.Registry.INSTANCE.getEFactory("http://eclipse.org/ocl/examples/impactanalyzer/testmodel/ngpm/behavioral/transactions.ecore"); 
-            if (theTransactionsFactory != null) {
-                return theTransactionsFactory;
-            }
-        }
-        catch (Exception exception) {
-            EcorePlugin.INSTANCE.log(exception);
-        }
-        return new TransactionsFactoryImpl();
-    }
+		try {
+			TransactionsFactory theTransactionsFactory = (TransactionsFactory)EPackage.Registry.INSTANCE.getEFactory("http://eclipse.org/ocl/examples/impactanalyzer/testmodel/ngpm/behavioral/transactions.ecore"); 
+			if (theTransactionsFactory != null) {
+				return theTransactionsFactory;
+			}
+		}
+		catch (Exception exception) {
+			EcorePlugin.INSTANCE.log(exception);
+		}
+		return new TransactionsFactoryImpl();
+	}
 
 	/**
-     * Creates an instance of the factory.
-     * <!-- begin-user-doc -->
+	 * Creates an instance of the factory.
+	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
 	public TransactionsFactoryImpl() {
-        super();
-    }
+		super();
+	}
 
 	/**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
 	@Override
 	public EObject create(EClass eClass) {
-        switch (eClass.getClassifierID()) {
-            case TransactionsPackage.DUMMY: return createDummy();
-            default:
-                throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
-        }
-    }
+		switch (eClass.getClassifierID()) {
+			case TransactionsPackage.DUMMY: return createDummy();
+			default:
+				throw new IllegalArgumentException("The class '" + eClass.getName() + "' is not a valid classifier");
+		}
+	}
 
 	/**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
 	public Dummy createDummy() {
-        DummyImpl dummy = new DummyImpl();
-        return dummy;
-    }
+		DummyImpl dummy = new DummyImpl();
+		return dummy;
+	}
 
 	/**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-     * @generated
-     */
+	 * @generated
+	 */
 	public TransactionsPackage getTransactionsPackage() {
-        return (TransactionsPackage)getEPackage();
-    }
+		return (TransactionsPackage)getEPackage();
+	}
 
 	/**
-     * <!-- begin-user-doc -->
+	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-     * @deprecated
-     * @generated
-     */
+	 * @deprecated
+	 * @generated
+	 */
 	@Deprecated
 	public static TransactionsPackage getPackage() {
-        return TransactionsPackage.eINSTANCE;
-    }
+		return TransactionsPackage.eINSTANCE;
+	}
 
 } //TransactionsFactoryImpl
