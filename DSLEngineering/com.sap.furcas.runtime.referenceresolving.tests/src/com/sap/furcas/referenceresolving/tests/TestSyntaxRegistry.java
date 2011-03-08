15c0b083c206be8ad22250b865e3242a4a4e885f
Axel Uhl <axel.uhl@sap.com>
2011-03-08 10:17:53 +0100

    added a simple test for resource re-containment

--- DSLEngineering/com.sap.furcas.runtime.referenceresolving.tests/src/com/sap/furcas/referenceresolving/tests/TestSyntaxRegistry.java
+++ DSLEngineering/com.sap.furcas.runtime.referenceresolving.tests/src/com/sap/furcas/referenceresolving/tests/TestSyntaxRegistry.java
@@ -1,11 +1,15 @@
 package com.sap.furcas.referenceresolving.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.ocl.ParserException;
 import org.junit.Test;
 
@@ -15,6 +19,17 @@ import com.sap.furcas.metamodel.FURCAS.TCS.ConcreteSyntax;
 import com.sap.furcas.runtime.syntaxprovider.SyntaxProvider;
 
 public class TestSyntaxRegistry {
+    @Test
+    public void simpleEMFResourceContainmentTest() {
+        ResourceSet rs1 = new ResourceSetImpl();
+        ResourceSet rs2 = new ResourceSetImpl();
+        Resource r = rs1.createResource(URI.createURI("http://something/r1"));
+        assertEquals(rs1, r.getResourceSet());
+        rs2.getResources().add(r);
+        assertEquals(rs2, r.getResourceSet());
+        assertFalse(rs1.getResources().contains(r)); // assuming that assigning resource to rs2 removed it from rs1 implicitly
+    }
+    
     @Test
     public void testGetSyntax() throws IOException, ParserException {
         SyntaxProvider bibtexProvider = Activator.getDefault();
