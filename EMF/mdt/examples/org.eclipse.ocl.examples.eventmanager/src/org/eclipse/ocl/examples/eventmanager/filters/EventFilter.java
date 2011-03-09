e7a84897d4f3a26979e5d7426d0b841dd9818d3b
philipp.berger <philipp.berger@student.hpi.uni-potsdam.de>
2011-03-10 00:26:02 +0100

    Added comment to the org.eclipse.ocl.examples.eventmanager.filters and made the constructor of AttributeFilter.java and AssociationFilter.java more specific

--- EMF/mdt/examples/org.eclipse.ocl.examples.eventmanager/src/org/eclipse/ocl/examples/eventmanager/filters/EventFilter.java
+++ EMF/mdt/examples/org.eclipse.ocl.examples.eventmanager/src/org/eclipse/ocl/examples/eventmanager/filters/EventFilter.java
@@ -11,29 +11,64 @@
 package org.eclipse.ocl.examples.eventmanager.filters;
 
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.ocl.examples.eventmanager.EventManager;
 
+/**
+ * An EventFilter matches a given specification for the properties 
+ * of a {@link Notification} against an incoming {@link Notification}.
+ * It is used to pass a {@link Notification} specification to an {@link EventManager}
+ * @author Philipp Berger
+ *
+ */
 public abstract class EventFilter {
 
     private boolean negated = false;
 
+    /**
+     * default constructor
+     */
     public EventFilter() {
         super();
     }
 
+    /**
+     * Tests if the incoming {@link Notification} matches the intern specification
+     * @param event the {@link Notification} to control
+     * @return whether the given {@link Notification} matches the intern specification
+     */
     abstract public boolean matchesFor(Notification event);
 
     public abstract int hashCode();
 
     public abstract boolean equals(Object obj);
 
+    /**
+     * Returns the unique property for an {@link EventFilter} object
+     * @return
+     */
     public abstract Object getFilterCriterion();
 
+    /**
+     * Returns whether the filter is negated
+     * if the filter is negated it will match every {@link Notification} 
+     * which is not matching the intern specification
+     * @return true if the filter is negated
+     */
     public boolean isNegated() {
         return negated;
 
     }
+    /* (non-Javadoc)
+     * @see java.lang.Object#clone()
+     */
     public abstract EventFilter clone() ;
 
+    /**
+     * Sets whether this filter is negated
+     * if the filter is negated it will match every {@link Notification} 
+     * which is not matching the intern specification
+     * @param b true for negating the filter, false for not
+     */
     public void setNegated(boolean b) {
         negated = b;
         
