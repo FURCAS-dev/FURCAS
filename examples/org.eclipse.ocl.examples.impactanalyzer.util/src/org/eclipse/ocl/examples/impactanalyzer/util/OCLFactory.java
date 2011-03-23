1069aa03be901aa3a244b68e08329fc114da1a4b
auhl <auhl>
2011-03-23 04:17:36 +0000

    Refactored OCLFactory/OCLFactoryImpl into a single class OCLFactory; offer DerivedPropertyNotifier creation in ImpactAnalyzerFactory API. The DerivedPropertyNotifier allows clients to have the EventManager/ImpactAnalyzer combo generate Notifications for OCL-specified derived properties.

--- examples/org.eclipse.ocl.examples.impactanalyzer.util/src/org/eclipse/ocl/examples/impactanalyzer/util/OCLFactory.java
+++ examples/org.eclipse.ocl.examples.impactanalyzer.util/src/org/eclipse/ocl/examples/impactanalyzer/util/OCLFactory.java
@@ -10,40 +10,65 @@
  ******************************************************************************/
 package org.eclipse.ocl.examples.impactanalyzer.util;
 
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EPackage.Registry;
 import org.eclipse.ocl.ecore.EcoreEnvironmentFactory;
 import org.eclipse.ocl.ecore.OCL;
 import org.eclipse.ocl.ecore.opposites.OppositeEndFinder;
-import org.eclipse.ocl.examples.impactanalyzer.util.impl.OCLFactoryImpl;
 
 /**
  * Required by the Impact Analyzer to create {@link org.eclipse.ocl.ecore.OCL} objects
- * that are properly and consistently configured. This factory interfaces allows for the
+ * that are properly and consistently configured. This factory allows for the
  * customization and extension of the creation process.<p>
  * 
- * A default instance is exposed by this interface: {@link #INSTANCE}.
+ * A default instance is exposed by {@link #getInstance()}.
  * 
  * @author Axel Uhl
  *
  */
-public interface OCLFactory {
+public class OCLFactory {
 	/**
 	 * Default instance which produces {@link org.eclipse.ocl.examples.impactanalyzer.util.OCL} instances
 	 * which use an extent map for <code>allInstances</code> evaluation that is consistent with the
 	 * {@link OppositeEndFinder} being used.
 	 */
-    static final OCLFactory INSTANCE = new OCLFactoryImpl();
+    private static final OCLFactory INSTANCE = new OCLFactory(EPackage.Registry.INSTANCE);
     
-    /**
+    private final Registry registry;
+    
+    public static OCLFactory getInstance() {
+		return INSTANCE;
+	}
+    
+    public static OCLFactory getInstance(Registry registry) {
+    	return new OCLFactory(registry);
+    }
+    
+    protected OCLFactory(Registry registry) {
+    	this.registry = registry;
+    }
+
+	/**
      * Creates an OCL instance that uses the environment factory provided. See also
      * {@link OCL#newInstance(org.eclipse.ocl.EnvironmentFactory)}.
      */
-    OCL createOCL(EcoreEnvironmentFactory environmentFactory);
-    
+    public OCL createOCL(EcoreEnvironmentFactory environmentFactory) {
+    	if (!(environmentFactory instanceof EcoreEnvironmentFactoryWithScopedExtentMap)) {
+    		throw new IllegalArgumentException("Inconsistent environment factory type. "+
+    				"Expected EcoreEnvironmentFactoryWithScopedExtentMap but found "+
+    				environmentFactory.getClass().getName());
+    	}
+        return OCL.newInstance((EcoreEnvironmentFactoryWithScopedExtentMap) environmentFactory);
+    }
+
     /**
      * Create an OCL instance that uses the {@link OppositeEndFinder} specified as parameter.
      * Implementations can achieve this, e.g., by using a {@link EcoreEnvironmentFactory factory}
      * which is initialized such that it returns the {@link OppositeEndFinder} specified here
      * from its {@link EcoreEnvironmentFactory#getOppositeEndFinder()} method.
      */
-    OCL createOCL(OppositeEndFinder oppositeEndFinder);
+    public OCL createOCL(OppositeEndFinder oppositeEndFinder) {
+        return org.eclipse.ocl.examples.impactanalyzer.util.OCL.newInstance(registry, oppositeEndFinder);
+    }
+    
 }
