e843af339c7bfeee942c66ca798692b3e6c8fcf7
akrsharma <akrsharma>
2011-04-08 08:20:53 +0000

    Changes in pom.xml - to run SWTBot UI tests

---
+++ tests/org.eclipse.emf.query2.index.ui.test/src/org/eclipse/emf/query2/index/ui/test/IndexUITestSuite.java
@@ -0,0 +1,13 @@
+package org.eclipse.emf.query2.index.ui.test;
+
+import org.eclipse.emf.query2.librarytest.IndexQueryCountTest;
+import org.eclipse.emf.query2.librarytest.LibraryTransformation;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+	@RunWith(Suite.class)
+	@Suite.SuiteClasses( { IndexViewUITest.class, DumpingAndLoadingOfIndicesTest.class })
+	public class IndexUITestSuite  {
+	
+	
+}
