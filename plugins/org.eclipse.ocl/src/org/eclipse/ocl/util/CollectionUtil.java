a20a145035a95cc0134fefbe68b3bd7aa63e5f87
Axel Uhl <axel.uhl@sap.com>
2011-04-15 17:53:50 +0200

    fixed CollectionUtil.excluding instanceof cascade where LinkedHashSet needs to be checked before Set

--- plugins/org.eclipse.ocl/src/org/eclipse/ocl/util/CollectionUtil.java
+++ plugins/org.eclipse.ocl/src/org/eclipse/ocl/util/CollectionUtil.java
@@ -468,7 +468,9 @@ public class CollectionUtil {
      */
     public static <E> Collection<E> excluding(Collection<E> self, Object object) {
         Collection<E> result = null;
-        if (self instanceof Set<?>) {
+        if (self instanceof LinkedHashSet<?>) {
+        	result = createNewOrderedSet(self);
+        } else if (self instanceof Set<?>) {
             result = createNewSet(self);
         } else if (self instanceof Bag<?>) {
             result = createNewBag(self);
@@ -479,9 +481,9 @@ public class CollectionUtil {
             }
             return resultSeq;
         } else {
-            result = createNewOrderedSet(self);
+        	throw new RuntimeException("Unsupported collection type "+self.getClass().getName()); //$NON-NLS-1$
         }
-
+        
         // non-sequences (bags remove all occurrences internally)
         result.remove(object);
         return result;
