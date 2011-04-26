fdf7139b17ddbb15739f70ee87d155feb18a8693
akrsharma <akrsharma>
2011-04-26 06:30:45 +0000

    Added hashcode and equals in userdata classes

--- plugins/org.eclipse.emf.query2.index.ui/src/org/eclipse/emf/query/index/ui/internal/view/tree/nodes/UserData.java
+++ plugins/org.eclipse.emf.query2.index.ui/src/org/eclipse/emf/query/index/ui/internal/view/tree/nodes/UserData.java
@@ -1,12 +1,12 @@
 package org.eclipse.emf.query.index.ui.internal.view.tree.nodes;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.eclipse.emf.query.index.ui.internal.properties.IndexViewProperty;
 
 public class UserData {
+
+	
 
 	private ArrayList<IndexViewProperty> userData;
 	private Object res;
@@ -23,6 +23,40 @@ public class UserData {
 	public Object getRes() {
 		return res;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((res == null) ? 0 : res.hashCode());
+		result = prime * result
+				+ ((userData == null) ? 0 : userData.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserData other = (UserData) obj;
+		if (res == null) {
+			if (other.res != null)
+				return false;
+		} else if (!res.equals(other.res))
+			return false;
+		if (userData == null) {
+			if (other.userData != null)
+				return false;
+		} else if (!userData.equals(other.userData))
+			return false;
+		return true;
+	}
+	
+
 	
 	
 }
