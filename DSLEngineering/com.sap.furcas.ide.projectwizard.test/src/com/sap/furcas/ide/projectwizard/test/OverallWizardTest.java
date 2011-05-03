c5953c3168b7e73b749a376742f290e6b96d9e39
Stephan Erb <stephan@dev.static-void.de>
2011-05-04 00:44:44 +0200

    Enable two more wizard tests.

--- DSLEngineering/com.sap.furcas.ide.projectwizard.test/src/com/sap/furcas/ide/projectwizard/test/OverallWizardTest.java
+++ DSLEngineering/com.sap.furcas.ide.projectwizard.test/src/com/sap/furcas/ide/projectwizard/test/OverallWizardTest.java
@@ -3,7 +3,6 @@ package com.sap.furcas.ide.projectwizard.test;
 import static org.junit.Assert.fail;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.sap.furcas.ide.projectwizard.util.CreateProject;
@@ -13,27 +12,31 @@ import com.sap.furcas.ide.projectwizard.wizards.FurcasWizard;
 public class OverallWizardTest {
 
 
-    @Ignore
     @Test
     public void wizardTest() {
-        FurcasWizard wizard = null;
         ProjectInfo pi = new ProjectInfo();
         GeneratedClassesTest.configureProjectInfo(pi);
+        
         String capLangName = CreateProject.capitalizeFirstChar(pi.getLanguageName());
         pi.setModelPath("/" + pi.getProjectName() + ".metamodel/model/" + capLangName + ".ecore");
         pi.setFromWorkspace(true);
-        wizard = runWizard(pi);
-        if (wizard.isHadError()) {
-            fail("Wizard has ERRORs. See console output for details.");
-        }
         
-
+        FurcasWizard wizard = null;
+        try {
+            wizard = runWizard(pi);
+            if (wizard.isHadError()) {
+                fail("Wizard has ERRORs. See console output for details.");
+            }
+        } finally {
+            if (wizard != null) {
+                wizard.deleteJunk(pi, new NullProgressMonitor());
+            }
+        }
     }
 
     private FurcasWizard runWizard(ProjectInfo pi) {
         FurcasWizard wizard = new FurcasWizard();
-        NullProgressMonitor monitor = new NullProgressMonitor();
-        wizard.structuredProcess(pi, monitor);
+        wizard.structuredProcess(pi, new NullProgressMonitor());
         return wizard;
 
     }
