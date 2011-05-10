54f9ce6ac925bb18b634e85c485e8db0d472d8ac
Axel Uhl <axel.uhl@sap.com>
2011-05-10 18:04:23 +0200

    removed commented test; added image for documentation; added test for use of local opposite end finder

--- tests/org.eclipse.ocl.ecore.tests/src/org/eclipse/ocl/ecore/tests/DelegatesTest.java
+++ tests/org.eclipse.ocl.ecore.tests/src/org/eclipse/ocl/ecore/tests/DelegatesTest.java
@@ -81,6 +81,7 @@ import org.eclipse.ocl.ecore.delegate.OCLValidationDelegateFactory;
 import org.eclipse.ocl.ecore.delegate.SettingBehavior;
 import org.eclipse.ocl.ecore.delegate.ValidationBehavior;
 import org.eclipse.ocl.ecore.delegate.ValidationDelegate;
+import org.eclipse.ocl.ecore.opposites.DefaultOppositeEndFinder;
 import org.eclipse.ocl.ecore.opposites.EcoreEnvironmentFactoryWithHiddenOpposites;
 import org.eclipse.ocl.internal.l10n.OCLMessages;
 import org.eclipse.osgi.util.NLS;
@@ -655,39 +656,20 @@ public class DelegatesTest extends AbstractTestSuite
 		assertNotNull(getUnrelated);
 		Object o = sup2Obj.eInvoke(getUnrelated, null);
 		assertEquals(unrelatedObj, o);
-	}	
-/*	public void test_hiddenOppositeInOperationDefined() throws InvocationTargetException {
-		URI uri = getTestModelURI("/model/HiddenOpposites.ecore");
-		Resource res = resourceSet.getResource(uri, true);
-		res.eAdapters().add(new ECrossReferenceAdapter());
-		EPackage hiddenOppositesPackage = (EPackage) res.getContents().get(0);
-		EAnnotation anno = hiddenOppositesPackage.getEAnnotation(OCLDelegateDomain.OCL_DELEGATE_URI);
-		anno.getDetails().put("oppositeEndFinderClass", "org.eclipse.ocl.ecore.opposites.DefaultOppositeEndFinder");
-		resourceSet.getPackageRegistry().put(hiddenOppositesPackage.getNsURI(), hiddenOppositesPackage);
-		EFactory hiddenOppositesFactory = hiddenOppositesPackage.getEFactoryInstance();
-		EClass sup2 = (EClass) hiddenOppositesPackage.getEClassifier("Sup2");
-		EClass unrelated = (EClass) hiddenOppositesPackage.getEClassifier("Unrelated");
-		EObject unrelatedObj = hiddenOppositesFactory.create(unrelated);
-		EObject sup2Obj = hiddenOppositesFactory.create(sup2);
-		res.getContents().add(unrelatedObj);
-		res.getContents().add(sup2Obj);
-		unrelatedObj.eSet(unrelated.getEStructuralFeature("forward"), sup2Obj);
-		EOperation getUnrelated = null;
-		for (EOperation eo : sup2.getEOperations()) {
-			if (eo.getName().equals("getUnrelated")) {
-				getUnrelated = eo;
-				break;
-			}
+	}
+	
+	public static class LocalOppositeEndFinder extends DefaultOppositeEndFinder {
+		public static boolean localOppositeEndFinderUsed = false;
+		public LocalOppositeEndFinder(Registry registry) {
+			super(registry);
+			localOppositeEndFinderUsed = true;
 		}
-		assertNotNull(getUnrelated);
-		Object o = sup2Obj.eInvoke(getUnrelated, null);
-		assertEquals(unrelatedObj, o);
-	} */
-
+	}
+	
 	public static class LocalEnvironmentFactory extends EcoreEnvironmentFactoryWithHiddenOpposites {
 		public static boolean localEnvironmentFactoryUsed = false;
 		public LocalEnvironmentFactory(Registry registry) {
-			super(registry);
+			super(registry, new LocalOppositeEndFinder(registry));
 			localEnvironmentFactoryUsed = true;
 		}
 	}
@@ -718,8 +700,10 @@ public class DelegatesTest extends AbstractTestSuite
 		assertNotNull(getUnrelated);
 		Object o = sup2Obj.eInvoke(getUnrelated, null);
 		assertEquals(unrelatedObj, o);
-		assertTrue("The configured local opposite end finder was not used",
+		assertTrue("The configured local environment factory was not used",
 			LocalEnvironmentFactory.localEnvironmentFactoryUsed);
+		assertTrue("The configured local opposite end finder was not used",
+			LocalOppositeEndFinder.localOppositeEndFinderUsed);
 	}
 
 	public void test_invariantCacheBeingUsed() throws ParserException {
