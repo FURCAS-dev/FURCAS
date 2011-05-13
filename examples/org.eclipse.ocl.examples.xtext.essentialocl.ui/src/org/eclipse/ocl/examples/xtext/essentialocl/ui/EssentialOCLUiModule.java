1590d1c3174a1721a7fac646fb2411be4ebddbfe
ewillink <ewillink>
2011-05-13 11:46:08 +0000

    Use ValidatingEditorCallback to get an initial validation

--- examples/org.eclipse.ocl.examples.xtext.essentialocl.ui/src/org/eclipse/ocl/examples/xtext/essentialocl/ui/EssentialOCLUiModule.java
+++ examples/org.eclipse.ocl.examples.xtext.essentialocl.ui/src/org/eclipse/ocl/examples/xtext/essentialocl/ui/EssentialOCLUiModule.java
@@ -12,7 +12,7 @@
  *
  * </copyright>
  *
- * $Id: EssentialOCLUiModule.java,v 1.5 2011/05/05 20:34:55 ewillink Exp $
+ * $Id: EssentialOCLUiModule.java,v 1.6 2011/05/13 11:46:08 ewillink Exp $
  */
 package org.eclipse.ocl.examples.xtext.essentialocl.ui;
 
@@ -20,7 +20,6 @@ import org.eclipse.ocl.examples.xtext.base.pivot2cs.BaseLocationInFileProvider;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.BaseDocument;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.BaseEObjectTextHover;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.BaseTerminalsTokenTypeToPartitionMapper;
-import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.NaturelessEditorCallback;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.BaseAntlrTokenToAttributeIdMapper;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.EssentialOCLHighlightingConfiguration;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.EssentialOCLSemanticHighlightingCalculator;
@@ -32,6 +31,7 @@ import org.eclipse.xtext.ui.editor.model.XtextDocument;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.eclipse.xtext.ui.editor.validation.ValidatingEditorCallback;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -67,7 +67,7 @@ public class EssentialOCLUiModule extends org.eclipse.ocl.examples.xtext.essenti
 
 	@Override
 	public Class<? extends org.eclipse.xtext.ui.editor.IXtextEditorCallback> bindIXtextEditorCallback() {
-		return NaturelessEditorCallback.class;
+		return ValidatingEditorCallback.class;
 	}
 
 	public Class<? extends XtextDocument> bindXtextDocument() {
