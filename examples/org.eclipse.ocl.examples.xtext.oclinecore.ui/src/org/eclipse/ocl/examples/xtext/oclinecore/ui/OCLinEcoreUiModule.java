f2f947871ddccd03697b77d0daa8f91ce00ede87
ewillink <ewillink>
2011-05-15 20:22:14 +0000

    Support Open Declaration and Find References

--- examples/org.eclipse.ocl.examples.xtext.oclinecore.ui/src/org/eclipse/ocl/examples/xtext/oclinecore/ui/OCLinEcoreUiModule.java
+++ examples/org.eclipse.ocl.examples.xtext.oclinecore.ui/src/org/eclipse/ocl/examples/xtext/oclinecore/ui/OCLinEcoreUiModule.java
@@ -12,14 +12,16 @@
  *
  * </copyright>
  *
- * $Id: OCLinEcoreUiModule.java,v 1.10 2011/05/13 11:46:05 ewillink Exp $
+ * $Id: OCLinEcoreUiModule.java,v 1.11 2011/05/15 20:22:15 ewillink Exp $
  */
 package org.eclipse.ocl.examples.xtext.oclinecore.ui;
 
 import org.eclipse.emf.common.EMFPlugin;
 import org.eclipse.ocl.examples.xtext.base.pivot2cs.BaseLocationInFileProvider;
+import org.eclipse.ocl.examples.xtext.essentialocl.ui.findrefs.BaseReferenceFinder;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.BaseEObjectTextHover;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.BaseTerminalsTokenTypeToPartitionMapper;
+import org.eclipse.ocl.examples.xtext.essentialocl.ui.model.BaseURIEditorOpener;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.BaseAntlrTokenToAttributeIdMapper;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.EssentialOCLHighlightingConfiguration;
 import org.eclipse.ocl.examples.xtext.essentialocl.ui.syntaxcoloring.EssentialOCLSemanticHighlightingCalculator;
@@ -28,6 +30,8 @@ import org.eclipse.ocl.examples.xtext.oclinecore.ui.model.OCLinEcoreDocument;
 import org.eclipse.ocl.examples.xtext.oclinecore.ui.model.OCLinEcoreDocumentProvider;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.xtext.resource.ILocationInFileProvider;
+import org.eclipse.xtext.ui.editor.IURIEditorOpener;
+import org.eclipse.xtext.ui.editor.IXtextEditorCallback;
 import org.eclipse.xtext.ui.editor.hover.DispatchingEObjectTextHover;
 import org.eclipse.xtext.ui.editor.model.IResourceForEditorInputFactory;
 import org.eclipse.xtext.ui.editor.model.ITokenTypeToPartitionTypeMapper;
@@ -36,7 +40,6 @@ import org.eclipse.xtext.ui.editor.model.XtextDocumentProvider;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.AbstractAntlrTokenToAttributeIdMapper;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
-import org.eclipse.xtext.ui.editor.validation.ValidatingEditorCallback;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -94,14 +97,23 @@ public class OCLinEcoreUiModule extends org.eclipse.ocl.examples.xtext.oclinecor
 	public Class<? extends IHighlightingConfiguration> bindIHighlightingConfiguration() {
 		return EssentialOCLHighlightingConfiguration.class;
 	}
+	
+	@SuppressWarnings("restriction")
+	public Class<? extends org.eclipse.xtext.ui.editor.findrefs.IReferenceFinder> bindIReferenceFinder() {
+		return BaseReferenceFinder.class;
+	}
 
 	@Override
 	public Class<? extends IResourceForEditorInputFactory> bindIResourceForEditorInputFactory() {
 		return OCLinEcoreResourceForEditorInputFactory.class;
 	}
 
+	public Class<? extends IURIEditorOpener> bindIURIEditorOpener() {
+		return BaseURIEditorOpener.class;
+	}
+
 	@Override
-	public Class<? extends org.eclipse.xtext.ui.editor.IXtextEditorCallback> bindIXtextEditorCallback() {
-		return ValidatingEditorCallback.class;
+	public Class<? extends IXtextEditorCallback> bindIXtextEditorCallback() {
+		return OCLinEcoreEditorCallback.class;
 	}
 }
