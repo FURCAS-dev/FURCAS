029e693afcf7cd64e923c77884eb73231db167f0
Stephan Erb <stephan@dev.static-void.de>
2011-05-18 23:21:28 +0200

    In addition to creating markers, print messages to stdout.

--- DSLEngineering/com.sap.furcas.ide.dslproject/src/com/sap/furcas/ide/dslproject/builder/ResourceMarkingGenerationErrorHandler.java
+++ DSLEngineering/com.sap.furcas.ide.dslproject/src/com/sap/furcas/ide/dslproject/builder/ResourceMarkingGenerationErrorHandler.java
@@ -20,31 +20,37 @@ public class ResourceMarkingGenerationErrorHandler implements GenerationErrorHan
 
     @Override
     public void error(ParsingError exception) {
+        System.out.println("ERROR: " + exception.getMessage());
         EclipseMarkerUtil.addMarker(resource, exception, IMarker.SEVERITY_ERROR);
     }
 
     @Override
     public void fatalError(ParsingError exception) {
+        System.out.println("FATAL ERROR: " + exception.getMessage());
         EclipseMarkerUtil.addMarker(resource, exception, IMarker.SEVERITY_ERROR);
     }
 
     @Override
     public void warning(ParsingError exception) {
+        System.out.println("WARNING: " + exception.getMessage());
         EclipseMarkerUtil.addMarker(resource, exception, IMarker.SEVERITY_WARNING);
     }
 
     @Override
     public void error(Message err) {
+        System.out.println("ERROR: " + err);
         EclipseMarkerUtil.addMarker(resource, err.toString(), err.line, IMarker.SEVERITY_ERROR);
     }
 
     @Override
     public void warn(Message warn) {
+        System.out.println("WARNING: " + warn);
         EclipseMarkerUtil.addMarker(resource, warn.toString(), warn.line, IMarker.SEVERITY_WARNING);
     }
 
     @Override
     public void info(String info) {
+        System.out.println("INFO: " + info);
         EclipseMarkerUtil.addMarker(resource, info, 0, IMarker.SEVERITY_INFO);
     }
 }
