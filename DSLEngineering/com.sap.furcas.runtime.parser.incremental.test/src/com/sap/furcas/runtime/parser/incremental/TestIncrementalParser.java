4ccac4cc4a709c1c7f93b3457d2f2cefb5db545a
Stephan Erb <stephan@dev.static-void.de>
2011-05-18 21:38:15 +0200

    Fix merge conflicts.

--- DSLEngineering/com.sap.furcas.runtime.parser.incremental.test/src/com/sap/furcas/runtime/parser/incremental/TestIncrementalParser.java
+++ DSLEngineering/com.sap.furcas.runtime.parser.incremental.test/src/com/sap/furcas/runtime/parser/incremental/TestIncrementalParser.java
@@ -30,7 +30,7 @@ import com.sap.furcas.runtime.textblocks.testutils.TextBlocksModelElementFactory
 import com.sap.furcas.test.fixture.ScenarioFixtureData;
 import com.sap.furcas.test.testutils.ResourceTestHelper;
 import com.sap.ide.cts.parser.errorhandling.SemanticParserException;
-import com.sap.ide.cts.parser.incremental.antlr.IncrementalParserFacade;
+import com.sap.ide.cts.parser.incremental.IncrementalParserFacade;
 
 public class TestIncrementalParser extends GeneratedParserAndFactoryBasedTest {
     
