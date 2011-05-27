f216785d84856a771102182542af48a384daced4
Stephan Erb <stephan@dev.static-void.de>
2011-05-27 14:35:22 +0200

    Reformat according to our coding guidelines.

--- DSLEngineering/com.sap.furcas.ide.editor/src/com/sap/furcas/ide/editor/contentassist/CtsContentAssistContext.java
+++ DSLEngineering/com.sap.furcas.ide.editor/src/com/sap/furcas/ide/editor/contentassist/CtsContentAssistContext.java
@@ -13,80 +13,79 @@ import com.sap.furcas.metamodel.FURCAS.TCS.SequenceElement;
 import com.sap.furcas.metamodel.FURCAS.TCS.Template;
 
 public class CtsContentAssistContext {
-	private Stack<FunctionCall> parentFunctionCallStack;
-	private Stack<Property> parentPropertyStack;
+    private Stack<FunctionCall> parentFunctionCallStack;
+    private Stack<Property> parentPropertyStack;
 
-	/**
-	 * stack of parent ClassTemplates and OperatorTemplates
-	 */
-	private Stack<Template> parentTemplateStack;
+    /**
+     * stack of parent ClassTemplates and OperatorTemplates
+     */
+    private Stack<Template> parentTemplateStack;
 
-	private SequenceElement sequenceElement;
-	private Token token;
-	private boolean isErrorContext;
+    private SequenceElement sequenceElement;
+    private Token token;
+    private boolean isErrorContext;
 
-	private boolean isOperator;
+    private boolean isOperator;
 
-	public boolean isOperator() {
-		return isOperator;
-	}
+    public boolean isOperator() {
+        return isOperator;
+    }
 
-	public void setOperator(boolean isOperator) {
-		this.isOperator = isOperator;
-	}
+    public void setOperator(boolean isOperator) {
+        this.isOperator = isOperator;
+    }
 
-	public boolean isErrorContext() {
-		return isErrorContext;
-	}
+    public boolean isErrorContext() {
+        return isErrorContext;
+    }
 
-	public void setErrorContext(boolean isErrorContext) {
-		this.isErrorContext = isErrorContext;
-	}
+    public void setErrorContext(boolean isErrorContext) {
+        this.isErrorContext = isErrorContext;
+    }
 
-	public void setToken(Token token) {
-		this.token = token;
-	}
+    public void setToken(Token token) {
+        this.token = token;
+    }
 
-	public Token getToken() {
-		return token;
-	}
+    public Token getToken() {
+        return token;
+    }
 
-	public void setParentFunctionCallStack(
-			Stack<FunctionCall> parentFunctionCallStack) {
-		this.parentFunctionCallStack = parentFunctionCallStack;
-	}
+    public void setParentFunctionCallStack(Stack<FunctionCall> parentFunctionCallStack) {
+        this.parentFunctionCallStack = parentFunctionCallStack;
+    }
 
-	public Stack<FunctionCall> getParentFunctionCallStack() {
-		return parentFunctionCallStack;
-	}
+    public Stack<FunctionCall> getParentFunctionCallStack() {
+        return parentFunctionCallStack;
+    }
 
-	public SequenceElement getSequenceElement() {
-		return sequenceElement;
-	}
+    public SequenceElement getSequenceElement() {
+        return sequenceElement;
+    }
 
-	public void setSequenceElement(SequenceElement sequenceElement) {
-		this.sequenceElement = sequenceElement;
+    public void setSequenceElement(SequenceElement sequenceElement) {
+        this.sequenceElement = sequenceElement;
 
-	}
+    }
 
-	@Override
-	public String toString() {
-		return token.getText();
-	}
+    @Override
+    public String toString() {
+        return token.getText();
+    }
 
-	public Stack<Property> getParentPropertyStack() {
-		return parentPropertyStack;
-	}
+    public Stack<Property> getParentPropertyStack() {
+        return parentPropertyStack;
+    }
 
-	public void setParentPropertyStack(Stack<Property> parentPropertyStack) {
-		this.parentPropertyStack = parentPropertyStack;
-	}
+    public void setParentPropertyStack(Stack<Property> parentPropertyStack) {
+        this.parentPropertyStack = parentPropertyStack;
+    }
 
-	public Stack<Template> getParentTemplateStack() {
-		return parentTemplateStack;
-	}
+    public Stack<Template> getParentTemplateStack() {
+        return parentTemplateStack;
+    }
 
-	public void setParentTemplateStack(Stack<Template> parentTemplateStack) {
-		this.parentTemplateStack = parentTemplateStack;
-	}
+    public void setParentTemplateStack(Stack<Template> parentTemplateStack) {
+        this.parentTemplateStack = parentTemplateStack;
+    }
 }
