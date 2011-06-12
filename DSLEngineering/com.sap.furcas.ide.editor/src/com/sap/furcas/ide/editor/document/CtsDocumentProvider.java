e2679f4410937c9d267a207d3be70b9fff6a25a0
Stephan Erb <stephan@dev.static-void.de>
2011-06-12 20:24:56 +0200

    Retrieve and display EMF Validation errors.
    For some strange reasons involving Query2 this only works when a
    textblocks resource has already been saved.

--- DSLEngineering/com.sap.furcas.ide.editor/src/com/sap/furcas/ide/editor/document/CtsDocumentProvider.java
+++ DSLEngineering/com.sap.furcas.ide.editor/src/com/sap/furcas/ide/editor/document/CtsDocumentProvider.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
@@ -15,17 +16,23 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.MultiRule;
 import org.eclipse.emf.common.command.BasicCommandStack;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EValidator;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
 import org.eclipse.jface.operation.IRunnableContext;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.Position;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.ui.internal.editors.text.WorkspaceOperationRunner;
 import org.eclipse.ui.texteditor.AbstractDocumentProvider;
 import org.eclipse.ui.texteditor.ResourceMarkerAnnotationModel;
 
 import com.sap.furcas.ide.editor.CtsActivator;
+import com.sap.furcas.ide.editor.imp.services.FurcasSourcePositionLocator;
+import com.sap.furcas.metamodel.FURCAS.textblocks.TextBlock;
 import com.sap.furcas.runtime.parser.PartitionAssignmentHandler;
 
 /**
@@ -39,6 +46,64 @@ import com.sap.furcas.runtime.parser.PartitionAssignmentHandler;
  */
 @SuppressWarnings("restriction")
 public class CtsDocumentProvider extends AbstractDocumentProvider {
+
+    /**
+     * EMF Validation adds error markes to resources. These markers don't have a valid
+     * position. This class reads these markers and sets a valid possition according
+     * to the content of the document attached to the annotation model.
+     * 
+     * @author Stephan Erb
+     *
+     */
+    private final class ResourceMarkerAnnotationModelExtension extends ResourceMarkerAnnotationModel {
+
+        private ResourceMarkerAnnotationModelExtension(IResource resource) {
+            super(resource);
+        }
+
+        @Override
+        protected Position createPositionFromMarker(IMarker marker) {
+            Position position = super.createPositionFromMarker(marker);
+            if (position != null) {
+                return position;
+            }
+            try {
+                if (!marker.getType().equals(EValidator.MARKER)) {
+                    return null; // not a marker we do care about.
+                }
+            } catch (CoreException e) {
+                // happens if the marker we try to receive is invalid
+                return null;
+            }
+            
+            final Position[] positions = new Position[1];
+            final FurcasSourcePositionLocator locator = new FurcasSourcePositionLocator();
+            final String uriAttribute = marker.getAttribute(EValidator.URI_ATTRIBUTE, null);
+            
+            if (uriAttribute != null) {
+                try {
+                    editingDomain.runExclusive(new Runnable() {
+                        @Override
+                        public void run() {
+                            EObject modelElementWithMarker = editingDomain.getResourceSet().getEObject(URI.createURI(uriAttribute), true);
+                            TextBlock rootBlock = ((CtsDocument) fDocument).getRootBlock();
+                            TextBlock tb = locator.findTextBlockOf(rootBlock, modelElementWithMarker, editingDomain.getResourceSet());
+                            if (tb != null) {
+                                int offset = locator.getStartOffset(tb);
+                                int length = locator.getLength(tb);
+                                positions[0] = new Position(offset, length);
+                            } else {
+                                // element not represented in this view
+                            }
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    return null;
+                }
+            } 
+            return positions[0]; // the one calculated or null
+        }
+    }
 
     private final IRunnableContext operationRunner = new WorkspaceOperationRunner();
     private final TransactionalEditingDomain editingDomain;
@@ -109,7 +174,7 @@ public class CtsDocumentProvider extends AbstractDocumentProvider {
     protected IAnnotationModel createAnnotationModel(Object element) throws CoreException {
         assert element.equals(editorInput.asLightWeightEditorInput());
         
-        return new ResourceMarkerAnnotationModel(WorkspaceSynchronizer.getFile(
+        return new ResourceMarkerAnnotationModelExtension(WorkspaceSynchronizer.getFile(
                 editorInput.getRootObject().eResource()));
     }
     
