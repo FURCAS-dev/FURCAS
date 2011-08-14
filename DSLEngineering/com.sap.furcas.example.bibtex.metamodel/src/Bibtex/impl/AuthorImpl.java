0b12d03634b4d5f55e5a85cefa637e2e1b88776d
Stephan Erb <stephan@dev.static-void.de>
2011-08-14 17:46:54 +0200

    Regenerate the bibtex language.

--- DSLEngineering/com.sap.furcas.example.bibtex.metamodel/src/Bibtex/impl/AuthorImpl.java
+++ DSLEngineering/com.sap.furcas.example.bibtex.metamodel/src/Bibtex/impl/AuthorImpl.java
@@ -45,271 +45,271 @@ import org.eclipse.emf.ecore.util.InternalEList;
  */
 public class AuthorImpl extends EObjectImpl implements Author {
     /**
-	 * The default value of the '{@link #getName() <em>Name</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @see #getName()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getName()
+     * @generated
+     * @ordered
+     */
     protected static final String NAME_EDEFAULT = null;
 
     /**
-	 * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
-	 * <!-- begin-user-doc -->
+     * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @see #getName()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getName()
+     * @generated
+     * @ordered
+     */
     protected String name = NAME_EDEFAULT;
 
     /**
-	 * The cached value of the '{@link #getPublications() <em>Publications</em>}' reference list.
-	 * <!-- begin-user-doc -->
+     * The cached value of the '{@link #getPublications() <em>Publications</em>}' reference list.
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @see #getPublications()
-	 * @generated
-	 * @ordered
-	 */
+     * @see #getPublications()
+     * @generated
+     * @ordered
+     */
     protected EList<Entry> publications;
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     protected AuthorImpl() {
-		super();
-	}
+        super();
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     @Override
     protected EClass eStaticClass() {
-		return BibtexPackage.Literals.AUTHOR;
-	}
+        return BibtexPackage.Literals.AUTHOR;
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     public String getName() {
-		return name;
-	}
+        return name;
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     public void setName(String newName) {
-		String oldName = name;
-		name = newName;
-		if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, BibtexPackage.AUTHOR__NAME, oldName, name));
-	}
+        String oldName = name;
+        name = newName;
+        if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, BibtexPackage.AUTHOR__NAME, oldName, name));
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     public EList<Entry> getPublications() {
-		if (publications == null) {
-			publications = new EObjectWithInverseResolvingEList.ManyInverse<Entry>(Entry.class, this, BibtexPackage.AUTHOR__PUBLICATIONS, BibtexPackage.ENTRY__AUTHOR);
-		}
-		return publications;
-	}
+        if (publications == null) {
+            publications = new EObjectWithInverseResolvingEList.ManyInverse<Entry>(Entry.class, this, BibtexPackage.AUTHOR__PUBLICATIONS, BibtexPackage.ENTRY__AUTHOR);
+        }
+        return publications;
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     public LiteratureDb getLiteraturedb() {
-		if (eContainerFeatureID() != BibtexPackage.AUTHOR__LITERATUREDB) return null;
-		return (LiteratureDb)eContainer();
-	}
+        if (eContainerFeatureID() != BibtexPackage.AUTHOR__LITERATUREDB) return null;
+        return (LiteratureDb)eContainer();
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	public NotificationChain basicSetLiteraturedb(LiteratureDb newLiteraturedb, NotificationChain msgs) {
-		msgs = eBasicSetContainer((InternalEObject)newLiteraturedb, BibtexPackage.AUTHOR__LITERATUREDB, msgs);
-		return msgs;
-	}
+        msgs = eBasicSetContainer((InternalEObject)newLiteraturedb, BibtexPackage.AUTHOR__LITERATUREDB, msgs);
+        return msgs;
+    }
 
 				/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     public void setLiteraturedb(LiteratureDb newLiteraturedb) {
-		if (newLiteraturedb != eInternalContainer() || (eContainerFeatureID() != BibtexPackage.AUTHOR__LITERATUREDB && newLiteraturedb != null)) {
-			if (EcoreUtil.isAncestor(this, newLiteraturedb))
-				throw new IllegalArgumentException("Recursive containment not allowed for " + toString());
-			NotificationChain msgs = null;
-			if (eInternalContainer() != null)
-				msgs = eBasicRemoveFromContainer(msgs);
-			if (newLiteraturedb != null)
-				msgs = ((InternalEObject)newLiteraturedb).eInverseAdd(this, BibtexPackage.LITERATURE_DB__AUTHOR, LiteratureDb.class, msgs);
-			msgs = basicSetLiteraturedb(newLiteraturedb, msgs);
-			if (msgs != null) msgs.dispatch();
-		}
-		else if (eNotificationRequired())
-			eNotify(new ENotificationImpl(this, Notification.SET, BibtexPackage.AUTHOR__LITERATUREDB, newLiteraturedb, newLiteraturedb));
-	}
+        if (newLiteraturedb != eInternalContainer() || (eContainerFeatureID() != BibtexPackage.AUTHOR__LITERATUREDB && newLiteraturedb != null)) {
+            if (EcoreUtil.isAncestor(this, newLiteraturedb))
+                throw new IllegalArgumentException("Recursive containment not allowed for " + toString());
+            NotificationChain msgs = null;
+            if (eInternalContainer() != null)
+                msgs = eBasicRemoveFromContainer(msgs);
+            if (newLiteraturedb != null)
+                msgs = ((InternalEObject)newLiteraturedb).eInverseAdd(this, BibtexPackage.LITERATURE_DB__AUTHOR, LiteratureDb.class, msgs);
+            msgs = basicSetLiteraturedb(newLiteraturedb, msgs);
+            if (msgs != null) msgs.dispatch();
+        }
+        else if (eNotificationRequired())
+            eNotify(new ENotificationImpl(this, Notification.SET, BibtexPackage.AUTHOR__LITERATUREDB, newLiteraturedb, newLiteraturedb));
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     @SuppressWarnings("unchecked")
     @Override
     public NotificationChain eInverseAdd(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
-		switch (featureID) {
-			case BibtexPackage.AUTHOR__PUBLICATIONS:
-				return ((InternalEList<InternalEObject>)(InternalEList<?>)getPublications()).basicAdd(otherEnd, msgs);
-			case BibtexPackage.AUTHOR__LITERATUREDB:
-				if (eInternalContainer() != null)
-					msgs = eBasicRemoveFromContainer(msgs);
-				return basicSetLiteraturedb((LiteratureDb)otherEnd, msgs);
-		}
-		return super.eInverseAdd(otherEnd, featureID, msgs);
-	}
+        switch (featureID) {
+            case BibtexPackage.AUTHOR__PUBLICATIONS:
+                return ((InternalEList<InternalEObject>)(InternalEList<?>)getPublications()).basicAdd(otherEnd, msgs);
+            case BibtexPackage.AUTHOR__LITERATUREDB:
+                if (eInternalContainer() != null)
+                    msgs = eBasicRemoveFromContainer(msgs);
+                return basicSetLiteraturedb((LiteratureDb)otherEnd, msgs);
+        }
+        return super.eInverseAdd(otherEnd, featureID, msgs);
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     @Override
     public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
-		switch (featureID) {
-			case BibtexPackage.AUTHOR__PUBLICATIONS:
-				return ((InternalEList<?>)getPublications()).basicRemove(otherEnd, msgs);
-			case BibtexPackage.AUTHOR__LITERATUREDB:
-				return basicSetLiteraturedb(null, msgs);
-		}
-		return super.eInverseRemove(otherEnd, featureID, msgs);
-	}
+        switch (featureID) {
+            case BibtexPackage.AUTHOR__PUBLICATIONS:
+                return ((InternalEList<?>)getPublications()).basicRemove(otherEnd, msgs);
+            case BibtexPackage.AUTHOR__LITERATUREDB:
+                return basicSetLiteraturedb(null, msgs);
+        }
+        return super.eInverseRemove(otherEnd, featureID, msgs);
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
 	@Override
 	public NotificationChain eBasicRemoveFromContainerFeature(NotificationChain msgs) {
-		switch (eContainerFeatureID()) {
-			case BibtexPackage.AUTHOR__LITERATUREDB:
-				return eInternalContainer().eInverseRemove(this, BibtexPackage.LITERATURE_DB__AUTHOR, LiteratureDb.class, msgs);
-		}
-		return super.eBasicRemoveFromContainerFeature(msgs);
-	}
+        switch (eContainerFeatureID()) {
+            case BibtexPackage.AUTHOR__LITERATUREDB:
+                return eInternalContainer().eInverseRemove(this, BibtexPackage.LITERATURE_DB__AUTHOR, LiteratureDb.class, msgs);
+        }
+        return super.eBasicRemoveFromContainerFeature(msgs);
+    }
 
 				/**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     @Override
     public Object eGet(int featureID, boolean resolve, boolean coreType) {
-		switch (featureID) {
-			case BibtexPackage.AUTHOR__NAME:
-				return getName();
-			case BibtexPackage.AUTHOR__PUBLICATIONS:
-				return getPublications();
-			case BibtexPackage.AUTHOR__LITERATUREDB:
-				return getLiteraturedb();
-		}
-		return super.eGet(featureID, resolve, coreType);
-	}
+        switch (featureID) {
+            case BibtexPackage.AUTHOR__NAME:
+                return getName();
+            case BibtexPackage.AUTHOR__PUBLICATIONS:
+                return getPublications();
+            case BibtexPackage.AUTHOR__LITERATUREDB:
+                return getLiteraturedb();
+        }
+        return super.eGet(featureID, resolve, coreType);
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     @SuppressWarnings("unchecked")
     @Override
     public void eSet(int featureID, Object newValue) {
-		switch (featureID) {
-			case BibtexPackage.AUTHOR__NAME:
-				setName((String)newValue);
-				return;
-			case BibtexPackage.AUTHOR__PUBLICATIONS:
-				getPublications().clear();
-				getPublications().addAll((Collection<? extends Entry>)newValue);
-				return;
-			case BibtexPackage.AUTHOR__LITERATUREDB:
-				setLiteraturedb((LiteratureDb)newValue);
-				return;
-		}
-		super.eSet(featureID, newValue);
-	}
+        switch (featureID) {
+            case BibtexPackage.AUTHOR__NAME:
+                setName((String)newValue);
+                return;
+            case BibtexPackage.AUTHOR__PUBLICATIONS:
+                getPublications().clear();
+                getPublications().addAll((Collection<? extends Entry>)newValue);
+                return;
+            case BibtexPackage.AUTHOR__LITERATUREDB:
+                setLiteraturedb((LiteratureDb)newValue);
+                return;
+        }
+        super.eSet(featureID, newValue);
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     @Override
     public void eUnset(int featureID) {
-		switch (featureID) {
-			case BibtexPackage.AUTHOR__NAME:
-				setName(NAME_EDEFAULT);
-				return;
-			case BibtexPackage.AUTHOR__PUBLICATIONS:
-				getPublications().clear();
-				return;
-			case BibtexPackage.AUTHOR__LITERATUREDB:
-				setLiteraturedb((LiteratureDb)null);
-				return;
-		}
-		super.eUnset(featureID);
-	}
+        switch (featureID) {
+            case BibtexPackage.AUTHOR__NAME:
+                setName(NAME_EDEFAULT);
+                return;
+            case BibtexPackage.AUTHOR__PUBLICATIONS:
+                getPublications().clear();
+                return;
+            case BibtexPackage.AUTHOR__LITERATUREDB:
+                setLiteraturedb((LiteratureDb)null);
+                return;
+        }
+        super.eUnset(featureID);
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     @Override
     public boolean eIsSet(int featureID) {
-		switch (featureID) {
-			case BibtexPackage.AUTHOR__NAME:
-				return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
-			case BibtexPackage.AUTHOR__PUBLICATIONS:
-				return publications != null && !publications.isEmpty();
-			case BibtexPackage.AUTHOR__LITERATUREDB:
-				return getLiteraturedb() != null;
-		}
-		return super.eIsSet(featureID);
-	}
+        switch (featureID) {
+            case BibtexPackage.AUTHOR__NAME:
+                return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+            case BibtexPackage.AUTHOR__PUBLICATIONS:
+                return publications != null && !publications.isEmpty();
+            case BibtexPackage.AUTHOR__LITERATUREDB:
+                return getLiteraturedb() != null;
+        }
+        return super.eIsSet(featureID);
+    }
 
     /**
-	 * <!-- begin-user-doc -->
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     @Override
     public String toString() {
-		if (eIsProxy()) return super.toString();
+        if (eIsProxy()) return super.toString();
 
-		StringBuffer result = new StringBuffer(super.toString());
-		result.append(" (name: ");
-		result.append(name);
-		result.append(')');
-		return result.toString();
-	}
+        StringBuffer result = new StringBuffer(super.toString());
+        result.append(" (name: ");
+        result.append(name);
+        result.append(')');
+        return result.toString();
+    }
 
 } //AuthorImpl
--- DSLEngineering/com.sap.furcas.example.bibtex.metamodel/src/Bibtex/util/BibtexSwitch.java
+++ DSLEngineering/com.sap.furcas.example.bibtex.metamodel/src/Bibtex/util/BibtexSwitch.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.util.Switch;
 
 /**
  * <!-- begin-user-doc -->
@@ -26,147 +28,131 @@ import org.eclipse.emf.ecore.EObject;
  * @see Bibtex.BibtexPackage
  * @generated
  */
-public class BibtexSwitch<T> {
+public class BibtexSwitch<T> extends Switch<T> {
     /**
-	 * The cached model package
-	 * <!-- begin-user-doc -->
+     * The cached model package
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     protected static BibtexPackage modelPackage;
 
     /**
-	 * Creates an instance of the switch.
-	 * <!-- begin-user-doc -->
+     * Creates an instance of the switch.
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @generated
-	 */
+     * @generated
+     */
     public BibtexSwitch() {
-		if (modelPackage == null) {
-			modelPackage = BibtexPackage.eINSTANCE;
-		}
-	}
+        if (modelPackage == null) {
+            modelPackage = BibtexPackage.eINSTANCE;
+        }
+    }
 
     /**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
+     * Checks whether this is a switch for the given package.
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
-    public T doSwitch(EObject theEObject) {
-		return doSwitch(theEObject.eClass(), theEObject);
-	}
+     * @parameter ePackage the package in question.
+     * @return whether this is a switch for the given package.
+     * @generated
+     */
+    @Override
+    protected boolean isSwitchFor(EPackage ePackage) {
+        return ePackage == modelPackage;
+    }
 
     /**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
+     * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+     * <!-- begin-user-doc -->
      * <!-- end-user-doc -->
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
-    protected T doSwitch(EClass theEClass, EObject theEObject) {
-		if (theEClass.eContainer() == modelPackage) {
-			return doSwitch(theEClass.getClassifierID(), theEObject);
-		}
-		else {
-			List<EClass> eSuperTypes = theEClass.getESuperTypes();
-			return
-				eSuperTypes.isEmpty() ?
-					defaultCase(theEObject) :
-					doSwitch(eSuperTypes.get(0), theEObject);
-		}
-	}
-
-    /**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
-     * <!-- end-user-doc -->
-	 * @return the first non-null result returned by a <code>caseXXX</code> call.
-	 * @generated
-	 */
+     * @return the first non-null result returned by a <code>caseXXX</code> call.
+     * @generated
+     */
+    @Override
     protected T doSwitch(int classifierID, EObject theEObject) {
-		switch (classifierID) {
-			case BibtexPackage.LITERATURE_DB: {
-				LiteratureDb literatureDb = (LiteratureDb)theEObject;
-				T result = caseLiteratureDb(literatureDb);
-				if (result == null) result = defaultCase(theEObject);
-				return result;
-			}
-			case BibtexPackage.ENTRY: {
-				Entry entry = (Entry)theEObject;
-				T result = caseEntry(entry);
-				if (result == null) result = defaultCase(theEObject);
-				return result;
-			}
-			case BibtexPackage.AUTHOR: {
-				Author author = (Author)theEObject;
-				T result = caseAuthor(author);
-				if (result == null) result = defaultCase(theEObject);
-				return result;
-			}
-			default: return defaultCase(theEObject);
-		}
-	}
+        switch (classifierID) {
+            case BibtexPackage.LITERATURE_DB: {
+                LiteratureDb literatureDb = (LiteratureDb)theEObject;
+                T result = caseLiteratureDb(literatureDb);
+                if (result == null) result = defaultCase(theEObject);
+                return result;
+            }
+            case BibtexPackage.ENTRY: {
+                Entry entry = (Entry)theEObject;
+                T result = caseEntry(entry);
+                if (result == null) result = defaultCase(theEObject);
+                return result;
+            }
+            case BibtexPackage.AUTHOR: {
+                Author author = (Author)theEObject;
+                T result = caseAuthor(author);
+                if (result == null) result = defaultCase(theEObject);
+                return result;
+            }
+            default: return defaultCase(theEObject);
+        }
+    }
 
     /**
-	 * Returns the result of interpreting the object as an instance of '<em>Literature Db</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpreting the object as an instance of '<em>Literature Db</em>'.
+     * <!-- begin-user-doc -->
      * This implementation returns null;
      * returning a non-null result will terminate the switch.
      * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpreting the object as an instance of '<em>Literature Db</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpreting the object as an instance of '<em>Literature Db</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
     public T caseLiteratureDb(LiteratureDb object) {
-		return null;
-	}
+        return null;
+    }
 
     /**
-	 * Returns the result of interpreting the object as an instance of '<em>Entry</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpreting the object as an instance of '<em>Entry</em>'.
+     * <!-- begin-user-doc -->
      * This implementation returns null;
      * returning a non-null result will terminate the switch.
      * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpreting the object as an instance of '<em>Entry</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpreting the object as an instance of '<em>Entry</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
     public T caseEntry(Entry object) {
-		return null;
-	}
+        return null;
+    }
 
     /**
-	 * Returns the result of interpreting the object as an instance of '<em>Author</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpreting the object as an instance of '<em>Author</em>'.
+     * <!-- begin-user-doc -->
      * This implementation returns null;
      * returning a non-null result will terminate the switch.
      * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpreting the object as an instance of '<em>Author</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpreting the object as an instance of '<em>Author</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+     * @generated
+     */
     public T caseAuthor(Author object) {
-		return null;
-	}
+        return null;
+    }
 
     /**
-	 * Returns the result of interpreting the object as an instance of '<em>EObject</em>'.
-	 * <!-- begin-user-doc -->
+     * Returns the result of interpreting the object as an instance of '<em>EObject</em>'.
+     * <!-- begin-user-doc -->
      * This implementation returns null;
      * returning a non-null result will terminate the switch, but this is the last case anyway.
      * <!-- end-user-doc -->
-	 * @param object the target of the switch.
-	 * @return the result of interpreting the object as an instance of '<em>EObject</em>'.
-	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
-	 * @generated
-	 */
+     * @param object the target of the switch.
+     * @return the result of interpreting the object as an instance of '<em>EObject</em>'.
+     * @see #doSwitch(org.eclipse.emf.ecore.EObject)
+     * @generated
+     */
+    @Override
     public T defaultCase(EObject object) {
-		return null;
-	}
+        return null;
+    }
 
 } //BibtexSwitch
