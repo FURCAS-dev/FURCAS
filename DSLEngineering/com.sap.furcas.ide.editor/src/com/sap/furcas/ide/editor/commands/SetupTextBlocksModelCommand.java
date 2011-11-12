7ea80db7364cb2848d6c47a050add1a403ca72ed
Stephan Erb <stephan@dev.static-void.de>
2011-11-12 14:32:24 +0100

    Always run the prettyprinter when an editor is opened.
    This happens even if there is an existing textblock. This assures that
    the incremental parser will never ever have to deal with stale
    textblocks.

--- DSLEngineering/com.sap.furcas.ide.editor/src/com/sap/furcas/ide/editor/commands/SetupTextBlocksModelCommand.java
+++ DSLEngineering/com.sap.furcas.ide.editor/src/com/sap/furcas/ide/editor/commands/SetupTextBlocksModelCommand.java
@@ -15,19 +15,17 @@ import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 
 import com.sap.furcas.ide.editor.CtsActivator;
-import com.sap.furcas.ide.editor.dialogs.PrettyPrintPreviewDialog;
 import com.sap.furcas.metamodel.FURCAS.TCS.ClassTemplate;
 import com.sap.furcas.metamodel.FURCAS.TCS.ConcreteSyntax;
 import com.sap.furcas.metamodel.FURCAS.textblocks.TextBlock;
+import com.sap.furcas.metamodel.FURCAS.textblocks.Version;
 import com.sap.furcas.prettyprinter.PrettyPrinter;
-import com.sap.furcas.prettyprinter.exceptions.SyntaxMismatchException;
 import com.sap.furcas.runtime.common.util.TCSSpecificOCLEvaluator;
 import com.sap.furcas.runtime.parser.PartitionAssignmentHandler;
 import com.sap.furcas.runtime.tcs.MessageHelper;
 import com.sap.furcas.runtime.tcs.TcsUtil;
 import com.sap.furcas.runtime.textblocks.TbUtil;
 import com.sap.furcas.runtime.textblocks.modifcation.TbChangeUtil;
-import com.sap.furcas.runtime.textblocks.validation.TbValidationUtil;
 import com.sap.ide.cts.parser.incremental.IncrementalParserFacade;
 import com.sap.ide.cts.parser.incremental.MappingRecoveringTextBlocksValidator;
 import com.sap.ide.cts.parser.incremental.TextBlockMappingRecoveringFailedException;
@@ -35,7 +33,10 @@ import com.sap.ide.cts.parser.incremental.TextBlockMappingRecoveringFailedExcept
 /**
  * Does everything that is required to create a valid TextBlocks model
  * for a given root object. Existing TextBlocks are re-used if available
- * and recovered if needed.
+ * and recovered if needed, in order to extract their information (comments, formatting, ...)
+ * 
+ * In any case, a fresh TextBlocks model is created. This is to make sure that the TextBlocks
+ * model is always valid and up-to-date (as expected by the incremental parser).
  * 
  * @author Stephan Erb
  */
@@ -66,100 +67,62 @@ public class SetupTextBlocksModelCommand extends RecordingCommand {
 
     @Override
     protected void doExecute() {
+        PrettyPrinter prettyPrinter = createPrettyPrinter();
         try  {
-            if (rootBlock == null) {
-                resultBlock = prettyPrint();
+            if (rootBlock != null) {
+                boolean wasNeeded = recoverOldBlock();
+                if (wasNeeded || TbUtil.getNewestVersion(rootBlock).getVersion() ==  Version.REFERENCE) {
+                    resultBlock = prettyPrinter.prettyPrint(rootObject, rootBlock);
+                    TbChangeUtil.deleteOtherVersions(rootBlock);
+                    TbChangeUtil.delete(rootBlock);
+                } else {
+                    resultBlock = rootBlock; // keep current one.
+                }
             } else {
-                resultBlock = validateAndMigrateTextBlocksModel();
+                resultBlock = prettyPrinter.prettyPrint(rootObject);
             }
-            
-            TbValidationUtil.assertTextBlockConsistencyRecursive(resultBlock);
-            TbValidationUtil.assertCacheIsUpToDate(resultBlock);
-            
-            if (resultBlock.eResource() == null) {
-                // It is not optimal to do this here, as it is meant to be the task
-                // of the parser facade. However, the invariant that each textblock
-                // is always assigned to a resources makes the implementation of the
-                // rest of the editor much easier. (Currently required to calculated
-                // marker positions within the current document).
-                partitionHandler.setDefaultPartition(rootObject.eResource());
-                partitionHandler.assignToDefaultTextBlocksPartition(resultBlock);
-            }
-            
         } catch (Exception e) {
             CtsActivator.logger.logError("Failed to initialize textual view", e);
             throw new RuntimeException("Failed to initialize textual view", e);
         }
+        setupPartitioning();
     }
 
-    private TextBlock validateAndMigrateTextBlocksModel() {
+    protected boolean recoverOldBlock() {
         MappingRecoveringTextBlocksValidator validator = new MappingRecoveringTextBlocksValidator(parserFacade);
-        
         if (validator.hasBrokenMapping(rootBlock)) {
-            TextBlock result = recoverBrokenTextBlockMapping(validator);
-            assertCorrectTextBlockType(result, TcsUtil.getMainClassTemplate(syntax));
-            return result;
-        } else {
-            assertCorrectTextBlockType(rootBlock, TcsUtil.getMainClassTemplate(syntax));
-            validator.checkAndMigrateTokenIds(rootBlock);
-            return rootBlock;
+            try {
+                validator.recoverMappingLink(rootBlock, mainTemplate);
+            } catch (TextBlockMappingRecoveringFailedException e) {
+                CtsActivator.logger.logError("Mapping recovery failed. Creating new textual view", e);
+            }
+            assertCorrectTextBlockType(rootBlock, mainTemplate);
+            return true;
         }
+        return false;
+    }
+    
+    protected void setupPartitioning() {
+        if (resultBlock.eResource() == null) {
+            // It is not optimal to do this here, as it is meant to be the task
+            // of the parser facade. However, the invariant that each textblock
+            // is always assigned to a resources makes the implementation of the
+            // rest of the editor much easier. (Currently required to calculated
+            // marker positions within the current document).
+            partitionHandler.setDefaultPartition(rootObject.eResource());
+            partitionHandler.assignToDefaultTextBlocksPartition(resultBlock);
+        }
+    }
+
+    protected PrettyPrinter createPrettyPrinter() {
+        return new PrettyPrinter(syntax, parserFacade.getParserScope().getMetamodelLookup(), 
+                new TCSSpecificOCLEvaluator(), parserFacade.getParserFactory());
     }
 
     private static void assertCorrectTextBlockType(TextBlock rootBlock, ClassTemplate rootTemplate) {
         if (rootBlock != null && !TbUtil.isTextBlockOfType(rootTemplate, rootBlock)) {
             throw new RuntimeException("Main template " + MessageHelper.getTemplateName(rootTemplate) +
                     " does not fit the given TextBlocks model.");
-        }
-    }
-        
-    /**
-     * Inspect the {@link #rootBlock} and try to recover it. After the recovery it might turn out that
-     * we have a block of the wrong type at hand. <p>
-     */
-    private TextBlock recoverBrokenTextBlockMapping(MappingRecoveringTextBlocksValidator validator) {
-        try {
-            validator.recoverMappingLink(rootBlock, mainTemplate);
-            return rootBlock;
-        } catch (TextBlockMappingRecoveringFailedException e) {
-            
-            String title = "Mapping Link Recovery Failed";
-            String error = "The link from the current document to the mapping definition is broken." + "The recovery failed! \n "
-            + e.getMessage() + "\n \n"
-            + "You have two options: \n"
-            + " a) Click OK and the model will be pretty printed. The old textual view will be replaced. \n"
-            + " b) Click Cancel to keep the texutal view as it is. However, this will force the recreation of all model elements"
-            + " defined in this view upon changes to this document.";
-            
-            TextBlock newRootBlock = prettyPrint();
-            
-            String oldTextualView = rootBlock.getCachedString();
-            String newTextualView = newRootBlock.getCachedString();
-            
-            PrettyPrintPreviewDialog previewDialog = new PrettyPrintPreviewDialog(title, error, oldTextualView, newTextualView);
-            boolean prettyPrintAccepted = previewDialog.open();
-            
-            if (prettyPrintAccepted) {
-                TbChangeUtil.deleteOtherVersions(rootBlock);
-                TbChangeUtil.delete(rootBlock);
-                return newRootBlock;
-            } 
-        }
-        return null;
-    }
-    
-    private TextBlock prettyPrint() {
-        try {
-            PrettyPrinter prettyPrinter = new PrettyPrinter(syntax, parserFacade.getParserScope().getMetamodelLookup(), 
-                    new TCSSpecificOCLEvaluator(), parserFacade.getParserFactory());
-            if (rootBlock == null) {
-                return prettyPrinter.prettyPrint(rootObject);
-            } else {
-                return prettyPrinter.prettyPrint(rootObject, rootBlock);
-            }
-        } catch (SyntaxMismatchException e) {
-            throw new RuntimeException("Model does not (fully) conform to syntax "
-                    + parserFacade.getParserFactory().getLanguageId() + ": \n\n" + e.getCause().getMessage(), e);
         }
     }
     
