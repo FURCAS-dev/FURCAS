6c7d43793a464ef2d31f9d4d2cad08a68c35c2c9
Stephan Erb <stephan@dev.static-void.de>
2012-01-11 11:31:24 +0100

    Drop outdated MOIN dependencies.

--- ngpm/com.sap.tc.moin.incubation.jdfacility.epi/src/com/sap/tc/moin/facility/pde/FacilityResourceChangeListener.java
+++
@@ -1,9 +0,0 @@
-package com.sap.tc.moin.facility.pde;
-
-import org.eclipse.core.resources.IResourceChangeListener;
-
-import com.sap.tc.moin.facility.primary.ipi.IpiFacilityComponent;
-
-public interface FacilityResourceChangeListener extends IResourceChangeListener, IpiFacilityComponent {
-
-}
