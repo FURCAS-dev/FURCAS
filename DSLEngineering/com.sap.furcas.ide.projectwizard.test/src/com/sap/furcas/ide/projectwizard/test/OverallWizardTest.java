5a21eb11191666d57955b554b4725acf5782b162
Stephan Erb <stephan@dev.static-void.de>
2012-01-13 17:12:14 +0100

    Try to enable the first set of wizard tests.

--- DSLEngineering/com.sap.furcas.ide.projectwizard.test/src/com/sap/furcas/ide/projectwizard/test/OverallWizardTest.java
+++ DSLEngineering/com.sap.furcas.ide.projectwizard.test/src/com/sap/furcas/ide/projectwizard/test/OverallWizardTest.java
@@ -3,16 +3,13 @@ package com.sap.furcas.ide.projectwizard.test;
 import static org.junit.Assert.fail;
 
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.sap.furcas.ide.projectwizard.util.CreateProject;
 import com.sap.furcas.ide.projectwizard.util.ProjectInfo;
 import com.sap.furcas.ide.projectwizard.wizards.FurcasWizard;
 
-@Ignore("UI Tests still not supported")
 public class OverallWizardTest {
-
 
     @Test
     public void wizardTest() {
