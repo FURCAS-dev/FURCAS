3da0a82c2218dbe5bc974092a403c7f77fe65d0c
Stephan Erb <stephan@dev.static-void.de>
2012-05-08 20:12:30 +0200

    Refactor SyntaxProvder towards a better interface.

--- DSLEngineering/com.sap.furcas.runtime.referenceresolving/src/com/sap/furcas/runtime/syntaxprovider/SyntaxProviderImpl.java
+++ DSLEngineering/com.sap.furcas.runtime.referenceresolving/src/com/sap/furcas/runtime/syntaxprovider/SyntaxProviderImpl.java
@@ -1,61 +1,39 @@
 package com.sap.furcas.runtime.syntaxprovider;
 
-import java.io.IOException;
-
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.ocl.ParserException;
+import org.eclipse.ocl.ecore.opposites.DefaultOppositeEndFinder;
 import org.eclipse.ocl.ecore.opposites.OppositeEndFinder;
 
-import com.sap.emf.ocl.trigger.TriggerManager;
 import com.sap.furcas.metamodel.FURCAS.TCS.ConcreteSyntax;
 import com.sap.furcas.runtime.parser.ParserFactory;
 import com.sap.furcas.runtime.referenceresolving.SyntaxRegistry;
 
 public class SyntaxProviderImpl implements SyntaxProvider {
     private ConcreteSyntax syntax;
-    private Resource syntaxResource;
     private final ParserFactory<?, ?> parserFactory;
-    private TriggerManager triggerManager;
     private final OppositeEndFinder oppositeEndFinder;
     
-    public SyntaxProviderImpl(ParserFactory<?, ?> parserFactory, OppositeEndFinder oppositeEndFinder) {
+    public SyntaxProviderImpl(ParserFactory<?, ?> parserFactory) {
         super();
         this.parserFactory = parserFactory;
-        this.oppositeEndFinder = oppositeEndFinder;
+        // Has to be consistent to the definition in the IncrementalParserFacade
+        this.oppositeEndFinder = DefaultOppositeEndFinder.getInstance();
     }
 
     @Override
-    public synchronized TriggerManager getTriggerManager(SyntaxRegistry syntaxRegistry) throws ParserException, IOException {
-        if (triggerManager == null) {
-            triggerManager = syntaxRegistry.getTriggerManagerForSyntax(getSyntax(null),
-                    oppositeEndFinder, /* monitor */ null, parserFactory);
-        }
-        return triggerManager;
-    }
-
-    @Override
-    public synchronized ConcreteSyntax getSyntax(ResourceSet loadSyntaxIn) throws IOException, ParserException {
-        if (syntax == null && syntaxResource == null) {
-            URI syntaxUri = parserFactory.getSyntaxResourceURI();
-            if (syntaxUri != null) {
-                if (loadSyntaxIn == null) {
-                    loadSyntaxIn = new ResourceSetImpl();
-                }
-                syntaxResource = loadSyntaxIn.createResource(syntaxUri);
-                syntaxResource.load(/* options */null);
-                for (EObject e : syntaxResource.getContents()) {
-                    if (e instanceof ConcreteSyntax) {
-                        syntax = (ConcreteSyntax) syntaxResource.getContents().iterator().next();
-                        break;
-                    }
-                }
-            }
+    public synchronized ConcreteSyntax getSyntax() {
+        if (syntax == null) {
+            ResourceSet resourceSet = new ResourceSetImpl();
+            syntax = (ConcreteSyntax) resourceSet.getEObject(URI.createURI(parserFactory.getSyntaxUUID()), true);
         }
         return syntax;
+    }
+
+    @Override
+    public synchronized void registerForIncrementalEvaluation(SyntaxRegistry registry) {
+        registry.registerSyntaxForIncrementalEvaluation(getSyntax(), oppositeEndFinder, /*progressmonitor*/null, parserFactory);
     }
 
 }
